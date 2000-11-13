15dc1e55cc8d07c5701107cc09bb8ef05d17ef48
berti <>
2000-11-13 16:18:54 +0000

    *** empty log message ***

---
+++ GrAL/external-control/src/IO/skip-comments.C
@@ -0,0 +1,27 @@
+#include "IO/skip-comments.h"
+
+template<>
+skip_comments_istream& operator>>(skip_comments_istream& in, std::string& s)
+{
+  istream& in1(in.the_istream());
+  int max_len = 256;
+  char* ss = new char[max_len];
+  // string ss = "";
+  int i = 0;
+  char c = 'a';
+  while(in1 && i < max_len-1 && (c != in.begin_comment()) && ! isspace(c)) {
+    c = in1.get();
+    ss[i] = c;
+    i++;
+  };
+  
+  if(c == in.begin_comment() || isspace(c)) {
+    if(in1)
+      in1.putback(c);
+    i--;
+  }
+
+   ss[i] = '\0';
+  s = ss;
+  return in;
+}
