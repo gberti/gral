028066c29578e5bf7006d9c20e01af4f1dd094c0
berti <>
2000-11-13 17:16:53 +0000

    removed duplicate spec. of op<<(string)

--- GrAL/external-control/include/IO/skip-comments.h
+++ GrAL/external-control/include/IO/skip-comments.h
@@ -57,31 +57,6 @@ inline std::istream& skip_comment(std::istream& in) {
 // und nicht als:  xyz ( #Kommentar wird ueberlesen).
 // scheint soweit zu klappen.
 
-inline skip_comments_istream& operator>>(skip_comments_istream& in, std::string& s)
-{
-  istream& in1(in.the_istream());
-  int max_len = 256;
-  char* ss = new char[max_len];
-  // string ss = "";
-  int i = 0;
-  char c = 'a';
-  while(in1 && i < max_len-1 && (c != in.begin_comment()) && ! isspace(c)) {
-    c = in1.get();
-    ss[i] = c;
-    i++;
-  };
-  
-  if(c == in.begin_comment() || isspace(c)) {
-    if(in1)
-      in1.putback(c);
-    i--;
-  }
-
-   ss[i] = '\0';
-  s = ss;
-  return in;
-}
-
 
 template<class T>
 inline skip_comments_istream& operator>>(skip_comments_istream& in, T& t)
