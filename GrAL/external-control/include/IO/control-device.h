081ed799cd7093ebd0be1562708b8dd1123f503a
berti <>
2000-11-14 16:25:02 +0000

    comments doxygen-ified

--- GrAL/external-control/include/IO/control-device.h
+++ GrAL/external-control/include/IO/control-device.h
@@ -16,20 +16,18 @@
 
 #include "IO/mutator.h"
 
-/////////////////////////////////////////////////////////////////
-//
-//   abstract interface for controlling parameters,
-//   e.g. they may be 
-//   - read from a file
-//   - set interactively via a GUI or a command interpreter
-//
-/////////////////////////////////////////////////////////////////
+
+/*! \file  \brief abstract interface for controlling parameters,
+     e.g. they may be 
+     - read from a file
+     - set interactively via a GUI or a command interpreter
+*/
 
 
 
 
 
-// implementation class
+//!  implementation class ("letter")
 class control_device_impl : public controlable {
 public:
   virtual void update() = 0;
@@ -48,7 +46,7 @@ public:
   virtual ~control_device_impl() {}
 };
 
-// envelope class
+//! envelope class
 class ControlDevice  {
 public:
   ControlDevice(control_device_impl* imp = 0) : impl(imp) {}
@@ -73,6 +71,7 @@ private:
 };
 
 
+//! register variable t to ControlDevice
 template<class T>
 inline void RegisterAt(ControlDevice& Ctrl, std::string const& name, T& t)
 { 
@@ -80,22 +79,28 @@ inline void RegisterAt(ControlDevice& Ctrl, std::string const& name, T& t)
   Ctrl.add(name, p); 
 }
 
+//! register variable t to ControlDevice
 template<class T>
 inline void RegisterAt(ControlDevice& Ctrl, char const*   name, T& t)
 {  Ctrl.add(name, new TypedMutator<T>(t)); }
 
 
 
-// a simple ControlDevice that reads name-value-pairs from a file
-extern ControlDevice GetStreamDevice(istream* in, std::string const& name = "");
-//extern ControlDevice GetFileControlDevice(const string& filename, const string& name);
-extern ControlDevice GetFileControlDevice(char const*   filename, std::string const& name);
+//! a simple ControlDevice that reads name-value-pairs from a stream
+extern ControlDevice GetStreamDevice(istream* in, 
+                                     std::string const& name = "");
 
+
+//! a simple ControlDevice that reads name-value-pairs from a file
+extern ControlDevice GetFileControlDevice(char const*   filename, 
+                                          std::string const& name);
+
+//! ControlDevice that first reads from file and then from command line
 extern ControlDevice GetCommandlineAndFileControlDevice(int argc, char* argv[],
 							std::string const& filename, 
 							std::string const& name);
 
-
+//! ControlDevice that reads from given a stream and from a file
 extern ControlDevice GetDuplexControlDevice(std::istream     & in2,
 					    char        const* filename, 
                                             std::string const& name);
