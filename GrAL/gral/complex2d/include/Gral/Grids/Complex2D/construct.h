846443e0f730dfcb171bb43d7a1225d4e8236e78
berti <>
2000-12-12 16:50:32 +0000

    changed names Construct() to Construct_complex2d Added destGeom arguments all over

--- GrAL/gral/complex2d/include/Gral/Grids/Complex2D/construct.h
+++ GrAL/gral/complex2d/include/Gral/Grids/Complex2D/construct.h
@@ -10,8 +10,9 @@
 //----------------------------------------------------------------
 
 
-#include "Grids/Algorithms/construct-grid.h"
-#include "Grids/Complex2D/complex2d.h"
+#include "Gral/Base/construct-grid.h"
+
+#include "Gral/Grids/Complex2D/complex2d.h"
 
 //----------------------------------------------------------------
 // copy-construct a Complex2D <dest> from another 2D grid <source_grid>
@@ -22,39 +23,41 @@
 // This template is defined in generic/complex2d-construct.C
 //----------------------------------------------------------------
 
-template<class Connectivity,class Geometry>
+template<class Geom1, class G2, class Geom2>
 extern void 
-Construct(Complex2D&          dest, 
-	  const Connectivity& source_grid, 
-	  const Geometry&     source_geom);
+Construct_complex2d(Complex2D& CC, 
+                    Geom1         & destGeom,
+                    G2       const& srcG,
+                    Geom2    const& srcGeom);
 
-
-
-template<class Connectivity, class Geometry, class VertexMap>
-extern void 
-Construct(Complex2D& CC, 
-	  const Connectivity& Connect,
-	  const Geometry& geom,
-	  VertexMap&  VertexCorr); 
+template<class Geom1, class G2, class Geom2, class VertexMap>
+extern void
+Construct_complex2d(Complex2D     & CC, 
+                    Geom1         & destGeom,
+                    G2       const& srcG,
+                    Geom2    const& geom,
+                    VertexMap     &  VertexCorr);
 // VertexCorr  maps src::vertex_handles to  complex2d::vertex_handles
 // for transfer of grid-related information, e.g. grid-functions.
 
-template<class Connectivity, class Geometry, class VertexMap, class CellMap>
+template<class Geom1, class G2, class Geom2, class VertexMap, class CellMap>
 extern void 
-Construct(Complex2D& CC, 
-	  const Connectivity& Connect,
-	  const Geometry& geom,
-	  VertexMap&  VertexCorr,  // maps src::handles to
-	  CellMap  &  CellCorr);   // complex2d::handles
+Construct_complex2d(Complex2D     & CC, 
+                    Geom1         & destGeom,
+                    G2       const& srcG,
+                    Geom2    const& srcGeom,
+                    VertexMap     & VertexCorr,  // maps src::handles to
+                    CellMap       & CellCorr);   // complex2d::handles
+
 
 // construct without geometry
-template<class Connectivity, class VertexMap, class CellMap>
-extern void 
-Construct0(Complex2D& CC, 
-	  const Connectivity& Connect,
-	  VertexMap&  VertexCorr,  // maps src::handles to
-	  CellMap  &  CellCorr);   // complex2d::handles
 
+template<class G2, class VertexMap, class CellMap>
+extern void 
+Construct_complex2d0(Complex2D     & CC, 
+                     G2       const& srcG,
+                     VertexMap     &  VertexCorr,  // maps src::handles to
+                     CellMap       &  CellCorr);   // complex2d::handles
 
 
 //----------------------------------------------------------------
@@ -62,24 +65,27 @@ Construct0(Complex2D& CC,
 //  Grids/Algorithms/construct-grid.h
 //----------------------------------------------------------------
 
-template<class G2, class Geom2>
+template<class Geom1, class G2, class Geom2>
 extern  void 
 ConstructGrid(Complex2D  & destG,
+              Geom1      & destGeom,
 	      G2    const& srcG, 
 	      Geom2 const& srcGeom);
 
-template<class G2, class Geom2, class VertexMap>
+template<class Geom1, class G2, class Geom2, class VertexMap>
 extern void 
 ConstructGridV(Complex2D     & destG, 
+               Geom1         & destGeom,
 	       G2       const& srcG,
 	       Geom2    const& srcGeom,
 	       VertexMap     & VCorrG2_G1); 
 // VertexCorr  maps G2::vertex_handles to  G1::vertex_handles
 // for transfer of grid-related information, e.g. grid-functions.
 
-template<class G2, class Geom2, class VertexMap, class CellMap>
+template<class Geom1, class G2, class Geom2, class VertexMap, class CellMap>
 extern void 
 ConstructGridVC(Complex2D     & destG, 
+                Geom1         & destGeom,
 		G2       const& srcG,
 		Geom2    const& srcGeom,
 		VertexMap     & VCorrG2_G1,
@@ -97,7 +103,7 @@ ConstructGrid0(Complex2D     & destG,
 // for inclusion model
 
 #ifdef NMWR_INCLUDE_TEMPLATE_DEFS
-#include "./generic/complex2d-construct.C"
+#include "Gral/Grids/Complex2D/internal/complex2d-construct.C"
 #endif
 
 #endif
