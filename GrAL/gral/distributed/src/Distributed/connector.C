511d57498b7f23650da64b403974296d15da9a0b
berti <>
2000-12-16 12:23:46 +0000

    *** empty log message ***

---
+++ GrAL/gral/distributed/src/Distributed/connector.C
@@ -0,0 +1,3 @@
+#include "Gral/Distributed/connector.h"
+
+connector_impl::~connector_impl() {}
---
+++ GrAL/gral/distributed/src/Distributed/overlap-pattern.C
@@ -0,0 +1,26 @@
+
+//----------------------------------------------------------------
+//   (c) Guntram Berti, 1998
+//   Chair for Numerical Mathematics & Scientific Computing (NMWR)
+//   TU Cottbus - Germany
+//   http://math-s.math.tu-cottbus.de/NMWR
+//   
+//----------------------------------------------------------------
+
+#include <string>
+
+#include "Gral/Distributed/overlap-pattern.h"
+#include "Utility/pre-post-conditions.h"
+
+overlap_pattern::overlap_pattern(string const& pat)
+{
+  for(unsigned i = 0; i < pat.size(); ++i)
+    if(pat[i] == 'C')
+      q.push(cell_tag);
+    else if  (pat[i] == 'V')
+      q.push(vertex_tag);
+    else
+      ENSURE_ALWAYS(false, "unknown adjacency pattern: " << pat[i] << '\n',1);
+}
+
+overlap_pattern::overlap_pattern() {}
