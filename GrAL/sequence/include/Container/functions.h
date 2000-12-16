9eed06f51c4c5499a67f367d505e42415d5049e2
berti <>
2000-12-16 14:40:24 +0000

    avoid name clash with (almost std) identity

--- GrAL/sequence/include/Container/functions.h
+++ GrAL/sequence/include/Container/functions.h
@@ -1,13 +1,12 @@
 #ifndef NMWR_GB_CONTAINER_FUNCTIONS_H
 #define NMWR_GB_CONTAINER_FUNCTIONS_H
 
-#include "compiler-config.h"
+#include "Config/compiler-config.h"
 
-#ifdef __KCC
-#include <functional.h>
+#include <functional>
 
 template <class T>
-struct identity : public std::unary_function<T, T> {
+struct my_identity : public std::unary_function<T, T> {
   const T& operator()(const T& x) const { return x; }
 };
 #endif
