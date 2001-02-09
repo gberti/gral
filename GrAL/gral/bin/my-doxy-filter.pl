8ff2c14b774b095b8a76270e2336304f4699df06
berti <>
2001-02-09 14:04:14 +0000

    *** empty log message ***

--- GrAL/gral/bin/my-doxy-filter.pl
+++ GrAL/gral/bin/my-doxy-filter.pl
@@ -2,8 +2,10 @@
 
 $STL = "http://www.sgi.com/Technology/STL";
 $GRAL = "file:/home/berti/CVS-work/gral/base/doc/hyperlatex/html";
+$ROOT = "file:/home/berti/CVS-work/";
 
 while(<>) {
+  s|\$gral-([^ ]+)[ ]*([a-z\-_A-Z0-9]+)|<A HREF="$ROOT/$1/doxygen/html/index.html"> $2 </A>|g;
   s|\$GrAL[ ]+([a-z\-_A-Z0-9]+)|<A HREF="$GRAL/$1.html">$1</A>|g;
   s|Adaptable Unary Function|<A HREF="$STL/AdaptableUnaryFunction.html"> Adaptable Unary Function</A>|g;
   s|Forward Iterator|<A HREF="$STL/ForwardIterator.html">Forward Iterator</A>|g;
