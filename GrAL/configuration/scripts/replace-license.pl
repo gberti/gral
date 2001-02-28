f99159b1273f192b358466c01122e5777b37f43c
berti <>
2001-02-28 16:27:46 +0000

    *** empty log message ***

---
+++ GrAL/configuration/scripts/replace-license.pl
@@ -0,0 +1,30 @@
+#! /opt/local/bin/perl -i
+
+
+$license1 = "
+/* ------------------------------------------------------------
+
+    Copyright (C) 1997-2001 Guntram Berti.
+
+    This file is part of the Grid Algorithms Library (GrAL),
+    which is available at 
+    http://www.math.tu-cottbus.de/~berti/gral
+
+    You are free to use, copy and modify the sources under
+    certain circumstances, provided this copyright notice
+    remains intact. See the file LICENSE for details.
+
+--------------------------------------------------------------- */
+";
+
+$license2 = "
+// \$LICENSE
+";
+
+$license = $license1;
+
+print "executing replace-license.pl\n";
+while(<>) {
+  s|//[ ]*\$LICENSE|$license|;
+  print;
+}
