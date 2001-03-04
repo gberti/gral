f9b29aaff5fa1a4ebbb02ce5034d75507ec494f5
berti <>
2001-03-04 18:09:41 +0000

    use relative default paths

--- GrAL/gral/bin/install-doc.pl
+++ GrAL/gral/bin/install-doc.pl
@@ -6,20 +6,27 @@ use Getopt::Long;
 
 $STL = "http://www.sgi.com/tech/stl";
 $METIS ="http://www-users.cs.umn.edu/~karypis/metis/";
-$GRALCONCEPTS = "file:/home/berti/CVS-work/gral/base/doc/hyperlatex/html";
-$ROOT = "file:/home/berti/CVS-work/";
+$ROOT = "../../../..";
 $DOXYPATH = "doxygen/html";
+
 
 &GetOptions("root=s" => \$ROOT,
             "path=s" => \$DOXYPATH,
             "stl=s"  => \$STL,
             "gral=s" => \$GRALCONCEPTS);
 
+$GRAL= "$ROOT/gral";
+$GRALCONCEPTS   ="$GRAL/base/doc/hyperlatex/html";
+$GRALCONCEPTSTOP="$GRALCONCEPTS/top.html";
+$GRALINDEX      ="$GRAL/$DOXYPATH/index.html";
+
 while(<>) {
   s|%STL|$STL|g;
   s|%Metis|$METIS|g;
   s|%METIS|$METIS|g;
-  s|%GRALCONCEPTS|$GRALCONCEPTS|g;
+  s|%GRALCONCEPTS\"|$GRALCONCEPTS\"|g;
+  s|%GRALINDEX\"|$GRALINDEX\"|g;
+  s|%GRALCONCEPTSTOP\"|$GRALCONCEPTSTOP\"|g;
   s|%ROOT|$ROOT|g;
   s|%DOXYPATH|$DOXYPATH|g;
   print;
