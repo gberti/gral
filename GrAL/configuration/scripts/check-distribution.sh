b4d8447b5013615fc041c66f4c66766e80f810e3
berti <>
2001-03-05 08:23:25 +0000

    *** empty log message ***

--- GrAL/configuration/scripts/check-distribution.sh
+++ GrAL/configuration/scripts/check-distribution.sh
@@ -5,9 +5,9 @@ TMP=$1
 cd ${TMP}
 rm -rf GrAL
 rm -rf gral
-zcat ${TMP}/gral-all.tar.gz | tar -xvf -
+gzip -cd ${TMP}/gral-all.tar.gz | tar -xvf -
 cd GrAL
-zcat ${TMP}/gral-doc.tar.gz | tar -xfv - 
+gzip -cd ${TMP}/gral-doc.tar.gz | tar -xfv - 
 gmake new
 gmake install PREFIX=${TMP}/gral
 gmake check DEVELOP=user PREFIX=${TMP}/gral
