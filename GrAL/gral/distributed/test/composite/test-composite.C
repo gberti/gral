b360b9fa49a650055ae81ce27c46c077232f1b8f
berti <>
2001-04-27 12:04:28 +0000

    tests now also for composite grid I/O

--- GrAL/gral/distributed/test/composite/test-composite.C
+++ GrAL/gral/distributed/test/composite/test-composite.C
@@ -10,7 +10,13 @@
 #include "Gral/Distributed/composite-grid.h"
 #include "Gral/Distributed/composite-grid-function.h"
 
-#include "Gral/IO/istream-grid-complex2d-fmt.h"
+#include "Gral/Distributed/copy-composite.h"
+#include "Gral/Distributed/composite-grid-io.h"
+#include "Gral/Distributed/overlapping-grid-io.h"
+#include "Gral/Distributed/overlap-io.h"
+#include "Gral/Distributed/overlap-range-io.h"
+
+#include "Gral/IO/complex2d-format.h"
 
 #include "Gral/Grids/Complex2D/complex2d.h"
 #include "Gral/Grids/Complex2D/boundary.h"
@@ -66,14 +72,15 @@ int main(int argc, char* argv[]) {
 
   typedef CompositeGrid<coarse_grid_type,fine_grid_type> comp_grid_type;
   typedef grid_types<coarse_grid_type>                   cgt;
-  typedef cgt::Cell         CoarseCell;
+  typedef cgt::Cell                                      CoarseCell;
+
 
   comp_grid_type  compG;
   typedef bijective_mapping<fgt::vertex_handle, fgt::vertex_handle> vertex_corr_map;
-  typedef bijective_mapping<fgt::cell_handle,  fgt::cell_handle>   cell_corr_map;
-  grid_function<CoarseCell, vertex_corr_map> master2part_v;
-  grid_function<CoarseCell, cell_corr_map>   master2part_c;
-  overlap_pattern ovlp_pattern(pattern_string);
+  typedef bijective_mapping<fgt::cell_handle,   fgt::cell_handle>   cell_corr_map;
+  grid_function<CoarseCell, vertex_corr_map>    master2part_v;
+  grid_function<CoarseCell, cell_corr_map>      master2part_c;
+  overlap_pattern                               ovlp_pattern(pattern_string);
  
   ConstructComposite(compG,
                      Prtng,MasterGeom,
@@ -83,28 +90,146 @@ int main(int argc, char* argv[]) {
 
   grid_function<cgt::Cell,fine_geom_type> compGeom(compG.TheCoarseGrid());
 
-  OstreamComplex2DFmt OG(cout);
-  ConstructGrid0(OG,compG.TheCoarseGrid());
-  cout << "master -> part (vertices):\n";
-  for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
-    cout << "part " << C.handle() << '\n'; 
-    for(fgt::VertexIterator v(MasterG); !v.IsDone(); ++v) {
-      if(master2part_v(*C).defined(v.handle()))
-        cout << v.handle() << " -> " << master2part_v(*C)(v.handle()) << '\n';
+  {
+    OstreamComplex2DFmt OG(cout);
+    ConstructGrid0(OG,compG.TheCoarseGrid());
+    cout << "master -> part (vertices):\n";
+    for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
+      cout << "part " << C.handle() << '\n'; 
+      for(fgt::VertexIterator v(MasterG); !v.IsDone(); ++v) {
+        if(master2part_v(*C).defined(v.handle()))
+          cout << v.handle() << " -> " << master2part_v(*C)(v.handle()) << '\n';
+      }
     }
-  }
-  cout << '\n';
-  for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
-    fine_grid_type const& G_C = compG.Grid(*C);
-    compGeom[*C].set_grid(G_C);
-    for(fgt::VertexIterator v(MasterG); !v.IsDone(); ++v) {
-      if(master2part_v(*C).defined(v.handle()))
-        compGeom[*C].coord(G_C.vertex(master2part_v(*C)(v.handle())))
-          = MasterGeom.coord(*v);
-    }
-    OstreamComplex2DFmt OG_C(cout);
-    ConstructGrid(OG_C, compG.Grid(*C), compGeom(*C));
     cout << '\n';
- }
+    for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
+      fine_grid_type const& G_C = compG.Grid(*C);
+      compGeom[*C].set_grid(G_C);
+      for(fgt::VertexIterator v(MasterG); !v.IsDone(); ++v) {
+        if(master2part_v(*C).defined(v.handle()))
+          compGeom[*C].coord(G_C.vertex(master2part_v(*C)(v.handle())))
+            = MasterGeom.coord(*v);
+      }
+      OstreamComplex2DFmt OG_C(cout);
+      ConstructGrid(OG_C, compG.Grid(*C), compGeom(*C));
+      cout << '\n';
+  }
+  }
 
+
+  comp_grid_type CG2;
+  // bijective_mapping<int,int> coarse_corr_c;
+  cell_morphism<coarse_grid_type, coarse_grid_type> coarse_corr_c(compG.TheCoarseGrid(),
+                                                                  CG2.TheCoarseGrid());
+  // it is not so simple here to use vertex/cell_morphism 
+  // instead of  bijective_mapping<> ! (Initialization!)
+  grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_v(compG.TheCoarseGrid());
+  grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_c(compG.TheCoarseGrid());
+
+  CopyComposite(CG2,compG, coarse_corr_c,fine_corr_v, fine_corr_c);
+
+  check_composite_grid(CG2);
+
+  
+   {
+    OstreamComplex2DFmt OG(cout);
+    ConstructGrid0(OG,CG2.TheCoarseGrid());
+    cout << "master -> part (vertices):\n";
+    for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
+      cgt::Cell C2 = coarse_corr_c(*C);
+      cout << "part " << C2.handle() << '\n'; 
+      for(fgt::VertexIterator mv(MasterG); !mv.IsDone(); ++mv) {
+        if(master2part_v(*C).defined(mv.handle()))
+          cout << mv.handle() << " -> " 
+               << fine_corr_v(C2)(master2part_v(*C)(mv.handle())) << '\n';
+      }
+    }
+    cout << '\n';
+    grid_function<cgt::Cell,fine_geom_type> CGeom2(CG2.TheCoarseGrid());
+    for(cgt::CellIterator C(compG.TheCoarseGrid()); !C.IsDone(); ++C) {
+      cgt::Cell C2 = coarse_corr_c(*C);
+      fine_grid_type const& G_C2 = CG2.Grid(C2);
+      CGeom2[C2].set_grid(G_C2);
+      for(fgt::VertexIterator mv(MasterG); !mv.IsDone(); ++mv) {
+        if(master2part_v(*C).defined(mv.handle()))
+          CGeom2[C2].coord(G_C2.vertex( fine_corr_v(C2)(master2part_v(*C)(mv.handle()))))
+            = MasterGeom.coord(*mv);
+      }
+      OstreamComplex2DFmt OG_C(cout);
+      ConstructGrid(OG_C, CG2.Grid(C2), CGeom2(C2));
+      cout << '\n';
+    }
+   }
+
+
+   typedef OstreamComplex2DFmt coarse_grid_output_t;
+   typedef OstreamComplex2DFmt fine_grid_output_t;
+   typedef  overlap_output<
+     coarse_grid_output_t, 
+     fine_grid_output_t, 
+     overlap_range_output> overlap_output_t;
+   typedef overlapping_grid_output<
+     coarse_grid_output_t, 
+     fine_grid_output_t,
+     overlap_output_t> overlapping_grid_output_t;
+
+   {
+     // use a block: compOut must get destructed such that files get closed!
+     composite_grid_output<coarse_grid_output_t, overlapping_grid_output_t> compOut("compgrid");
+     
+     cell_morphism<coarse_grid_type, coarse_grid_output_t> coarse_corr_c_out(compG  .TheCoarseGrid(),
+                                                                             compOut.TheCoarseGrid());
+     // it is not so simple here to use vertex/cell_morphism 
+     // instead of  bijective_mapping<> ! (Initialization!)
+     grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_v_out(compG.TheCoarseGrid());
+     grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_c_out(compG.TheCoarseGrid());
+     
+     CopyComposite(compOut,compG, coarse_corr_c_out, fine_corr_v_out, fine_corr_c_out);
+   }
+
+   {
+     
+     typedef IstreamComplex2DCombFmt coarse_grid_input_t;
+     typedef IstreamComplex2DCombFmt fine_grid_input_t;
+     typedef  overlap_input<
+       coarse_grid_type, 
+       fine_grid_input_t, 
+       overlap_range_input>  overlap_input_t;
+     typedef overlapping_grid_input<
+       coarse_grid_type, 
+       fine_grid_input_t,
+       overlap_input_t> overlapping_grid_input_t;
+     
+     typedef composite_grid_input<
+       coarse_grid_input_t,
+       coarse_grid_type,
+       overlapping_grid_input_t>  composite_input_t;
+
+     composite_input_t compIn("compgrid");
+     comp_grid_type compG2;
+     cell_morphism<composite_input_t::coarse_grid_type, coarse_grid_type> 
+       coarse_corr_c_in(compIn.TheCoarseGrid(),
+                        compG2.TheCoarseGrid());
+     // it is not so simple here to use vertex/cell_morphism 
+     // instead of  bijective_mapping<> ! (Initialization!)
+     grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_v_in(compIn.TheCoarseGrid());
+     grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_c_in(compIn.TheCoarseGrid());
+     
+     CopyComposite(compG2,compIn, coarse_corr_c_in, fine_corr_v_in, fine_corr_c_in);
+
+     check_composite_grid(compG2);
+
+     // re-output the grid compG2 to see if there is any difference
+     {
+       composite_grid_output<coarse_grid_output_t, overlapping_grid_output_t> compOut("compgridB");
+       cell_morphism<coarse_grid_type, coarse_grid_output_t> coarse_corr_c_out(compG2 .TheCoarseGrid(),
+                                                                             compOut.TheCoarseGrid());
+       grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_v_out(compG2.TheCoarseGrid());
+       grid_function<CoarseCell, bijective_mapping<int,int> > fine_corr_c_out(compG2.TheCoarseGrid());
+       
+       CopyComposite(compOut,compG2, coarse_corr_c_out, fine_corr_v_out, fine_corr_c_out);
+     }
+
+   }
+   
 }
