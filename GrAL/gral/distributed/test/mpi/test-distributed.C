8015d1e20130519fa1008926aef7afa5715465f1
berti <>
2001-05-05 17:40:08 +0000

    test copying of distributed grid functions.

--- GrAL/gral/distributed/test/mpi/test-distributed.C
+++ GrAL/gral/distributed/test/mpi/test-distributed.C
@@ -141,6 +141,15 @@ int main(int argc, char* argv[]) {
   for(RgeVertexIterator V = MpiG.LocalRange().FirstVertex(); ! V.IsDone(); ++V)
     dgfv[*V] = (*V).handle();
 
+  distr_grid_function<Cell,  int,distr_grid_type> dgfc_copy(dgfc);
+  distr_grid_function<Vertex,int,distr_grid_type> dgfv_copy(dgfv);
+  for(RgeCellIterator   C = MpiG.LocalRange().FirstCell();   ! C.IsDone(); ++C) {
+    REQUIRE_ALWAYS( (dgfc(*C) == dgfc_copy(*C)), "copy not identical!\n",1);
+  }
+  for(RgeVertexIterator V = MpiG.LocalRange().FirstVertex(); ! V.IsDone(); ++V) {
+    REQUIRE_ALWAYS( (dgfv(*V) == dgfv_copy(*V)), "copy not identical!\n",1);
+  }
+
   // put # adj local cells on dgfv2
   distr_grid_function<Vertex,int,distr_grid_type> dgfv2(MpiG,0);
   dgfv2.set_added_on_shared();
