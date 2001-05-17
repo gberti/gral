6b929cd2e2242f23534e1d77874cd04077afb19a
berti <>
2001-05-17 13:44:55 +0000

    *** empty log message ***

---
+++ GrAL/gral/cartesian2d/include/Gral/Grids/Cartesian2D/all.h
@@ -0,0 +1,12 @@
+#ifndef NMWR_GB_GRAL_CARTESIAN2D_ALL_H
+#define NMWR_GB_GRAL_CARTESIAN2D_ALL_H
+
+#include "Gral/Grids/Cartesian2D/cartesian-grid2d.h"
+#include "Gral/Grids/Cartesian2D/grid-functions.h"
+#include "Gral/Grids/Cartesian2D/partial-grid-functions.h"
+#include "Gral/Grids/Cartesian2D/boundary.h"
+#include "Gral/Grids/Cartesian2D/subrange.h"
+#include "Gral/Grids/Cartesian2D/stored-geometry.h"
+#include "Gral/Grids/Cartesian2D/mapped-geometry.h"
+
+#endif
