600cb90eb1f49dfb85aaf4ae7166d6db159d8857
berti <>
2001-09-19 07:22:20 +0000

    now element_traits derive from base classes in Base/element-traits.h support for consecutive_tag added

--- GrAL/gral/complex2d/include/Gral/Grids/Complex2D/element-traits.h
+++ GrAL/gral/complex2d/include/Gral/Grids/Complex2D/element-traits.h
@@ -1,22 +1,14 @@
 #ifndef NMWR_GB_COMPLEX2D_ELEMENT_TRAITS_H
 #define NMWR_GB_COMPLEX2D_ELEMENT_TRAITS_H
 
-
-
 // $LICENSE
-
-
-//----------------------------------------------------------------
-//
-//  traits classes for enabling uniform treatment
-//  of different element types (vertex, edge etc)
-//  in template class grid_function<E,T>.
-//
-//----------------------------------------------------------------
 
 #include "Gral/Base/element-traits.h"
 #include "Gral/Grids/Complex2D/complex2d.h"
 
+/* specializations of element_traits for element
+   types of Complex2D
+*/
 
 template<class E> 
 struct hasher_for_complex2d {};
@@ -30,64 +22,30 @@ struct hasher_for_complex2d<Edge2D>
 struct hasher_for_complex2d<Cell2D> 
   : public hash_cell2d {};
 
-template<class E>
-struct element_traits_complex2d {};
 
-struct element_traits_complex2d<Vertex2D> {
-  typedef Complex2D                          grid_type;
-  typedef grid_types<Complex2D>              gt;
-  typedef  gt::Vertex                        element_type;
-  typedef  gt::VertexIterator                ElementIterator;
-  typedef  gt::vertex_handle                 handle_type;
+struct element_traits<Vertex2D> 
+ : public element_traits_vertex_base<Complex2D> 
+{
   typedef hasher_for_complex2d<element_type> hasher_type;
-  typedef vertex_type_tag                    element_type_tag;
-
-  static ElementIterator FirstElement(const Complex2D& g) { return g.FirstVertex();}
-  static element_type    handle2element(const Complex2D& g, handle_type h)
-    { return g.vertex(h);}
-  static handle_type handle(element_type const& e) { return e.handle();}
-  static size_t  size(const Complex2D& g) { return g.NumOfVertices();}
-
+  typedef consecutive_integer_tag<0>         consecutive_tag;
 };
 
 
-struct element_traits_complex2d<Edge2D> {
-  typedef Complex2D                          grid_type;
-  typedef grid_types<Complex2D>              gt;
-  typedef  gt::Edge                  element_type;
-  typedef  gt::EdgeIterator          ElementIterator;
-  typedef  gt::edge_handle           handle_type;
+template<>
+struct element_traits<Edge2D> 
+  : public element_traits_edge_base<Complex2D> 
+{
   typedef hasher_for_complex2d<element_type> hasher_type;
-  typedef edge2d_type_tag                    element_type_tag;
-  //  typedef facet1d_type_tag                     element_cotype_tag;
-  
-
-  static ElementIterator FirstElement(const Complex2D& g)  { return g.FirstEdge();}
-  static element_type    handle2element(const Complex2D& g, const handle_type& h)
-    { return g.edge(h);}
-  static handle_type handle(element_type const& e) { return e.handle();}
-  static size_t size(const Complex2D& g) { return g.NumOfEdges();}
 };
 
 
-struct element_traits_complex2d<Cell2D> {
-  typedef Complex2D                          grid_type;
-  typedef grid_types<Complex2D>              gt;
-  typedef  gt::Cell                  element_type;
-  typedef  gt::cell_handle           handle_type;
-  typedef  gt::CellIterator          ElementIterator;
+template<>
+struct element_traits<Cell2D>
+  : public element_traits_cell_base<Complex2D>
+{
   typedef hasher_for_complex2d<element_type> hasher_type;
-  typedef cell2d_type_tag                    element_type_tag;
-
-  static ElementIterator FirstElement(const Complex2D& g)  { return g.FirstCell();}
-  static element_type    handle2element(const Complex2D& g, handle_type h)
-    { return g.cell(h);}
-  static handle_type handle(element_type const& e) { return e.handle();}
-  static size_t size(const Complex2D& g) { return g.NumOfCells();}
+  typedef consecutive_integer_tag<0>         consecutive_tag;
 };
 
-struct element_traits<Vertex2D> : public element_traits_complex2d<Vertex2D> {};
-struct element_traits<Edge2D>   : public element_traits_complex2d<Edge2D>   {};
-struct element_traits<Cell2D>   : public element_traits_complex2d<Cell2D>   {};
 
 #endif
