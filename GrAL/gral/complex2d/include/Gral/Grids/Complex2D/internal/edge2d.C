934097836ff32edad27100beb1a35d6e96b21adf
berti <>
2001-09-19 07:28:53 +0000

    *** empty log message ***

--- GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge2d.C
+++ GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge2d.C
@@ -6,6 +6,11 @@
 
 #include "Gral/Grids/Complex2D/internal/edge2d.h"
 
+inline Edge2D::Edge2D(Complex2D const& cc)
+    { *this = *(cc.FirstEdge());} 
+inline Edge2D::Edge2D(Complex2D const& cc, edge_handle eh)
+    { *this = cc.edge(eh);} 
+
 inline VertexOnEdge2D_Iterator Edge2D::FirstVertex() const
 { return VertexOnEdge2D_Iterator(*this); }
 
