19ab98d7f66ce47c90efd18099a0e6e646e3bf55
berti <>
2001-09-19 07:26:00 +0000

    v1(), v2() added

--- GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge-on-cell2d-it.C
+++ GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge-on-cell2d-it.C
@@ -8,12 +8,24 @@ EdgeOnCell2D_Iterator::EdgeOnCell2D_Iterator(CellOnCell2D_Iterator const& nb)
 inline Edge2D
 EdgeOnCell2D_Iterator::operator*() const { return  Edge(*this); }
 
-inline EdgeOnCell2D_Iterator::Cell const&
-EdgeOnCell2D_Iterator::TheCell() const { return C;}
+inline  EdgeOnCell2D_Iterator::vertex_handle 
+EdgeOnCell2D_Iterator::v1() const { return TheCell().vertex(1,*this);}
 
-inline EdgeOnCell2D_Iterator::Cell
-EdgeOnCell2D_Iterator::OtherCell() const 
-{ return TheGrid().cell(TheGrid()._cells[C.handle()]._neighbours[lf]);}
+inline  EdgeOnCell2D_Iterator::vertex_handle 
+EdgeOnCell2D_Iterator::v2() const { return TheCell().vertex(2,*this);}
+
+inline  EdgeOnCell2D_Iterator::cell_handle   
+EdgeOnCell2D_Iterator::the_cell()   const { return C.handle();}
+  
+inline  EdgeOnCell2D_Iterator::cell_handle   
+EdgeOnCell2D_Iterator::other_cell() const 
+{ return TheGrid()._cells[C.handle()]._neighbours[lf];}
+
+inline   Cell2D const& 
+EdgeOnCell2D_Iterator::TheCell()   const { return C;}
+
+inline  Cell2D        
+EdgeOnCell2D_Iterator::OtherCell() const { return Cell(TheGrid(),other_cell()); }
 
 inline EdgeOnCell2D_Iterator
 EdgeOnCell2D_Iterator::CyclicSucc() const 
