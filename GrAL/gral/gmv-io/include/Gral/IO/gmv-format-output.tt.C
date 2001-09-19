c16a7db55369626a422eaaa874a96b0cbfd096de
berti <>
2001-09-19 15:14:37 +0000

    *** empty log message ***

---
+++ GrAL/gral/gmv-io/include/Gral/IO/gmv-format-output.tt.C
@@ -0,0 +1,68 @@
+#ifndef GRAL_GB_GMV_FMT_OUTPUT_TT
+#define GRAL_GB_GMV_FMT_OUTPUT_TT
+
+#include "Gral/IO/gmv-format-output.h"
+
+template<class GRID,class GEOM>
+void ConstructGrid(OstreamGMV3DFmt& Out, 
+		   GRID const& G,
+		   GEOM const& GEO)
+{
+  typedef OstreamGMV3DFmt GMV3D;
+  ostream& out = Out.Out();
+  typedef grid_types<GRID> gt;
+  typedef grid_types<GMV3D::archetype_type> gmv_agt;
+
+  out << "gmvinput ascii\n";
+  out << "nodev " << G.NumOfVertices() << '\n';
+
+  for(typename gt::VertexIterator v(G); ! v.IsDone(); ++v)
+    out << GEO.coord(*v) << '\n';
+
+  // map G's vertices to GMV numbers, starting from 1.
+  element_numbering<typename gt::Vertex> G2GMV(G,1);
+
+  // map archetypes of G to those of GMV
+  typedef partial_grid_morphism<typename gt::archetype_type,
+                                GMV3D::archetype_type>  
+    morphism_type;
+ 
+  bijective_mapping<typename gt::archetype_type const*,
+                    std::pair<morphism_type, int> > 
+    phi;
+
+  for(typename gt::ArchetypeIterator a = G.BeginArchetype();
+      a != G.EndArchetype(); ++a) {
+    bool found = false;
+    int cnt_a = 0; // could be GMV::archetype_handle
+    for(GMV3D::ArchetypeIterator a_gmv = Out.BeginArchetype();
+	a_gmv != Out.EndArchetype(); ++a_gmv, ++cnt_a) {
+      morphism_type phi_a(*a,*a_gmv);
+      found = construct_isomorphism(*a,*a_gmv, phi_a);
+      if(found) {
+	phi[&(*a)] = std::make_pair(phi_a,cnt_a);
+	break;
+      }
+    }
+    ENSURE(found, "sorry, no mapping to GMV cell type found!\n",1);
+    // should use the general cell feature of GMV.
+  }
+
+  // map cells
+  out << "cells " << G.NumOfCells() << "\n";
+  for(typename gt::CellIterator c(G); ! c.IsDone(); ++c) {
+    morphism_type const& phi_a = phi(&G.ArchetypeOf(*c)).first;
+    int                  cnt_a = phi(&G.ArchetypeOf(*c)).second;
+    GMV3D::archetype_type const& archetype_gmv(phi_a.ImgGrid());
+    out << Out.name(cnt_a) << " "
+	<< archetype_gmv.NumOfVertices() << " ";
+    // gmv-archetype -> G::archetype -> G::global -> GMV::global
+    for(gmv_agt::VertexIterator vc(archetype_gmv); !vc.IsDone(); ++vc)
+      out << G2GMV( (*c).V(phi_a.inverse()(*vc)))  << " ";
+    out << '\n';
+  }
+
+  out << "endgmv\n";
+}
+
+#endif
