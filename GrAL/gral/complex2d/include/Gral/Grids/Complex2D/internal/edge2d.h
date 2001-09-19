30f79cb2b1dc5d814cf952e530ace39df15e703b
berti <>
2001-09-19 07:28:30 +0000

    v1(), v2() added checking functions added new constructor syntax  (grid, handle) __STD:: replaced by std::

--- GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge2d.h
+++ GrAL/gral/complex2d/include/Gral/Grids/Complex2D/internal/edge2d.h
@@ -23,8 +23,12 @@ private:
 public:
   //------------- constructors -------------------------
   Edge2D() : _e() {}
+  Edge2D(Complex2D const& cc);
+  Edge2D(Complex2D const& cc, edge_handle eh);
+
   Edge2D(const Cell& C1, const Cell& C2)
     : _e(C1.find_local_facet(C2),C1) {}
+
 
   Edge2D(const EdgeOnCell2D_Iterator& e) : _e(e) {}
   Edge2D(const CellOnCell2D_Iterator& e) : _e(e) {}
@@ -44,16 +48,29 @@ public:
 
 
   //--------- edge-vertex operations ---------
-  inline Vertex V1() const;
-  inline Vertex V2() const;
+  inline vertex_handle v1() const { return _e.TheCell().vertex(1,_e);}
+  inline vertex_handle v2() const { return _e.TheCell().vertex(2,_e);}
+
+
+  inline Vertex V1() const { return Vertex(TheGrid(),v1());}
+  inline Vertex V2() const { return Vertex(TheGrid(),v2());}
+
+
 
   //--------- facet-cell operations ----------
-  inline Cell   C1() const;
-  inline Cell   C2() const;
-  
+
+  inline cell_handle c1() const { return _e.the_cell();}
+  inline cell_handle c2() const { return _e.other_cell();}
+
+  inline Cell   C1() const { return (_e.TheCell()  );}
+  inline Cell   C2() const { return (_e.OtherCell());}
+
+  //----------- switch operations -----------
 
   void FlipVertex(Vertex& v) const { v = ( v == V1() ? V2() : V1());}
   void FlipCell(Cell& c)     const { c = ( c == C1() ? C2() : C1());}
+  Vertex FlippedVertex(Vertex const& v) const { return ( v == V1() ? V2() : V1());}
+  Cell   FlippedCell  (Cell   const& c) const { return ( c == C1() ? C2() : C1());}
 
   //-------------- anchors -------------------
   
@@ -74,16 +91,7 @@ public:
 
 //---------------------------------------------------------
 
-inline Edge2D::Vertex Edge2D::V1() const 
-{ return Vertex(_e.TheCell().vertex(1,_e), TheGrid());}
 
-inline Edge2D::Vertex Edge2D::V2() const 
-{ return Vertex(_e.TheCell().vertex(2,_e), TheGrid());}
-
-inline Edge2D::Cell   Edge2D::C1() const 
-{ return (_e.TheCell());}
-inline Edge2D::Cell   Edge2D::C2() const 
-{ return _e.OtherCell();} 
 
 inline bool operator==(const Edge2D& lhs, const Edge2D& rhs)
 { // vertices anstelle von cells: sonst probleme am rand, speziell an ecken!
@@ -96,11 +104,11 @@ inline bool operator<(const Edge2D& s1, const Edge2D& s2)
 {
   // etwas effizienter mit ordered_pair(C(1),C(2))
   // evtl. gleich cnbit gleich so waehlen, dass reihenfolge stimmt?
-  typedef Edge2D::Vertex Vertex;
-  Vertex max1(__STD::max(s1.V1(),s1.V2()));
-  Vertex min1(__STD::min(s1.V1(),s1.V2()));
-  Vertex max2(__STD::max(s2.V1(),s2.V2()));
-  Vertex min2(__STD::min(s2.V1(),s2.V2()));
+  typedef Edge2D::vertex_handle vh;
+  vh max1(std::max(s1.v1(),s1.v2()));
+  vh min1(std::min(s1.v1(),s1.v2()));
+  vh max2(std::max(s2.v1(),s2.v2()));
+  vh min2(std::min(s2.v1(),s2.v2()));
   
   return ( (max1 < max2)  || ( (max1 == max2) && (min1 < min2)));
 }
