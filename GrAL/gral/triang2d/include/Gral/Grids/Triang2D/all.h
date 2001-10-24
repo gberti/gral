e402c688edc9b0bef2746e20b9e4beae2412520f
berti <>
2001-10-24 14:54:39 +0000

    *** empty log message ***

---
+++ GrAL/gral/triang2d/include/Gral/Grids/Triang2D/all.h
@@ -0,0 +1,10 @@
+#ifndef GRAL_GB_GRIDS_TRIANG2D_ALL_H
+#define GRAL_GB_GRIDS_TRIANG2D_ALL_H
+
+#include "Gral/Grids/Triang2D/triang2d.h"
+#include "Gral/Grids/Triang2D/grid-functions.h"
+#include "Gral/Grids/Triang2D/partial-grid-functions.h"
+#include "Gral/Grids/Triang2D/element-traits.h"
+#include "Gral/Grids/Triang2D/construct.h"
+
+#endif
