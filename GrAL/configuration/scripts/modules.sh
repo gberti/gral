9c0fe0ea04bc0f1d82c00b2acc65eaaab83f4d7f
berti <>
2001-10-30 09:53:28 +0000

    *** empty log message ***

--- GrAL/configuration/scripts/modules.sh
+++ GrAL/configuration/scripts/modules.sh
@@ -7,6 +7,6 @@ MODULES='configuration utilities external-control sequence geometry
          function-algebra graphics-device
          gral gral/base gral/complex2d gral/distributed gral/partitioning
          gral/cartesian2d gral/cartesian3d gral/complex3d gral/gmv-io
-         gral-testing';
+         gral/testing';
 
 echo $MODULES;
