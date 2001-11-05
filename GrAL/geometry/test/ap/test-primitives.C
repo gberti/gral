9b280d5b16a768b6ccf8941a1c2927fe1126f9ed
berti <>
2001-11-05 09:35:14 +0000

    clutter removed

--- GrAL/geometry/test/ap/test-primitives.C
+++ GrAL/geometry/test/ap/test-primitives.C
@@ -6,49 +6,9 @@
 
 #include <iostream>
 
-/*
-typedef double double3[3];
-
-template<>
-struct point_traits<double3>
-{
-  struct Ptype {
-    double3 X;
-    void operator=(const double3 Y) { X[0]=Y[0];X[1]=Y[1];X[2]=Y[2];}
-    operator double* () { return X;}
-  };
-
-  typedef double component_type;
-  static double3 orig;
-  static double3& Origin()         { return orig;}
-  static double3& Origin(unsigned) { return orig;}
-
-  static int LowerIndex(const double3 ) { return 0;}
-  static int UpperIndex(const double3) { return 2;}
-  static int Dim(const double3 ) { return 3;}
-  static int Dim()               { return 3;}
-  static double  x(double3 const& p) { return p[0];}
-  static double& x(double3      & p) { return p[0];}
-  static double  y(double3 const& p) { return p[1];}
-  static double& y(double3      & p) { return p[1];}
-  static double  z(double3 const& p) { return p[2];}
-  static double& z(double3      & p) { return p[2];}
-};
-
-double3 point_traits<double3>::orig = {0,0,0};
-
-template<>
-class point_traits<float[3]>
-{};
-*/
 
 
 int main() {
-
-  /*
- test_algebraic_primitives<double3> test_d3;
- test_d3.do_tests(std::cout);
-  */
 
  std::cout << "Testing general primitives: \n\n";
 	   
