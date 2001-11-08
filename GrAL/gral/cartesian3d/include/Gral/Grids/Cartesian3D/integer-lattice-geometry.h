c272cbef3f9cbfb0b3d50e3373d7c69a7a904b65
berti <>
2001-11-08 10:33:05 +0000

    *** empty log message ***

---
+++ GrAL/gral/cartesian3d/include/Gral/Grids/Cartesian3D/integer-lattice-geometry.h
@@ -0,0 +1,33 @@
+#ifndef GRAL_GB_GRIDS_CARTESIAN3D_LATTICE_GEOMETRY_H
+#define GRAL_GB_GRIDS_CARTESIAN3D_LATTICE_GEOMETRY_H
+
+// $LICENSE
+
+/*! A geometry living in the integer lattice \f$ \Z^3 \f$
+
+
+ */
+class lattice_geometry_cartesian3d {
+
+public:
+  typedef CartesianGrid3D             grid_type;
+  typedef grid_types<CartesianGrid3D> gt;
+  typedef gt::Vertex              Vertex; 
+
+  typedef grid_type::index_type   coord_type;
+  typedef int                     real;
+
+private:
+  grid_type const* g;
+
+public:
+  lattice_geometry_cartesian3d(grid_type const& gg)
+    : g(&gg)
+    {}
+
+  coord_type const& coord(gt::Vertex const& v) const { return v.index();}
+  
+  real volume(gt::Cell const&) const { return 1;}
+};
+
+#endif
