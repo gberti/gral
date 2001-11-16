8d44a12ffcf46de831e3265ea55bb7b9275ff014
berti <>
2001-11-16 21:13:15 +0000

    *** empty log message ***

--- GrAL/configuration/scripts/check-distribution-v0_2.sh
+++ GrAL/configuration/scripts/check-distribution-v0_2.sh
@@ -5,13 +5,13 @@ GRAL=GrAL-0.2
 GRALINSTALL=gral-v0.2
 GCC95=/usr/local/bin/g++
 cd ${TMP}
-rm -rf $GRAL
+rm -rf $GRAL ${GRALINSTALL}
 
 gzip -cd ${TMP}/gral-src.tar.gz | tar -xvf -
 cd $GRAL
 gzip -cd ${TMP}/gral-doc.tar.gz | tar -xvf -
 cp configuration/defs/mpi/success.defs configuration/defs/mpi/mpi.defs  
 gmake new     DEVELOP=develop CCC=${GCC95} CCNAME=gcc
-gmake install PREFIX=${TMP}/gral-v0.2
+gmake install PREFIX=${TMP}/${GRALINSTALL}
 gmake test    DEVELOP=user PREFIX=${TMP}/${GRALINSTALL} TARGET=new CCC=${GCC95} CCNAME=gcc
 gmake check   DEVELOP=user PREFIX=${TMP}/${GRALINSTALL} CCC=${GCC95} CCNAME=gcc
