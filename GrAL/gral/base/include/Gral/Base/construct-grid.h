c413da9f4b9f157ce4a3318facd948d8dfa4b592
berti <>
2002-02-13 14:30:47 +0000

    more generic ConstructGrid versions. Docs updated

--- GrAL/gral/base/include/Gral/Base/construct-grid.h
+++ GrAL/gral/base/include/Gral/Base/construct-grid.h
@@ -6,10 +6,13 @@
 
 
 //----------------------------------------------------------------
-/*! \brief copy-construct a grid destG from another 2D grid srcG
+/*! \defgroup copyoperations Generic grid copy operations
+    \brief copy-construct a grid destG from another 2D grid srcG
     \ingroup mutatingoperations
-  The template functions must be overloaded with respect to
-  the `destination' parameter G1.
+
+  The generic versions implemented in this module
+  are based on an overloaded master version for each destination
+  grid type \c G1.
 
   Template parameters:
    - G2: model of $GrAL Cell-VertexInputGridRange, i.e. must support
@@ -25,36 +28,10 @@
      - <tt> cell_handle1 operator()(cell_handle2) </tt> (read)
      - <tt> cell_handle1 operator[](cell_handle2) </tt> (write)
  
-  The following template functions have to be implemented
+  The following template function has to be implemented
   separately for each special type of G1:
 
   \code
-  template<class G1, class G2, class Geom2>
-  extern void 
-  ConstructGrid(G1         & destG,
-	        G2    const& srcG, 
-	        Geom2 const& srcGeom);
-
-  template<class G1, class G2, class Geom2, class VertexMap>
-  extern void 
-  ConstructGridV(G1            & destG, 
-	         G2       const& srcG,
-	         Geom2    const& srcGeom,
-	         VertexMap     & VCorrG2_G1); 
-
-  // VertexCorr  maps G2::vertex_handles to  G1::vertex_handles
-  // for transfer of grid-related information, e.g. grid-functions.
-
-  template<class G1, class G2, class Geom2, class VertexMap, class CellMap>
-  extern void 
-  ConstructGridVC(G1            & destG, 
-	   	  G2       const& srcG,
-		  Geom2    const& srcGeom,
-		  VertexMap     & VCorrG2_G1,
-		  CellMap       & CCorrG2_G1); 
-  // CellCorr  maps G2::cell_handles to  G1::vertex_handles
-  // for transfer of grid-related information, e.g. grid-functions.
-
 
   // construct without geometry
   template<class G1, class G2, class VertexMap, class CellMap>
@@ -71,7 +48,14 @@
 
 #include "Container/dummy-mapping.h"
 #include "Container/partial-mapping.h"
+#include "Geometry/point-traits.h"
 
+#include "Gral/Base/grid-morphism.h"
+
+
+/*!  Construct combinatorial grid, without mappings
+   \ingroup copyoperations
+ */
 template<class G1, class G2>
 inline void 
 ConstructGrid0(G1      & destG, 
@@ -84,17 +68,22 @@ ConstructGrid0(G1      & destG,
   typedef typename grid_types<G1>::vertex_handle vertex_handle_1;
 
   // here we are not interested in a cell correspondance
-  dummy_mapping<cell_handle_2,cell_handle_1>       CellCorr;
+  dummy_mapping<cell_handle_2,cell_handle_1>       c_corr;
+
   // VertexCorr must be a functional type, because it is used.
-  partial_mapping<vertex_handle_2,vertex_handle_1> VertexCorr;
-  ConstructGrid0(destG,srcG,VertexCorr,CellCorr);
+  vertex_morphism<G_SRC, G_DEST> v_corr(G_src, G_dest);
+
+  ConstructGrid0(destG,srcG,v_corr,c_corr);
 }
 
 
+/*!  Construct combinatorial grid, return vertex mapping
+   \ingroup copyoperations
+ */
 template<class G1, class G2, class V_CORR>
 inline void 
 ConstructGrid0(G1      & destG, 
-	       G2 const&  srcG,
+	       G2 const& srcG,
 	       V_CORR  & VertexCorr)
 {
   typedef grid_types<G2>                         gt2;
@@ -103,8 +92,87 @@ ConstructGrid0(G1      & destG,
 
   // here we are not interested in a cell correspondance
   dummy_mapping<cell_handle_2,cell_handle_1>       CellCorr;
+
+  // provided by the implementation of  G1
   ConstructGrid0(destG,srcG,VertexCorr,CellCorr);
 }
 
 
+/*!  Construct combinatorial & generic grid, with vertex and cell  mappings
+     \ingroup copyoperations
+
+     This is a default version; it must be enabled explicitly by
+     saying
+     \code
+     template<class GEOM_DEST,
+             class G_SRC,  class GEOM_SRC,
+             class VCORR,  class CCORR>
+      void ConstructGridVC (MyGrid        & G_dest,
+			    GEOM_DEST     & Geom_dest,
+			    G_SRC    const& G_src,
+			    GEOM_SRC const& Geom_src,
+			    VCORR         & v_corr,
+			    CCORR         & c_corr)
+      { ConstructGridVC_generic(G_dest, Geom_dest, ...);}
+     \endcode
+     for each type \c MyGrid.
+
+     This version just copies vertex coordinates from \c Geom_src
+     to \c Geom_dest. For non-linear geometries, it has to be specialized.
+ */
+
+template<class G_DEST, class GEOM_DEST,
+         class G_SRC,  class GEOM_SRC,
+         class VCORR,  class CCORR>
+inline
+void ConstructGridVC_generic (G_DEST        & G_dest,
+			      GEOM_DEST     & Geom_dest,
+			      G_SRC    const& G_src,
+			      GEOM_SRC const& Geom_src,
+			      VCORR         & v_corr,
+			      CCORR         & c_corr)
+{ 
+  // provided by the implementation of G_DEST
+  ConstructGrid0(G_dest, G_src, v_corr, c_corr); 
+  Geom_dest.set_grid(G_dest);
+  typedef grid_types<G_SRC>  srcgt;
+  typedef grid_types<G_DEST> destgt;
+  for(typename srcgt::VertexIterator vs(G_src); ! vs.IsDone(); ++vs) {
+    assign_point(Geom_dest.coord(typename destgt::Vertex(G_dest, v_corr(vs.handle()))),
+		 Geom_src .coord(*vs));
+  }
+}
+
+template<class G_DEST, class GEOM_DEST,
+         class G_SRC,  class GEOM_SRC,
+         class VCORR>
+inline
+void ConstructGridV(G_DEST        & G_dest,
+		    GEOM_DEST     & Geom_dest,
+		    G_SRC    const& G_src,
+		    GEOM_SRC const& Geom_src,
+		    VCORR         & v_corr)
+{ 
+  typedef grid_types<G_SRC>  srcgt;
+  typedef grid_types<G_DEST> destgt;
+  dummy_mapping<typename srcgt ::cell_handle,
+                typename destgt::cell_handle> c_corr;
+  ConstructGridVC_generic(G_dest,Geom_dest, G_src, Geom_src, v_corr, c_corr);
+}
+
+
+template<class G_DEST, class GEOM_DEST,
+         class G_SRC,  class GEOM_SRC>
+inline
+void ConstructGrid(G_DEST        & G_dest,
+		   GEOM_DEST     & Geom_dest,
+		   G_SRC    const& G_src,
+		   GEOM_SRC const& Geom_src)
+{ 
+  vertex_morphism<G_SRC, G_DEST> v_corr(G_src, G_dest);
+  ConstructGridV(G_dest, Geom_dest, G_src, Geom_src, v_corr);
+}
+
+
 #endif
+
