54c7f360155a395f3332a9dfbc9d2a2d408e8d9f
berti <>
2002-02-15 10:56:43 +0000

    adapted to success/ mpich

--- GrAL/configuration/scripts/check-all-cvswork.sh
+++ GrAL/configuration/scripts/check-all-cvswork.sh
@@ -1,17 +1,30 @@
 #! /bin/bash
 
+source ${HOME}/.bashrc;
+GCC95=/usr/local/bin/g++
+CCC=${GCC95}
+CCNAME=gcc
+export MPICH_CCC=${CCC}
+export MPICH_CCLINKER=${CCC}
+export LD_LIBARY_PATH=${HOME}/lib/:${LD_LIBRARY_PATH}
 
 cvswork=${CVSWORK-${HOME}/CVS-work}
 MODULES=`$cvswork/configuration/scripts/modules.sh`;
 #MODULES=sequence
+echo $cvswork
+
 cd $cvswork
 for i in ${MODULES}
 do
+ pwd;
  cd $i;
- gmake clean 
- gmake new   
- gmake test TARGET=new-check-clean
- gmake clean 
+ gmake new CCC=${CCC} CCNAME=${CCNAME}  
+ cd $cvswork
+done;
+for i in ${MODULES}
+do
+ cd $i;
+ gmake test-check-clean TARGET=new CCC=${CCC} CCNAME=${CCNAME}
  cd $cvswork
 done;
 
