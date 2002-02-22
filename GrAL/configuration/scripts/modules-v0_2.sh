dbc18529cc9ec54c6e6b56f85e49c135c969e3eb
berti <>
2002-02-22 12:31:35 +0000

    added function-algebra and graphics-device

--- GrAL/configuration/scripts/modules-v0_2.sh
+++ GrAL/configuration/scripts/modules-v0_2.sh
@@ -4,7 +4,7 @@
 # of GRaL version 0.2
 
 MODULES='configuration utilities external-control
-         sequence geometry 
+         sequence geometry function-algebra graphics-device
          gral gral/base gral/complex2d gral/distributed gral/partitioning
          gral/cartesian2d gral/cartesian3d gral/complex3d gral/gmv-io
          gral/testing gral/triang2d gral/geomview-io'
