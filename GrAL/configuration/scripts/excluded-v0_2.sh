e1bb748385b8650117249d3e3c0ecf4bbe1550a9
berti <>
2002-02-25 09:52:44 +0000

    removed function-algebra and graphics-device, and gral/tools

--- GrAL/configuration/scripts/excluded-v0_2.sh
+++ GrAL/configuration/scripts/excluded-v0_2.sh
@@ -3,7 +3,7 @@
 # central place to collect the names of all modules
 # of GRaL version 0.2
 
-MODULES='gral/measurement'
+MODULES='function-algebra graphics-device gral/measurement gral/tools'
 
 
 echo $MODULES;
