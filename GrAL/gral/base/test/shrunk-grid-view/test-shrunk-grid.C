e2d92bd364f5aefacc61c9ba0a238718614b74df
berti <>
2002-06-27 15:33:19 +0000

    testing grid function adapters

--- GrAL/gral/base/test/shrunk-grid-view/test-shrunk-grid.C
+++ GrAL/gral/base/test/shrunk-grid-view/test-shrunk-grid.C
@@ -14,8 +14,9 @@
 int main() {
  
   // create Cartesian grid & geometry
-  typedef CartesianGrid3D reg_grid_type; 
-  typedef coordN<3> coord_type;
+  typedef CartesianGrid3D            reg_grid_type;
+  typedef grid_types<reg_grid_type>  rgt; 
+  typedef coordN<3>                  coord_type;
   typedef std::identity<coord_type>  mapping_type;
   typedef mapped_geometry_cartesian3d<mapping_type> reg_geom_type;
 
@@ -43,6 +44,18 @@ int main() {
   Complex3D UG;
   simple_geometry<Complex3D, coord_type> GeomUG; //(UG);
   ConstructGrid(UG, GeomUG, ShrunkG, ShrunkGeom);
+
+  // instantiate grid function adapters for vertex and cell
+  typedef grid_function<rgt::Vertex, int> gfR_v_int_type;
+  gfR_v_int_type gfRv(R,0);
+  sgv::grid_function_view<gfR_v_int_type> gf_v_Shrink(ShrunkG,gfRv);
+  typedef grid_function<rgt::Cell, int> gfR_c_int_type;
+  gfR_c_int_type gfRc(R,0);
+  sgv::grid_function_view<gfR_c_int_type> gf_c_Shrink(ShrunkG,gfRc);
+
+  // can use this only for gf's which can be instantiated
+  //test_grid_functions(gfShrink,cout);
+
   
   // output ShrunkG to files
 
@@ -52,7 +65,19 @@ int main() {
   }
 
   {
-    OstreamGMV3DFmt Out2("shrunk2.gmv.out");
-    ConstructGrid(Out2, ShrunkG, ShrunkGeom);
+    OstreamGMV3DFmt Out("shrunk2.gmv.out");
+    ConstructGrid(Out, ShrunkG, ShrunkGeom);
   }
+
+  {
+    namespace hl = heterogeneous_list;
+    hl::BEGIN B;
+    OstreamGMV3DFmt Out("shrunk3.gmv.out");
+    ConstructGrid_GF(Out, ShrunkG, ShrunkGeom, 
+		     (B, 
+		      Out.pair("gfShrinkV",gf_v_Shrink),
+		      Out.pair("gfShrinkC",gf_c_Shrink)));
+  }
+
+
 }
