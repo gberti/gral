2d133396a0b3e7db9d12de7b16c50b2413eea320
berti <>
2002-08-07 08:21:17 +0000

    support gcc 3.1, icc 6.0. hash<> template is available in namespace STLEXT (macro)

--- GrAL/sequence/include/Container/my-hash-map.h
+++ GrAL/sequence/include/Container/my-hash-map.h
@@ -3,17 +3,58 @@
 
 // $LICENSE
 
-
 #include "Config/compiler-config.h"
 
-#ifdef __KCC
-// namespace nmwr_kai_hash {
-#include "KCC/hash_map.h"
-//}
-// using nmwr_kai_hash::hash_map;
+// define STDEXT as the namespace where hash<> templates live.
+// put hash_map into global namespace
+// (intel defines hash<> in std::, but our substitute for their hash_map
+// cannot be in std::)
 
-#else
-#include "hash_map.h"
+// KCC currently not supported
+#if defined __KCC
+//#include "KCC/hash_map.h"
+#error "hash_map for KCC is currently not supported!"
+
+#elif defined INTEL_CC
+// Intel compiler uses Dinkumware STL
+// make a SGI-STL like wrapper
+#include "Container/intel-hash.h"
+using my_hash_intel::hash_map;
+//namespace ext = std;
+#define STDEXT std
+
+#elif defined __GNUC__ && __GNUC__ >= 3 && __GNUC_MINOR__>= 1
+// use SGI-like hash_map
+#include <ext/hash_map>
+using __gnu_cxx::hash_map;
+#define STDEXT __gnu_cxx
+namespace STDEXT {
+
+  template<class T>
+  struct hash<T*> {
+    typedef T* key_type;
+    typedef T* argument_type;
+    typedef size_t result_type;
+    size_t operator()(T* t) const { return reinterpret_cast<unsigned>(t);}
+  };
+}
+
+#else 
+#include <hash_map>
+using std::hash_map;
+#define STDEXT std 
+
+namespace STDEXT {
+
+  template<class T>
+  struct hash<T*> {
+    typedef T* key_type;
+    typedef T* argument_type;
+    typedef size_t result_type;
+    size_t operator()(T* t) const { return reinterpret_cast<unsigned>(t);}
+  };
+}
+
 #endif
 
 
