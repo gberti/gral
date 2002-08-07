8945b1ca198b850cbfd5c1f7323ed7a0d922ffa3
berti <>
2002-08-07 08:14:24 +0000

    use sstream when possible (depends on GRAL_HAS_SSTREAM)

--- GrAL/utilities/include/Utility/as-string.h
+++ GrAL/utilities/include/Utility/as-string.h
@@ -5,14 +5,22 @@
 
 // $LICENSE
 
-
-
-#include <strstream.h>
 #include <string>
+
+#ifdef GRAL_HAS_SSTREAM
+#include <sstream>
+#else
+#include <strstream.h>
+#endif
 
 template<class T>
 inline std::string as_string(const T& t)
 {
+#ifdef GRAL_HAS_SSTREAM
+  std::ostringstream s;
+  s << t;
+  return s.str();
+#else
   std::strstream s;
   s << t << std::ends;
   int n = strlen(s.str());
@@ -20,6 +28,7 @@ inline std::string as_string(const T& t)
   strcpy(copy,s.str());
  
   return std::string(copy);
+#endif
 }
 
 #endif
