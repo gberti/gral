bb2c0cbde319222814ee96eb20f7f72c2d3fff6c
berti <>
2002-08-08 09:14:46 +0000

    modified cvsstat to take cvs program as argument. Can be used to employ wrapper for cvs using different CVS_RSH to avoid password query.

--- GrAL/configuration/scripts/check-mods.sh
+++ GrAL/configuration/scripts/check-mods.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-
+CVSPG=${HOME}/bin/cvscb
 cvswork=${CVSWORK-${HOME}/CVS-work}
 MODULES=`$cvswork/configuration/scripts/modules-toplevel.sh`;
 
 cd ${HOME}/CVS-work
 for i in ${MODULES}
 do
- echo $i;
+ echo ">>>>>>  $i ";
  cd $i; 
- cvsstat -mods;
+ cvsstat -cvs ${CVSPG} -mods;
  cd ${HOME}/CVS-work;
 done;
 
