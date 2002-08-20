094c02d376d2a3562cce1da90f6112e0a7364e4e
berti <>
2002-08-20 13:11:18 +0000

    *** empty log message ***

---
+++ GrAL/gral/base/include/Gral/Base/facet-on-cell-function.h
@@ -0,0 +1,55 @@
+#ifndef GRAL_GB_FACET_ON_CELL_FUNCTION_H
+#define GRAL_GB_FACET_ON_CELL_FUNCTION_H
+
+// $LICENSE
+
+#include "Gral/Base/grid-functions.h"
+
+
+/*! \brief Associate data which each cell-facet incidence
+    \ingroup gridfunctions
+    
+    This class can be used e.g. for cell-cell neighborhood calculations.
+    (see cell_neighbor_search)
+    \ingroup gridfunctions
+   
+
+    \todo The implementation contains some overhead in terms of storage:
+    The grid function on archetype cells (i.e. cell facets) does not
+    need a grid pointer.
+    \todo Similar classes could be designed for cell-vertex and cell-edge
+    incidences.
+ */
+template<class GRID, class T>
+class facet_on_cell_function 
+{
+  typedef GRID                  grid_type;
+  typedef grid_types<grid_type> gt;
+  typedef typename gt::Cell                Cell;
+  typedef typename gt::archgt::Cell        ArchCell;
+  typedef typename gt::FacetOnCellIterator FacetOnCellIterator;
+
+ grid_function<Cell, grid_function<ArchCell,T> > gf;
+
+public:
+  facet_on_cell_function(grid_type const& g) :  gf(g) 
+    {
+      for(typename gt::CellIterator c(gf.TheGrid()); ! c.IsDone(); ++c)
+	gf[*c].set_grid((*c).TheArchetype());
+    }
+  facet_on_cell_function(grid_type const& g, T const& t) :  gf(g) 
+    {
+      for(typename gt::CellIterator c(gf.TheGrid()); ! c.IsDone(); ++c)
+	gf[*c] = grid_function<ArchCell,T>((*c).TheArchetype(),t);
+    }
+
+  T const& operator()(FacetOnCellIterator const& fc) const
+    { return gf(fc.TheCell())(fc.ArchetypeCell());}
+  T      & operator[](FacetOnCellIterator const& fc) 
+    { return gf[fc.TheCell()][fc.ArchetypeCell()];}
+
+  grid_type const& TheGrid() const { return gf.TheGrid();}
+};
+
+
+#endif
