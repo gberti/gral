24f46fa802ba87cf971393840236270cf428ebef
berti <>
2002-08-20 13:52:39 +0000

    *** empty log message ***

--- GrAL/gral/tools/converter/complex2d2gmv.C
+++ GrAL/gral/tools/converter/complex2d2gmv.C
@@ -4,10 +4,14 @@
 #include "IO/control-device.h"
 
 int main(int argc, char* argv[]) {
+  using namespace std;
+
+  cout << "------ Starting complex2d2gmv -------\n" << endl;
 
   ControlDevice Ctrl = 
     GetCommandlineAndFileControlDevice(argc,argv,"complex2d2gmv.in","main");
-
+  string h = "complex2d2gmv - converted mesh in complex2d format to gmv format\n";
+  h += "Usage: complex2d2gmv -g <complex2d> -o <gmv>  -d <space dim = 2> -off <offset = 0>\n";
   string grid_in;
   RegisterAt(Ctrl, "-g", grid_in);
   string grid_out;
@@ -16,11 +20,21 @@ int main(int argc, char* argv[]) {
   RegisterAt(Ctrl, "-d", sdim);
   unsigned offset = 0;
   RegisterAt(Ctrl, "-off", offset);
+
+  Mutator* helpmsg = new MessageOnReadMutator(cerr,h);
+  Ctrl.add("-h",     helpmsg);
+  Ctrl.add("--help", helpmsg);
+  Ctrl.add("-?",     helpmsg);
+
+
   Ctrl.update();
 
   IstreamComplex2DFmt In(grid_in, offset);
   In.set_spacedim(sdim);
+
   OstreamGMV2DFmt     Out(grid_out);
 
+  cout << "Reading grid from file " << grid_in << " ... " << flush;
   ConstructGrid(Out, In, In);
+  cout << "Written to gmv file " << grid_out << endl;
 }
