1bc56163901c65745b18582068248649f7ea47a1
berti <>
2002-11-26 13:22:28 +0000

    *** empty log message ***

---
+++ GrAL/gral/cartesian2d/test/gridfunctions/test-cartesian2d-gridfunctions.C
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+#include "Gral/Grids/Cartesian2D/all.h"
+
+#include "Gral/Test/test-grid-functions.h"
+#include "Gral/Test/test-partial-grid-functions.h"
+
+#include "Utility/pre-post-conditions.h"
+
+int main() {
+  using namespace std;
+
+  ostream& out = cout; // new ofstream("gridfunctions.out");
+
+  typedef grid_types<CartesianGrid2D> gt;
+
+  int NX = 3, NY = 4;
+  CartesianGrid2D R(NX,NY);
+
+
+  out << "CartesianGrid2D (" << NX << ',' << NY << ")\n"
+      << R.NumOfCells()    << " cells\n"
+      << R.NumOfEdges()    << " edges\n"
+      << R.NumOfVertices() << " vertices\n";
+
+  test_grid_functions<gt::Vertex>        (R,out);
+  test_partial_grid_functions<gt::Vertex>(R,out);
+  test_grid_functions<gt::Edge>        (R,out);
+  test_partial_grid_functions<gt::Edge>(R,out);
+  test_grid_functions<gt::Cell>        (R,out);
+  test_partial_grid_functions<gt::Cell>(R,out);
+
+  out << endl;
+
+  return 0;
+}
