b9e21c515dda22219a4dbcf671f10830705bf312
berti <>
2002-11-29 19:42:23 +0000

    test grid functions and partial grid functions for all element types.

--- GrAL/gral/cartesian3d/test/grid-functions/test-cart-gf.C
+++ GrAL/gral/cartesian3d/test/grid-functions/test-cart-gf.C
@@ -1,102 +1,52 @@
 #include <fstream>
 
-#include "Gral/Grids/Cartesian3D/cartesian3d.h"
-#include "Gral/Grids/Cartesian3D/grid-functions.h"
+#include "Gral/Grids/Cartesian3D/all.h"
 
+#include "Gral/Test/test-grid-functions.h"
+#include "Gral/Test/test-partial-grid-functions.h"
 #include "Utility/pre-post-conditions.h"
 
 int main() {
   using namespace std;
+  using namespace cartesian3d;
 
   typedef CartesianGrid3D grid_type;
   typedef grid_types<grid_type> gt;
  
-  ofstream* out = new ofstream("grid-functions.out");
+  ostream& out = cout;
 
+  // check empty grid
   grid_type EmptyG; 
   REQUIRE_ALWAYS(EmptyG.NumOfVertices() == 0,
 		 " NumOfVertices() == " << EmptyG.NumOfVertices(), 1);
+  REQUIRE_ALWAYS(EmptyG.NumOfEdges() == 0,
+		 " NumOfEdges() == " << EmptyG.NumOfEdges(), 1);
+  REQUIRE_ALWAYS(EmptyG.NumOfFacets() == 0,
+		 " NumOfFacets() == " << EmptyG.NumOfFacets(), 1);
   REQUIRE_ALWAYS(EmptyG.NumOfCells() == 0,
 		 " NumOfCells() == " << EmptyG.NumOfCells(), 1);
 
+  test_grid_functions<gt::Vertex>(EmptyG,out);
+  test_grid_functions<gt::Edge  >(EmptyG,out);
+  test_grid_functions<gt::Facet >(EmptyG,out);
+  test_grid_functions<gt::Cell  >(EmptyG,out);
+  test_partial_grid_functions<gt::Vertex>(EmptyG,out);
+  test_partial_grid_functions<gt::Edge  >(EmptyG,out);
+  test_partial_grid_functions<gt::Facet >(EmptyG,out);
+  test_partial_grid_functions<gt::Cell  >(EmptyG,out);
+
+
   grid_type G(3,3,3);
 
-  grid_function<gt::Vertex,gt::vertex_handle> gfv(G,1);
-  grid_function<gt::Cell,  gt::cell_handle  > gfc(G,1);
+  // check grid functions
+  test_grid_functions<gt::Vertex>(G,out);
+  test_grid_functions<gt::Edge  >(G,out);
+  test_grid_functions<gt::Facet >(G,out);
+  test_grid_functions<gt::Cell  >(G,out);
+  test_partial_grid_functions<gt::Vertex>(G,out);
+  test_partial_grid_functions<gt::Edge  >(G,out);
+  test_partial_grid_functions<gt::Facet >(G,out);
+  test_partial_grid_functions<gt::Cell  >(G,out);
 
-  for(gt::VertexIterator v(G); ! v.IsDone(); ++v)
-    gfv[*v] = v.handle();
-
-  typedef grid_function<gt::Vertex,gt::vertex_handle>::const_iterator gfv_iter;
-  for(gfv_iter vi = gfv.begin(); vi != gfv.end(); ++vi)
-    *out << *vi << '\n';
-  *out << endl;
-
-  for(gt::CellIterator   c(G); ! c.IsDone(); ++c)
-    gfc[*c] = c.handle();
-  typedef grid_function<gt::Cell,gt::cell_handle>::const_iterator gfc_iter;
-  for(gfc_iter ci = gfc.begin(); ci != gfc.end(); ++ci)
-    *out << *ci << '\n';
-  *out << endl;
-
-
-
-  grid_function<gt::Vertex,gt::vertex_handle> gfv_copy1(gfv);
-  REQUIRE_ALWAYS(gfv_copy1.size() == gfv_copy1.TheGrid().NumOfVertices(), 
-		 "  size() = " << gfv_copy1.size() 
-		 << " NumOfVertices() =  "  
-		 << gfv_copy1.TheGrid().NumOfVertices() , 1);
- 
-  grid_function<gt::Vertex,gt::vertex_handle> gfv_copy2(G);
-  REQUIRE_ALWAYS(gfv_copy2.size() == gfv_copy2.TheGrid().NumOfVertices(), 
-		 "  size() = " << gfv_copy2.size() 
-		 << " NumOfVertices() =  "  
-		 << gfv_copy2.TheGrid().NumOfVertices() , 1);
-  gfv_copy2 = gfv;
-
-  grid_function<gt::Vertex,gt::vertex_handle> gfv_copy3;
-  gfv_copy3 = gfv;
-  REQUIRE_ALWAYS(gfv_copy3.size() == gfv_copy3.TheGrid().NumOfVertices(), 
-		 "  size() = " << gfv_copy3.size() 
-		 << " NumOfVertices() =  "  
-		 << gfv_copy3.TheGrid().NumOfVertices() , 1);
-
-
-  for(gt::VertexIterator v(G); ! v.IsDone(); ++v) {
-    REQUIRE_ALWAYS( (gfv(*v) == gfv_copy1(*v)), "v = " << v.handle(),1);
-    REQUIRE_ALWAYS( (gfv(*v) == gfv_copy2(*v)), "v = " << v.handle(),1);
-    REQUIRE_ALWAYS( (gfv(*v) == gfv_copy3(*v)), "v = " << v.handle(),1);
-  } 
-
-
-  // test cell grid functions
-  grid_function<gt::Cell,gt::cell_handle> gfc_copy1(gfc);
-  REQUIRE_ALWAYS(gfc_copy1.size() == gfc_copy1.TheGrid().NumOfCells(), 
-		 "  size() = " << gfc_copy1.size() 
-		 << " NumOfCells() =  "  
-		 << gfc_copy1.TheGrid().NumOfCells() , 1);
- 
-  grid_function<gt::Cell,gt::cell_handle> gfc_copy2(G);
-  REQUIRE_ALWAYS(gfc_copy2.size() == gfc_copy2.TheGrid().NumOfCells(), 
-		 "  size() = " << gfc_copy2.size() 
-		 << " NumOfCells() =  "  
-		 << gfc_copy2.TheGrid().NumOfCells() , 1);
-  gfc_copy2 = gfc;
-
-  grid_function<gt::Cell,gt::cell_handle> gfc_copy3;
-  gfc_copy3 = gfc;
-  REQUIRE_ALWAYS(gfc_copy3.size() == gfc_copy3.TheGrid().NumOfCells(), 
-		 "  size() = " << gfc_copy3.size() 
-		 << " NumOfCells() =  "  
-		 << gfc_copy3.TheGrid().NumOfCells() , 1);
-
-
-  for(gt::CellIterator c(G); ! c.IsDone(); ++c) {
-    REQUIRE_ALWAYS( (gfc(*c) == gfc_copy1(*c)), "c = " << c.handle(),1);
-    REQUIRE_ALWAYS( (gfc(*c) == gfc_copy2(*c)), "c = " << c.handle(),1);
-    REQUIRE_ALWAYS( (gfc(*c) == gfc_copy3(*c)), "c = " << c.handle(),1);
-  } 
-
-
-  delete out;
+  return 0;
 }
