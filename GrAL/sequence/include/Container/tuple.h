f6d1cb8689bb4efd5f60a1bc2e9827547b7fab11
berti <>
2002-12-02 12:07:26 +0000

    added specialization for N=2

--- GrAL/sequence/include/Container/tuple.h
+++ GrAL/sequence/include/Container/tuple.h
@@ -4,15 +4,9 @@
 
 // $LICENSE
 
+#include <iostream>
+#include "Utility/pre-post-conditions.h"
 
-/*! \brief simple fixed-size container  with STL-style  begin() / end()
-    \ingroup containers
-
-    Tuple<T,N> is a model of STL Random Access Container (except reverse iterators).
- 
-    \todo
-    Add reverse iterators.
-*/
 
 template<class T, unsigned N>
 class tuple_base {
@@ -47,6 +41,15 @@ private:
 };
 
 
+/*! \brief simple fixed-size container  with STL-style  begin() / end()
+    \ingroup containers
+
+    tuple<T,N> is a model of STL Random Access Container (except reverse iterators).
+ 
+    \todo
+    Add reverse iterators.
+*/
+
 template<class T, unsigned N>
 class tuple : public tuple_base<T,N> {
   typedef tuple_base<T,N> base;
@@ -77,6 +80,33 @@ class tuple : public tuple_base<T,N> {
 };
 
 template<class T>
+class tuple<T,2> : public tuple_base<T,2> {
+  typedef tuple_base<T,2> base;
+ public:
+  // should not be necessary ... but problems with gcc 2.96
+  typedef typename base::iterator       iterator;
+  typedef typename base::const_iterator const_iterator;
+  typedef typename base::c_array        c_array;
+
+  tuple() {}
+  tuple(const T& t)        { for(iterator i = begin(); i != end(); ++i)  *i = t;}
+  tuple(const c_array& rs) { for(iterator i = begin(); i != end(); ++i)  *i = rs[i-begin()];}
+  tuple(const_iterator b, const_iterator /*e */)
+  { 
+    for(iterator it = begin(); it != end(); ++it,++b) 
+      *it = *b; 
+  }
+  template<class It>
+  tuple(It b, It e) {
+    for(iterator it = begin(); it != end(); ++it,++b) 
+      *it = *b; 
+    REQUIRE( (b == e), "invalid range in constructor!\n",1);
+  }
+
+  tuple(T const& t1, T const& t2) { X[0] = t1; X[1] = t2;}
+};
+
+template<class T>
 class tuple<T,3> : public tuple_base<T,3> {
   typedef tuple_base<T,3> base;
  public:
@@ -90,8 +120,6 @@ class tuple<T,3> : public tuple_base<T,3> {
   tuple(const c_array& rs) { for(iterator i = begin(); i != end(); ++i)  *i = rs[i-begin()];}
   tuple(const_iterator b, const_iterator /*e */)
   { 
-    // REQUIRE(((e-b) == N),
-    // 	       "tuple<T," << N << ">::tuple(b,e) :  e-b = " << e-b << "!" ,1)
     for(iterator it = begin(); it != end(); ++it,++b) 
       *it = *b; 
   }
@@ -127,6 +155,25 @@ tuple<T,N> operator-(tuple<T,N> const& lhs, tuple<T,N> const& rhs)
   for(unsigned k = 0; k < N; ++k)
     tmp[k] -= rhs[k];
   return tmp;
+}
+
+
+template<class T, unsigned N>
+inline
+std::ostream& operator<<(std::ostream& out, tuple<T,N> const& t)
+{
+  for(unsigned i = 0; i < N; ++i)
+    out << t[i] << ' ';
+  return out;
+}
+
+template<class T, unsigned N>
+inline
+std::istream& operator>>(std::istream& in, tuple<T,N> & t)
+{
+  for(unsigned i = 0; i < N; ++i)
+    in >> t[i];
+  return in;
 }
 
 #endif
