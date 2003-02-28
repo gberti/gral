4588f6351dda65fb58479c65f97c9ecae3a378c1
berti <>
2003-02-28 17:17:55 +0000

    *** empty log message ***

---
+++ GrAL/gral/complexnd/include/Gral/Grids/ComplexND/all.h
@@ -0,0 +1,10 @@
+#ifndef GRAL_GB_COMPLEXND_ALL_H
+#define GRAL_GB_COMPLEXND_ALL_H
+
+// $LICENSE_NEC
+
+#include "Gral/Grids/ComplexND/complexnd.h"
+#include "Gral/Grids/ComplexND/construct.C"
+
+
+#endif
