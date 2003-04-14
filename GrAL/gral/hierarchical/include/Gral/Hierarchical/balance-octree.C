a2d0e1de35518b7feb20ceb4ec288e0093fab9d2
berti <>
2003-04-14 17:51:49 +0000

    *** empty log message ***

---
+++ GrAL/gral/hierarchical/include/Gral/Hierarchical/balance-octree.C
@@ -0,0 +1,47 @@
+#ifndef GRAL_GB_BALANCE_OCTREE_C
+#define GRAL_GB_BALANCE_OCTREE_C
+
+// $LICENSE_NEC 
+
+#include "Gral/Hierarchical/refine-octree-cell-towards.C"
+
+/*! \addtogroup octreealgorithms Octree Algorithms
+   
+    Algorithms operating on octrees.     
+*/
+
+/*! \brief Balance an octree
+
+    \author Guntram Berti    
+    \ingroup  octreealgorithms
+
+
+*/
+
+template<class OCTREE>
+void balance_octree(OCTREE& Tree, unsigned balance)
+{
+  typedef OCTREE oct;
+  typename oct::level_handle k = Tree.finest_level();
+  while(k >= Tree.coarsest_level()) {
+    for(typename oct::ActiveLevelCellIterator c = Tree.ActiveRange(k)->FirstCell(); ! c.IsDone(); ++c) {
+      typename oct::hier_cell_type hc(Tree.TheHierGrid(), *c, k);
+      if(Tree.isLeaf(hc)) {
+	typedef typename oct::flatgt flatgt;
+	// FIXME: need also neighbors across edges!
+	for(typename flatgt::CellOnCellIterator nc(*c); ! nc.IsDone(); ++nc) {
+	  typename oct::hier_cell_type hnc(Tree.TheHierGrid(), *nc, k);
+	  if(! Tree.isActive(hnc)) {
+	    typename oct::hier_cell_type pnc = Tree.leaf_ancestor(hnc);
+	    int level_diff = hnc.level() - pnc.level();
+	    if(level_diff > balance)
+	      refine_towards(Tree, pnc, hnc, level_diff - balance);
+	  }
+	}
+      }
+    }
+    --k;
+  } // while(k >= Tree.coarsest_level())
+}
+
+#endif
---
+++ GrAL/gral/hierarchical/include/Gral/Hierarchical/refine-octree-cell-towards.C
@@ -0,0 +1,55 @@
+#ifndef GRAL_GB_HIERARCHICAL_REFINE_OCTREE_CELL_TOWARDS_C
+#define GRAL_GB_HIERARCHICAL_REFINE_OCTREE_CELL_TOWARDS_C
+
+// $LICENSE_NEC
+
+#include <stack>
+
+/*! \addtogroup octreealgorithms Octree Algorithms
+   
+    Algorithms operating on octrees.     
+*/
+
+
+/*! \brief Refine an octree cell towards a descendent cell
+
+    \author Guntram Berti    
+    \ingroup  octreealgorithms
+
+    The cell \c Leaf of Octree \c tree is refined \c nl times towards the cell \c subLeaf,
+    that is, we refine always that descendent of \c Leaf which contains \c subLeaf.
+
+    \pre <tt> Tree.isLeaf(Leaf) == true </tt>
+    \pre <tt> Tree.leaf_ancestor(tree, subLeaf) == Leaf </tt>
+    \pre <tt> nl <=  subLeaf.level() - Leaf.level()  </tt> 
+
+*/
+template<class OCTREE, class OCTCELL> // = typename OCTREE::oct_cell_type>
+void refine_towards(OCTREE & Tree, OCTCELL const& Leaf, OCTCELL const& subLeaf, unsigned nl)
+{
+  REQUIRE(Tree.leaf_ancestor(subLeaf) == Leaf, "subLeaf is not a descendant of Leaf", 1);
+  int k = subLeaf.level() - Leaf.level(); 
+  REQUIRE(k >= (int)nl, "",1);
+  // Leaf = parent^k(subLeaf)
+  // we need the nl oldest ancestors of subLeaf:  
+  // parent^k(subLeaf) = Leaf, ..., parent^{k-nl+1}(subLeaf);
+  typename OCTREE::hier_cell_type p = subLeaf;
+  // skip the first k-nl ancestors 
+  for(int i = 1; i <= k-nl; ++i)
+    p = p.Parent(); // p = parent^i(subLeaf)
+  // now p = parent^{k-nl}(subLeaf)
+  std::stack<typename OCTREE::hier_cell_type> ancestors;
+  for(int i = k-nl+1; i <= k; ++i) {
+    p = p.Parent(); // p = parent^i(subLeaf)
+    ancestors.push(p);
+  }
+  REQUIRE( (p == Leaf), "p=" << p.level() << ":" << p.Flat().index()
+	   << " Leaf=" << Leaf.level() << ":" << Leaf.Flat().index(), 1);
+  while(!ancestors.empty()) {
+    Tree.split_cell(ancestors.top());
+    ancestors.pop();
+  }
+
+}
+
+#endif
