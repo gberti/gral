8e5ed3a592b3aa6384aec85eefcde018923ebc69
berti <>
2003-05-21 18:11:36 +0000

    copy does now work for target ComplexND<N> N <= 3

--- GrAL/gral/cartesiannd/test/copy/test-cartesiannd-copy.C
+++ GrAL/gral/cartesiannd/test/copy/test-cartesiannd-copy.C
@@ -68,7 +68,9 @@ int main() {
     std::vector<gtc::cell_handle>   c_corr(R.NumOfCells());
     test_archetypes(R,cout);
 
+    cout << "Grid:\n";
     R.print(cout);
+    cout << "Archetype:\n";
     cartesiannd::grid<3>::TheArchetype().print(cout);
 
     ConstructGrid0(G, R, v_corr, c_corr);
@@ -79,5 +81,66 @@ int main() {
 
   }
 
+  {
+    cout << "Copy cartesiannd::grid<3>:" << endl;
+
+    typedef grid_types<cartesiannd::grid<3> > gt;;
+    typedef gt::vertex_index_type it;
+    cartesiannd::grid<3> R(it(2,2,2));
+    typedef complexnd::ComplexND<3>   dst_grid_type;
+    typedef grid_types<dst_grid_type> dgt;
+
+    test_archetypes(R,cout);
+
+    cout << "Grid:\n";
+    R.print(cout);
+    cout << "Archetype:\n";
+    cartesiannd::grid<3>::TheArchetype().print(cout);
+
+    dst_grid_type G;
+
+    partial_grid_morphism< cartesiannd::grid<3>, dst_grid_type> Phi(R,G);
+    //std::vector<dgt::vertex_handle> v_corr(R.NumOfVertices());
+    //std::vector<dgt::cell_handle>   c_corr(R.NumOfCells());
+    
+    complexnd::ConstructGrid0(G, R, Phi);
+    // ConstructGrid0(G, R, v_corr, c_corr);
+
+    cout << "Orig. grid:  ";  print_grid(R, cout);
+    cout << "Copied grid: ";  print_grid(G, cout);
+
+  }
+  // does not work, partial morphism is not defined for dim > 3
+  /*
+  {
+    cout << "Copy cartesiannd::grid<4>:" << endl;
+    typedef cartesiannd::grid<4>      src_grid_type;
+    typedef grid_types<src_grid_type> gt;;
+    typedef gt::vertex_index_type it;
+    src_grid_type  R(it(2));
+    typedef complexnd::ComplexND<4>   dst_grid_type;
+    typedef grid_types<dst_grid_type> dgt;
+
+    test_archetypes(R,cout);
+
+    cout << "Grid:\n";
+    R.print(cout);
+    cout << "Archetype:\n";
+    src_grid_type::TheArchetype().print(cout);
+
+    dst_grid_type G;
+
+    partial_grid_morphism<src_grid_type, dst_grid_type> Phi(R,G);
+    //std::vector<dgt::vertex_handle> v_corr(R.NumOfVertices());
+    //std::vector<dgt::cell_handle>   c_corr(R.NumOfCells());
+    
+    complexnd::ConstructGrid0(G, R, Phi);
+    // ConstructGrid0(G, R, v_corr, c_corr);
+
+    cout << "Orig. grid:  ";  print_grid(R, cout);
+    cout << "Copied grid: ";  print_grid(G, cout);
+
+  }
+  */
 
 }
