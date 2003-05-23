1ee73611eb5a63f8a9ac999d113ad74998fa5f21
berti <>
2003-05-23 19:30:34 +0000

    test full ConstructGridVC

--- GrAL/gral/complex3d/test/construct/test-construct-complex3d.C
+++ GrAL/gral/complex3d/test/construct/test-construct-complex3d.C
@@ -2,6 +2,7 @@
 #include "Gral/Grids/Cartesian3D/all.h"
 
 #include "Gral/Test/all.h"
+#include "Gral/Geometries/simple-geometry.h"
 
 #include <iostream>
 
@@ -9,6 +10,7 @@ int main() {
   using std::cout;  
   using cartesian3d::CartesianGrid3D;
 
+  typedef grid_types<Complex3D> gt; 
   CartesianGrid3D R(3,3,3);
   Complex3D       G;
 
@@ -34,6 +36,17 @@ int main() {
   test_vertex_on_cell_iterator(G,cout);
   test_edge_on_cell_iterator(G,cout);
   test_facet_on_cell_iterator(G,cout);
+
+
+  Complex3D G3;
+  cartesian3d::lattice_geometry GeomR(R);
+  typedef cartesian3d::lattice_geometry::coord_type coord_type;
+  simple_geometry<Complex3D, coord_type> GeomG3(G3);
+  std::vector<gt::vertex_handle> v_corr;
+  std::vector<gt::cell_handle>   c_corr;
+  ConstructGridVC(G3, GeomG3,
+		  R,  GeomR,
+		  v_corr, c_corr);
 
   
 }
