a90adc8960ea17bdbfd6f47c8e055f31fda862fa
berti <>
2003-05-23 19:18:37 +0000

    added mixins providing archetype functionality to 1D grids

--- GrAL/gral/base/include/Gral/Base/archetype-0D.h
+++ GrAL/gral/base/include/Gral/Base/archetype-0D.h
@@ -5,13 +5,20 @@
 
 #include "Gral/Base/grid-function-vector.h"
 
+namespace archetype_0D {
 
-class archetype_0D {
+/*! \brief  A 0-dimensional grid which can be used as archetype for regular 1-dimensional grids
+
+*/
+
+class archetype_t {
 public:
-  enum { dimension = 0 };
+  enum { dim = 0 };
+
+  unsigned dimension() const { return 0;}
   
   struct VertexIterator {
-    typedef archetype_0D   grid_type;
+    typedef archetype_t   grid_type;
     typedef VertexIterator self;
 
     grid_type    const*  g;
@@ -42,28 +49,79 @@ public:
   CellIterator   FirstCell()   const { return CellIterator(*this);}
 };
 
+
+
+inline bool operator==(archetype_t::VertexIterator const& lhs,
+		       archetype_t::VertexIterator const& rhs) 
+{ return  lhs.handle() == rhs.handle();}
+inline bool operator!=(archetype_t::VertexIterator const& lhs,
+		       archetype_t::VertexIterator const& rhs) 
+{ return  !(lhs == rhs);}
+
+
+
+struct grid_types_mixin {
+  typedef archetype_t           archetype_type;
+  typedef archetype_type const* archetype_iterator;
+  typedef int                   archetype_handle;
+
+};
+
+class grid_mixin_base  : public grid_types_mixin {
+private:
+  static archetype_t the_archetype[1];
+public:
+  /*! \name Archetype handling
+   */
+  /*@{*/ 
+  static archetype_iterator BeginArchetype() { return the_archetype; }
+  static archetype_iterator EndArchetype()   { return the_archetype+1; }
+  static archetype_handle   handle(archetype_iterator it)  { return it - BeginArchetype();}
+  static archetype_type const& Archetype(archetype_handle = 0) { return *BeginArchetype();} 
+  static unsigned NumOfArchetypes() { return 1;}
+  /*@}*/  
+};
+
+
+template<class GRID, class GT = grid_types<GRID> >
+class grid_mixin : public grid_mixin_base {
+public:
+  static archetype_type const& ArchetypeOf (typename GT::Cell const&)  
+    { return *BeginArchetype();}
+  static archetype_type   const& ArchetypeOf (typename GT::cell_handle) 
+    { return *BeginArchetype();}
+  static archetype_handle        archetype_of(typename GT::cell_handle) 
+    { return 0;}
+  static archetype_handle        archetype_of(typename GT::Cell const&) 
+    { return 0;}
+};
+
+
+} // namespace archetype_0D
+
+
 template<>
-struct grid_types<archetype_0D> : public grid_types_base<archetype_0D> 
+struct grid_types<archetype_0D::archetype_t> : public grid_types_base<archetype_0D::archetype_t> 
 {
   typedef grid_dim_tag<0> dimension_tag;  
 };
 
 template<>
-struct element_traits<archetype_0D::Vertex> 
-  : public element_traits_vertex_base<archetype_0D>
+struct element_traits<archetype_0D::archetype_t::Vertex> 
+  : public element_traits_vertex_base<archetype_0D::archetype_t>
 {
-  typedef element_traits_vertex_base<archetype_0D>  base;
+  typedef element_traits_vertex_base<archetype_0D::archetype_t>  base;
   typedef consecutive_integer_tag<0>                       consecutive_tag;
   typedef base::hasher_type_elem_base                      hasher_type;
 };
 
 
 template<class T>
-class grid_function<archetype_0D::Vertex, T> 
-  : public grid_function_vector<archetype_0D::Vertex, T>
+class grid_function<archetype_0D::archetype_t::Vertex, T> 
+  : public grid_function_vector<archetype_0D::archetype_t::Vertex, T>
 {
-  typedef grid_function_vector<archetype_0D::Vertex, T> base;
-  typedef archetype_0D                                  grid_type;
+  typedef grid_function_vector<archetype_0D::archetype_t::Vertex, T> base;
+  typedef archetype_0D::archetype_t                                  grid_type;
 public:
   grid_function() {}
   grid_function(grid_type const& g) : base(g) {}
@@ -71,18 +129,11 @@ public:
 };
 
 
-inline bool operator==(archetype_0D::VertexIterator const& lhs,
-		       archetype_0D::VertexIterator const& rhs) 
-{ return  lhs.handle() == rhs.handle();}
-inline bool operator!=(archetype_0D::VertexIterator const& lhs,
-		       archetype_0D::VertexIterator const& rhs) 
-{ return  !(lhs == rhs);}
-
 
 
 template<class V_CORR, class C_CORR>
-void ConstructGrid0(archetype_0D      & dst,
-		    archetype_0D const& src,
+void ConstructGrid0(archetype_0D::archetype_t      & dst,
+		    archetype_0D::archetype_t const& src,
 		    V_CORR       & v_corr,
 		    C_CORR       & c_corr)
 {
@@ -92,6 +143,7 @@ void ConstructGrid0(archetype_0D      & dst,
   c_corr[1] = 1;
 }
 
+/*
 template<class PHI>
 void ConstructGrid0(archetype_0D      & dst,
 		    archetype_0D const& src,
@@ -101,6 +153,8 @@ void ConstructGrid0(archetype_0D      & dst,
   phi[Vertex(src,0)] = Vertex(dst,0);
   phi[Vertex(src,1)] = Vertex(dst,1);
 }
+*/
+
 
 
 #endif
