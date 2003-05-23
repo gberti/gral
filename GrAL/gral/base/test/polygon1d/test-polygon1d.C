9f2d9e684141bd162513a0d0afc86c0cd9b78d5c
berti <>
2003-05-23 19:20:52 +0000

    test new features

--- GrAL/gral/base/test/polygon1d/test-polygon1d.C
+++ GrAL/gral/base/test/polygon1d/test-polygon1d.C
@@ -3,9 +3,31 @@
 */
 
 #include "Gral/Base/polygon.h"
-#include "Gral/Test/test-iterators.h"
+#include "Gral/Test/all.h"
+
+#include "Utility/pre-post-conditions.h"
 
 #include <iostream>
+
+template<class GRID1D>
+void test_switch(GRID1D const& G, std::ostream& out)
+{
+  typedef grid_types<GRID1D> gt;
+  typename gt::Cell   c(* G.FirstCell()); 
+  typename gt::Vertex v(* c.FirstVertex());
+  typename gt::Vertex v_first = v;
+
+  do{
+    out << "  ->  ";
+    out << "(v=" << v.handle() << " c=" << c.handle() << ") ";
+    G.switch_vertex(v,c);
+    out << "  ->  ";
+    out << "(v=" << v.handle() << " c=" << c.handle() << ") ";
+    G.switch_cell  (v,c);
+  }
+  while(v != v_first);
+  out << std::endl;
+}
 
 
 int main() {
@@ -20,5 +42,52 @@ int main() {
   test_vertex_on_cell_iterator(p, *out);
   test_facet_on_cell_iterator (p, *out);
 
+  test_grid_functions<gt::Vertex>(p, *out);
+  test_grid_functions<gt::Cell>  (p, *out);
+
+  test_archetypes(p, *out);
+
   checkgt<gt>(*out);
+
+  test_switch(p, *out);
+
+  {
+    polygon1d::polygon p2;
+    vertex_morphism<polygon1d::polygon, polygon1d::polygon> v_corr(p,p2);
+    cell_morphism  <polygon1d::polygon, polygon1d::polygon> c_corr(p,p2);
+    ConstructGrid0(p2,p, v_corr, c_corr);
+    
+    {
+      gt::VertexIterator v_p  = p .FirstVertex();
+      gt::VertexIterator v_p2 = p2.FirstVertex();
+      for( ; !v_p.IsDone(); ++v_p, ++v_p2)
+	REQUIRE_ALWAYS( (v_corr[v_p.handle()] == v_corr[v_p2.handle()]), "", 1);
+    }
+    {
+      gt::CellIterator v_p  = p .FirstCell();
+      gt::CellIterator v_p2 = p2.FirstCell();
+      for( ; !v_p.IsDone(); ++v_p, ++v_p2)
+	REQUIRE_ALWAYS( (c_corr[v_p.handle()] == c_corr[v_p2.handle()]), "", 1);
+    }
+  }
+
+  {
+    polygon1d::polygon p2;
+    vertex_morphism<polygon1d::polygon, polygon1d::polygon> v_corr(p,p2);
+    ConstructGrid0(p2,p, v_corr);
+    
+    {
+      gt::VertexIterator v_p  = p .FirstVertex();
+      gt::VertexIterator v_p2 = p2.FirstVertex();
+      for( ; !v_p.IsDone(); ++v_p, ++v_p2)
+	REQUIRE_ALWAYS( (v_corr[v_p.handle()] == v_corr[v_p2.handle()]), "", 1);
+    }
+  }
+
+  {
+    polygon1d::polygon p2;
+    ConstructGrid0(p2,p);
+  }
+
+
 }
