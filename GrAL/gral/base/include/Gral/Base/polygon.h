e11bcd02162c2623415b8e255133304bc38b6e59
berti <>
2003-05-23 19:19:30 +0000

    added switch_xxx, grid_functions, archetypes

--- GrAL/gral/base/include/Gral/Base/polygon.h
+++ GrAL/gral/base/include/Gral/Base/polygon.h
@@ -5,8 +5,11 @@
 #include "Gral/Base/element-handle.h"
 #include "Gral/Base/vertex-iterator-int.h"
 #include "Gral/Base/cell-iterator-int.h"
+#include "Gral/Base/archetype-0D.h"
+#include "Gral/Base/construct-grid.h"
 
-namespace polygon1d {
+
+namespace polygon1d  { 
   
   //class VertexIterator1d;
   //class CellIterator1d;
@@ -45,11 +48,13 @@ namespace polygon1d {
      An object of type polygon represents a simple polygon.
 
   */
-  class polygon : public grid_types_p1d {
+  class polygon : public grid_types_p1d, public archetype_0D::grid_mixin<polygon, grid_types_p1d> {
   private:
     unsigned nv;
   public:
     explicit polygon(int nnv = 0) : nv(nnv) {}
+
+    void set_number_of_vertices(int nnv) { nv = nnv;}
     
     unsigned NumOfVertices() const { return nv;}
     unsigned NumOfCells()    const { return nv;}
@@ -65,6 +70,16 @@ namespace polygon1d {
 
     unsigned NumOfVertices(Cell const&) const { return 2;}
     unsigned NumOfFacets  (Cell const&) const { return 2;}
+
+    int next_handle(int h) const { return (h == (int) NumOfVertices()-1 ?  0                : h+1);}
+    int prev_handle(int h) const { return (h == 0                 ? NumOfVertices()-1 : h-1);}
+
+    inline void switch_vertex(Vertex      & v, Cell const& c) const;
+    inline void switch_cell  (Vertex const& v, Cell      & c) const;
+
+    inline Vertex switched_vertex(Vertex const& v, Cell const& c) const;
+    inline Cell   switched_cell  (Vertex const& v, Cell const& c) const;
+
   };
 
 
@@ -102,16 +117,158 @@ namespace polygon1d {
 } // namespace polygon1d
 
 template<>
-struct grid_types<polygon1d::polygon> : public grid_types_base<polygon1d::grid_types_p1d>
+struct grid_types<polygon1d::polygon> 
+ : public grid_types_base<polygon1d::grid_types_p1d>,
+   public archetype_0D::grid_types_mixin
 {
   // in order to allow derivation from this class
   grid_types() {}
 };
 
+template<>
+struct element_traits<polygon1d::polygon::Vertex> 
+  : public element_traits_vertex_base<polygon1d::polygon>
+{
+  typedef element_traits_vertex_base<polygon1d::polygon> base;
+  typedef consecutive_integer_tag<0>                     consecutive_tag;
+  typedef base::hasher_type_elem_base                    hasher_type;
+};
+
+template<>
+struct element_traits<polygon1d::polygon::Cell> 
+  : public element_traits_cell_base<polygon1d::polygon>
+{
+  typedef element_traits_cell_base<polygon1d::polygon> base;
+  typedef consecutive_integer_tag<0>                     consecutive_tag;
+  typedef base::hasher_type_elem_base                    hasher_type;
+};
+
+
+template<class T>
+class grid_function<polygon1d::polygon::Vertex, T> 
+  : public grid_function_vector<polygon1d::polygon::Vertex, T> 
+{
+  typedef grid_function_vector<polygon1d::polygon::Vertex, T> base;
+public:
+  typedef polygon1d::polygon                                  grid_type;
+  grid_function() {}
+  grid_function(grid_type const& g) : base(g) {}
+  grid_function(grid_type const& g, T const& t) : base(g,t) {}
+  grid_function(ref_ptr<grid_type const> g) : base(g) {}
+  grid_function(ref_ptr<grid_type const> g, T const& t) : base(g,t) {}
+};
+
+template<class T>
+class grid_function<polygon1d::polygon::Cell, T> 
+  : public grid_function_vector<polygon1d::polygon::Cell, T> 
+{
+  typedef grid_function_vector<polygon1d::polygon::Cell, T> base;
+public:
+  typedef polygon1d::polygon                                  grid_type;
+  grid_function() {}
+  grid_function(grid_type const& g) : base(g) {}
+  grid_function(grid_type const& g, T const& t) : base(g,t) {}
+  grid_function(ref_ptr<grid_type const> g) : base(g) {}
+  grid_function(ref_ptr<grid_type const> g, T const& t) : base(g,t) {}
+};
+
+/*
+template<class VCORR, class CCORR>
+void ConstructGrid0(polygon1d::polygon      & G_dst, 
+		    polygon1d::polygon const& G_src,
+		    VCORR & v_corr,
+		    CCORR & c_corr)
+{
+  typedef grid_types<polygon1d::polygon> gtsrc;
+  typedef grid_types<polygon1d::polygon> gtdst;
+
+  G_dst.set_number_of_vertices(G_src.NumOfVertices());
+
+  gtsrc::VertexIterator v_src = G_src.FirstVertex(); 
+  gtdst::VertexIterator v_dst = G_dst.FirstVertex();
+  for(; ! v_src.IsDone(); ++v_src, ++v_dst)
+    v_corr[v_src.handle()] = v_dst.handle();
+
+  gtsrc::CellIterator c_src = G_src.FirstCell(); 
+  gtdst::CellIterator c_dst = G_dst.FirstCell();
+  for(; ! c_src.IsDone(); ++c_src, ++c_dst)
+    c_corr[c_src.handle()] = c_dst.handle();
+}
+*/
+
+template<class GSRC, class VCORR, class CCORR>
+void ConstructGrid0(polygon1d::polygon      & G_dst, 
+		    GSRC               const& G_src,
+		    VCORR & v_corr,
+		    CCORR & c_corr)
+{
+  typedef grid_types<GSRC>               gtsrc;
+  typedef grid_types<polygon1d::polygon> gtdst;
+
+  G_dst.set_number_of_vertices(G_src.NumOfVertices());
+
+  if(G_src.NumOfVertices() > 0) {
+    // std::vector<typename gtsrc::vertex_handle> ordered_src_vertices(G_src.NumOfVertices());
+    
+    typename gtsrc::Cell   c_src = * G_src.FirstCell();
+    typename gtsrc::Vertex v_src = * c_src.FirstVertex();
+    gtdst         ::Cell   c_dst = * G_dst.FirstCell();
+    gtdst         ::Vertex v_dst = * c_dst.FirstVertex();
+    typename gtsrc::Vertex v_src_first = v_src;    
+
+    v_corr[v_src.handle()] = v_dst.handle();
+    c_corr[c_src.handle()] = c_dst.handle();
+    unsigned cnt = 0;
+    //    ordered_src_vertices[cnt] = v_src.handle();
+    G_src.switch_vertex(v_src,c_src);
+    G_src.switch_cell  (v_src,c_src);
+    G_dst.switch_vertex(v_dst,c_dst);
+    G_dst.switch_cell  (v_dst,c_dst);
+    ++cnt;
+    while ( v_src != v_src_first) {
+      //ordered_src_vertices[cnt] = v.handle();
+      v_corr[v_src.handle()] = v_dst.handle();
+      c_corr[c_src.handle()] = c_dst.handle();
+      G_src.switch_vertex(v_src,c_src);
+      G_src.switch_cell  (v_src,c_src);
+      G_dst.switch_vertex(v_dst,c_dst);
+      G_dst.switch_cell  (v_dst,c_dst);
+      ++cnt;
+    }
+    // this can happen only if more than 1 component or crossings allowed in G_src!
+    if(cnt < G_src.NumOfVertices() -1) {
+      ENSURE(false, 
+	     "=> G_src is not a simple polygon! cnt=" << cnt 
+	     << "  G_src.NumOfVertices()=" <<  G_src.NumOfVertices(), 1);  
+    }
+
+  }
+  
+}
 
 namespace polygon1d {
   inline polygon::VertexIterator polygon::FirstVertex() const { return VertexIterator(*this);}
   inline polygon::CellIterator   polygon::FirstCell()   const { return CellIterator  (*this);}
+
+  inline void polygon::switch_vertex(polygon::Vertex      & v, polygon::Cell const& c) const 
+  { 
+    v = ( (v == * c.FirstVertex()) 
+	  ? Vertex(*this, next_handle(v.handle())) 
+	  : Vertex(*this, prev_handle(v.handle())));
+  }
+
+  inline void polygon::switch_cell  (polygon::Vertex const& v, polygon::Cell      & c) const
+  { 
+    c = ( (v == * c.FirstVertex()) 
+	  ? Cell(*this, prev_handle(c.handle())) 
+	  : Cell(*this, next_handle(c.handle())));
+  }
+
+  inline polygon::Vertex polygon::switched_vertex(polygon::Vertex const& v, polygon::Cell const& c) const
+  { Vertex res(v); switch_vertex(res,c); return res;}
+
+  inline  polygon::Cell   polygon::switched_cell  (polygon::Vertex const& v, polygon::Cell const& c) const
+  { Cell   res(c); switch_cell  (v,res); return res;}
 
 } // namespace polygon1d
 
