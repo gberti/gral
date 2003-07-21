84c455166bf698957b1bab242846e8f3c5b793ac
berti <>
2003-07-21 15:33:24 +0000

    added bubblesort + same sign check for permuted sequences

--- GrAL/sequence/include/Container/sequence-algorithms.h
+++ GrAL/sequence/include/Container/sequence-algorithms.h
@@ -2,6 +2,11 @@
 #define GRAL_GB_SEQUENCE_ALGORITHMS_H
 
 
+#include "Utility/pre-post-conditions.h"
+
+#include <iterator>
+#include <functional>
+#include <algorithm>
 
 namespace sequence {
 
@@ -50,17 +55,69 @@ namespace sequence {
     return max_iter;
   }
 
-  /*!
+  /*! \brief Check whether any item \c i in \c [first,end[ exist with <tt> p(i) == true </tt>
       \ingroup algorithms
   */
  template <typename InputIterator, typename Predicate>
-  inline bool exists(InputIterator first, InputIterator last, Predicate p) {
-    while(first != last) {
+  inline bool exists(InputIterator first, InputIterator end, Predicate p) {
+    while(first != end) {
       if(p(*first))
 	return true;
       ++first;
     }
     return false; 
+  }
+
+
+  /*! \brief The classic bubble sort.
+
+    \see test-bubblesort.C
+    \ingroup algorithms
+  */
+  template<typename ForwardIterator, typename Less>
+  int bubble_sort(ForwardIterator begin, ForwardIterator end, Less less)
+  {
+    if(begin == end) return 0;
+    ForwardIterator next = begin; 
+    ++next;
+    if(next == end) return 0;
+
+    int num_transpositions = 0;
+    bool ordered = false;
+    while(ordered == false) {
+      ordered = true;
+      while(next != end) {
+	if(! less(*begin, *next)) {
+	  std::swap(*begin, *next);
+	  ordered = false;
+	  ++num_transpositions;
+	}
+	++begin;
+	++next;
+      }
+    } 
+    return num_transpositions;
+  } // bubble_sort
+
+  template<typename ForwardIterator>
+  int bubble_sort(ForwardIterator begin, ForwardIterator end)
+  { return bubble_sort(begin, end, std::less<typename std::iterator_traits<ForwardIterator>::value_type>());}
+
+
+  /*! \brief Check wether the two sequences have the same permutation sign
+    \ingroup algorithms
+
+    \pre The two sequences must be identical as sets
+    \post The two sequences will be sorted
+  */
+  template<typename ForwardIterator1, typename ForwardIterator2>
+  bool same_permutation_sign(ForwardIterator1 begin1, ForwardIterator1 end1,
+			     ForwardIterator2 begin2, ForwardIterator2 end2)
+  { 
+    int t1 = bubble_sort(begin1, end1);
+    int t2 = bubble_sort(begin2, end2);
+    REQUIRE(equal(begin1, end1, begin2), "", 1);
+    return (t1%2) == (t2%2);
   }
 
 } // namespace sequence
