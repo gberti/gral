82376abd0e972bbe9d50f21e2dcb51a5ea880b4c
berti <>
2003-08-01 18:41:45 +0000

    test descendants(h_element_t<>,level) replaced RegGrid2D with cartesiannd::grid<2>

--- GrAL/gral/hierarchical/test/construct/test-hier-construct.C
+++ GrAL/gral/hierarchical/test/construct/test-hier-construct.C
@@ -20,10 +20,16 @@ void test_hier_grid(GRID const& root,
   typedef hierarchical::hgrid_cartesian<cart_grid_type> hier_grid_type;
   typedef hier_grid_type                                hgt;
   typedef typename hgt::hier_cell_type                  hier_cell_type;
+  typedef typename hgt::hier_facet_type                 hier_facet_type;
   typedef typename hgt::hier_vertex_type                hier_vertex_type;
   typedef typename hgt::element_base_type               element_base_type;
-  typedef hierarchical::h_vertex_on_cell_iterator_t<element_base_type> HierVertexOnCellIterator;
-  typedef hgt::index_type index_type;
+  //  typedef hierarchical::h_vertex_on_cell_iterator_t<element_base_type> HierVertexOnCellIterator;
+  //  typedef hierarchical::h_vertex_on_element_iterator_t<element_base_type, cell_type_tag>  HierVertexOnCellIterator;
+  //   typedef hierarchical::h_vertex_on_element_iterator_t<element_base_type, facet_type_tag> HierVertexOnFacetIterator;
+  typedef typename hierarchical::h_incidence_iterator_t<element_base_type, vertex_type_tag, cell_type_tag> 
+                                                        HierVertexOnCellIterator;
+  typedef typename hgt::index_type                      index_type;
+  typedef hierarchical::h_element_t<hierarchical::h_element_base_t<hier_grid_type>, typename cgt::Facet >  hier_facet1_type;
 
   hier_grid_type H(root,pattern);
   H.add_finer_level();
@@ -32,7 +38,7 @@ void test_hier_grid(GRID const& root,
   for(typename hgt::level_handle lev = H.coarsest_level(); lev <= H.finest_level(); lev = H.next_finer_level(lev)) {
     out << "Level " << lev << ": " << H.FlatGrid(lev)->cell_size() << " cells" << std::endl;
     for(typename cgt::CellIterator c = H.FlatGrid(lev)->FirstCell(); !c.IsDone(); ++c) {
-      hgt::cart_subrange_type R = H.descendants(hier_cell_type(H,*c,lev), H.finest_level());
+      typename hgt::cart_subrange_type R = H.descendants(hier_cell_type(H,*c,lev), H.finest_level());
       out << "Descendant of cell " << (*c).index() << ": " 
 	  << R.NumOfCells()    << " cells: "    << "[" << R.low_cell_index()   << "," << R.high_cell_index()   << "]"
 	  << "  "
@@ -40,6 +46,20 @@ void test_hier_grid(GRID const& root,
 	  << "\n";
       REQUIRE_ALWAYS( R.high_cell_index()   + index_type(1)  ==  R.beyond_cell_index(),   "", 1);
       REQUIRE_ALWAYS( R.high_vertex_index() + index_type(1)  ==  R.beyond_vertex_index(), "", 1);
+      for(typename cgt::FacetOnCellIterator fc(*c); ! fc.IsDone(); ++fc) {
+	hier_facet_type hf(H, *fc, lev);
+      }
+    }
+    for(typename cgt::FacetIterator f = H.FlatGrid(lev)->FirstFacet(); !f.IsDone(); ++f) {
+      //h_element_t<h_element_base<hier_grid_type>, typename cgt::Facet> hf(H,*f,lev);
+      hier_facet1_type hf(H,*f,lev);
+      typename hgt::cart_subrange_type R = H.descendants(hf, H.finest_level());
+      
+      out << "Descendant of facet " << (*f).index() << ": " 
+	  << R.NumOfFacets()    << " facets: "    << "[" << R.low_cell_index()   << "," << R.high_cell_index()   << "]"
+	  << "  "
+	  << R.NumOfVertices() << " vertices: " << "[" << R.low_vertex_index() << "," << R.high_vertex_index() << "]"
+	  << "\n";
     }
   }
   
@@ -59,7 +79,7 @@ void test_hier_grid(GRID const& root,
 
     for(HierVertexOnCellIterator vc(h); !vc.IsDone(); ++vc) {
       hier_vertex_type v = *vc;
-      hier_cell_type   c = vc.TheCell();
+      hier_cell_type   c = vc.TheAnchor();
       REQUIRE_ALWAYS(c == h, "", 1);
     }
   }
@@ -126,9 +146,11 @@ int main() {
 
   cout << "----------------------------" << std::endl;
   {
-    namespace cart = cartesian2d;
-    typedef cart::CartesianGrid2D cart_grid_type;
-
+    //    namespace cart = cartesian2d;
+    //typedef cart::CartesianGrid2D cart_grid_type;
+    typedef cartesiannd::grid<2> cart_grid_type;
+    typedef grid_types<cart_grid_type> gt;
+    typedef gt::index_type             it;
     // CartesianGrid2D can be have lower index != (0,0)!
     // Then hierarchical_grid does not work any more!
     cart_grid_type root(4,4);
@@ -138,13 +160,16 @@ int main() {
   
   cout << "----------------------------" << std::endl;
   {
-    namespace cart = cartesian2d;
-    typedef cart::CartesianGrid2D cart_grid_type;
+    //   namespace cart = cartesian2d;
+    //  typedef cart::CartesianGrid2D cart_grid_type;
+    typedef cartesiannd::grid<2> cart_grid_type;
+    typedef grid_types<cart_grid_type> gt;
+    typedef gt::index_type             it;
 
     // CartesianGrid2D can be have lower index != (0,0)!
     // Then hierarchical_grid does not work any more!
-    cart_grid_type root(3,3);
-    cart_grid_type ref_pattern(3,3); // 2x2 cells!
+    cart_grid_type root(it(3,3));
+    cart_grid_type ref_pattern(it(3,3)); // 2x2 cells!
     test_hier_grid(root, ref_pattern, cout);   
   }
   
