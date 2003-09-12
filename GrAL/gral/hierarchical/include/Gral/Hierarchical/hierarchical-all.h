627040a972c2b5762bf96c8fae7b9593db5c597c
berti <>
2003-09-12 14:23:46 +0000

    *** empty log message ***

---
+++ GrAL/gral/hierarchical/include/Gral/Hierarchical/hierarchical-all.h
@@ -0,0 +1,11 @@
+#ifndef GRAL_GB_HIERARCHICAL_ALL_H
+#define GRAL_GB_HIERARCHICAL_ALL_H
+
+// $LICENSE_NEC
+
+#include "Gral/Hierarchical/hierarchical-grid.h"
+#include "Gral/Hierarchical/hierarchical-geometry.h"
+#include "Gral/Hierarchical/hierarchical-grid-function.h"
+#include "Gral/Hierarchical/hierarchical-partial-grid-function.h"
+
+#endif
