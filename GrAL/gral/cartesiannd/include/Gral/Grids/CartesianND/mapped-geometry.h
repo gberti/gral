5197611ac0a4c9bda32b9d4e7debd9a725c7576c
berti <>
2003-09-19 19:04:03 +0000

    added support for point location

--- GrAL/gral/cartesiannd/include/Gral/Grids/CartesianND/mapped-geometry.h
+++ GrAL/gral/cartesiannd/include/Gral/Grids/CartesianND/mapped-geometry.h
@@ -5,6 +5,7 @@
 
 #include "Gral/Grids/CartesianND/cartesiannd.h"
 #include "Utility/pre-post-conditions.h"
+#include "Gral/Geometries/point-location-base.h"
 
 #include "Container/tuple.h"
 #include "Container/tuple-point-traits.h"
@@ -15,8 +16,8 @@ namespace cartesiannd {
   template<class CARTGRID>
   struct default_coord {
     // could use CARTGRID::index_type::rebind<double>
-    enum { dimension = CARTGRID::dim };
-    typedef tuple<double, dimension> type;
+    enum { dim = CARTGRID::dim };
+    typedef tuple<double, dim> type;
   };
 
 
@@ -32,9 +33,13 @@ namespace cartesiannd {
     typedef typename gt::Vertex Vertex;
     typedef typename gt::Cell   Cell;
     typedef typename gt::index_type index_type;
+
+    typedef point_location_result<gt, coord_type> location_result_type;
   private:
     ref_ptr<grid_type const> g;
-    mapping_type       f;     // defined on unit cube (at least).
+    ref_ptr<mapping_type const> f;     // defined on unit cube (at least).
+    ref_ptr<mapping_type const> f_inverse;
+
     index_type         low_;
     index_type         high_; // [low_, high_] will be first mapped to unit cube and
                               // then mapped by f.
@@ -42,11 +47,12 @@ namespace cartesiannd {
   public:
     mapped_geometry() {}
     mapped_geometry(grid_type const& gg, mapping_type ff = mapping_type()) 
-      : g(gg), f(ff), low_(gg.low_vertex_index()), high_(gg.high_vertex_index()) { init();}
+      : g(gg), f(new mapping_type(ff)), low_(gg.low_vertex_index()), high_(gg.high_vertex_index()) { init();}
     mapped_geometry(grid_type const& gg, index_type lw, index_type hgh, 
 		    mapping_type ff = mapping_type()) 
-      : g(gg), f(ff), low_(lw), high_(hgh) { init();}
+      : g(gg), f(new mapping_type(ff)), low_(lw), high_(hgh) { init();}
 
+    void set_inverse_mapping(mapping_type inv) { f_inverse = new mapping_type(inv);}
 
     void init() {
       delta = coord_type(1.0);
@@ -59,8 +65,8 @@ namespace cartesiannd {
       init();
     }
     void rebind(grid_type const& gg, mapping_type const& ff) {
-      f = ff;
-      rebind(g);
+      f = ref_ptr<mapping_type const>(ff);
+      rebind(gg);
     }
 
     ref_ptr<grid_type const> TheGrid() const { return g;}
@@ -68,19 +74,55 @@ namespace cartesiannd {
 
     coord_type unit_coord(index_type idx) const { 
       coord_type r(idx - low_);
-      return quotient(r, high_);
+      return product(r, delta);
     }
-    coord_type coord (Vertex const& v) const { cb(); return f(unit_coord(v.index()));}
+    //! index i such that local_p is in cell with index i
+    index_type unit2index(coord_type local_p) const {
+      return floor_tuple(quotient(local_p, delta)) + coord_type(low_);
+    }
+    coord_type coord (Vertex const& v) const { cb(); return (*f)(unit_coord(v.index()));}
     /*! \brief Result is the mapping of the center of c with respect to unit coordinates \f$ \in [0,1]^d \f$
         
         This is the center of inertia only if the mapping \c f is affine. 
     */
-    coord_type center(Cell   const& c) const { cb(); return f(unit_coord(c.index()) + 0.5*delta);} 
+    coord_type center(Cell   const& c) const { cb(); return (*f)(unit_coord(c.index()) + 0.5*delta);} 
 
     bool bound() const { return g != 0;}
     void cb()    const { REQUIRE(bound(), "", 1);}
 
+    /*! \brief locate point \c pp in grid
+
+     */
+    template<class COORD>
+    location_result_type locate(COORD const& pp) const;
+   
   }; // mapped_geometry<CARTGRID,MAP>
+
+
+  template<class CARTGRID, class MAP>
+    template<class COORD>
+    typename mapped_geometry<CARTGRID,MAP>::location_result_type
+    typename mapped_geometry<CARTGRID,MAP>::locate(COORD const& pp) const 
+    {
+      REQUIRE( (f_inverse != 0), "Must specifiy inverse mapping!", 1);
+      coord_type p; assign_point(p,pp);
+      coord_type local_p = (*f_inverse)(p); // in [0,1]^d iff p in some grid cell
+      index_type cell_of_p = unit2index(local_p);
+      if(TheGrid()->valid_cell_index(cell_of_p))
+	return location_result_type(Cell(TheGrid(), cell_of_p), p);
+      else {
+	//index_type grid_low  = TheGrid()->low_cell_index();
+	//index_type grid_high = TheGrid()->high_cell_index();
+	index_type projected_cell    = clamp_tuple(TheGrid()->low_cell_index(), TheGrid()->high_cell_index(), cell_of_p);
+	coord_type projected_local_p = clamp_tuple(unit_coord(TheGrid()->low_vertex_index()), 
+						   unit_coord(TheGrid()->high_vertex_index()), local_p);
+        // FIXME: Problems occur for points at 'upper' grid boundary (one coordinate = higher grid coordinate)
+	// These will be rounded to outside cell => should measure relative difference of local_p and projected_local_p 
+	// This is a real problem only for exact arithmetic, though.
+	return location_result_type(Cell(TheGrid(), projected_cell), (*f)(projected_local_p), 
+				    location_result_type::projection_tag);
+      }
+    }
 
 } // namespace cartesiannd
 
