3260bf096657b66529506e3357b2b0b84f73c9b4
berti <>
2003-10-16 11:19:04 +0000

    new tests added

--- GrAL/gral/testing/include/Gral/Test/test-iterators.h
+++ GrAL/gral/testing/include/Gral/Test/test-iterators.h
@@ -7,19 +7,35 @@
 
  */
 
+// sequence iterators
 #include "Gral/Test/test-vertex-iterator.h"
 #include "Gral/Test/test-edge-iterator.h"
+#include "Gral/Test/test-face-iterator.h"
 #include "Gral/Test/test-facet-iterator.h"
 #include "Gral/Test/test-cell-iterator.h"
 
+// incidence iterators
+
+// on cells
 #include "Gral/Test/test-vertex-on-cell-iterator.h"
 #include "Gral/Test/test-edge-on-cell-iterator.C"
 #include "Gral/Test/test-facet-on-cell-iterator.h"
 #include "Gral/Test/test-cell-on-cell-iterator.h"
 
+// on facets
 #include "Gral/Test/test-vertex-on-facet-iterator.h"
 #include "Gral/Test/test-edge-on-facet-iterator.h"
 
+// on faces
+#include "Gral/Test/test-vertex-on-face-iterator.h"
+#include "Gral/Test/test-edge-on-face-iterator.h"
+
+// on edges
 #include "Gral/Test/test-vertex-on-edge-iterator.h"
+
+// on vertices
+#include "Gral/Test/test-vertex-on-vertex-iterator.h"
+#include "Gral/Test/test-cell-on-vertex-iterator.h"
+
 
 #endif
