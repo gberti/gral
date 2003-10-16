de630419e9a8fee5170f231829c3841d57fa2319
berti <>
2003-10-16 10:49:27 +0000

    test matrix operations

--- GrAL/geometry/include/Geometry/internal/test-primitives2d.C
+++ GrAL/geometry/include/Geometry/internal/test-primitives2d.C
@@ -40,13 +40,40 @@ void test_algebraic_primitives2d<POINT>::do_tests(std::ostream& out)
   for(int i = 0; i < 2; ++i) {
     inv[i] = pt::Origin(2);
   }
-  /*
-  ap::inverse(e[0],e[1],inv[0],inv[1]);
+ 
+ 
+  ap::invert(e[0],e[1],inv[0],inv[1]);
   out << inv[0] << "  " << inv[1] << '\n';
-  ap::inverse(e[1],e[0],inv[0],inv[1]);
+  ap::invert(e[1],e[0],inv[0],inv[1]);
   out << inv[0] << "  " << inv[1] << '\n';
-  */
 
+  typename ap::matrix_type A(0);
+  typename ap::matrix_type B(ap::matrix_type::UnitMatrix());
+  typename ap::matrix_type C = A*B;
+
+  out << "A= " << A[li+0] << "  " << A[li+1] << "\n"
+      << "B= " << B[li+0] << "  " << B[li+1] << "\n"
+      << "C= " << C[li+0] << "  " << C[li+1] << "\n";
+ 
+  A(li+0,li+0) = 1;
+  A(li+1,li+1) = 2;
+  out << "A=   "   << A[li+0] << "  " << A[li+1] << "\n";
+  C = A*B;
+  out << "A*B= " << C[li+0] << "  " << C[li+1] << "\n";
+  typename ap::matrix_type Ainv = ap::inverse(A);
+  out << "A^-1 = " << Ainv[li+0] << "  " << Ainv[li+1] << "\n";
+
+  typename ap::matrix_type D(0);
+  D(li+0, li+1) = 1;
+  D(li+1, li+0) = 1;
+  out << "D=     " << D[li+0] << "  " << D[li+1] << "\n";
+  typename ap::matrix_type Dinv = ap::inverse(D);
+  out << "D^-1 = " << Dinv[li+0] << "  " << Dinv[li+1] << "\n";
+
+  D *= D;
+  out << "D^2 =  " << D[li+0] << "  " << D[li+1] << "\n";
+
+  
 }
 
 
