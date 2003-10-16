3d946bef5b371e61b3a05c609d720e46856e2b9d
berti <>
2003-10-16 10:52:45 +0000

    test comparison operators

--- GrAL/geometry/test/simplex/test-simplex.C
+++ GrAL/geometry/test/simplex/test-simplex.C
@@ -3,6 +3,7 @@
 */
 
 #include "Geometry/simplex.h"
+#include "Utility/pre-post-conditions.h"
 
 template class simplex<0,float>;
 template class simplex<1,float>;
@@ -22,5 +23,45 @@ int main()
   simplex<3> s3B(3,2,1,0);
   simplex<3> s3C = s3A;
   simplex<3> s3D; s3D = s3B;
+
+  {
+    simplex<1> s1(1,2);
+    simplex<1> s2(2,1);
+
+    REQUIRE_ALWAYS( equal_ordered  ::equal(s1,s2), "", 1);
+    REQUIRE_ALWAYS(!equal_unordered::equal(s1,s2), "", 1);
+    REQUIRE_ALWAYS(!compare_ordered  ::less(s1,s2), "", 1);
+    REQUIRE_ALWAYS( compare_unordered::less(s1,s2), "", 1);
+
+    REQUIRE_ALWAYS( (s1 == s2), "", 1);
+    REQUIRE_ALWAYS(!(s1 != s2), "", 1);
+    REQUIRE_ALWAYS(!(s1 <  s2), "", 1);
+    REQUIRE_ALWAYS(!(s1 >  s2), "", 1);
+
+    simplex<1> s3(3,0);
+
+    REQUIRE_ALWAYS(!equal_ordered  ::equal(s1,s3), "", 1);
+    REQUIRE_ALWAYS(!equal_unordered::equal(s1,s3), "", 1);
+    REQUIRE_ALWAYS(!compare_ordered  ::less(s1,s3), "", 1);
+    REQUIRE_ALWAYS( compare_unordered::less(s1,s3), "", 1);
+
+    REQUIRE_ALWAYS(!(s1 == s3), "", 1);
+    REQUIRE_ALWAYS( (s1 != s3), "", 1);
+    REQUIRE_ALWAYS(!(s1 <  s3), "", 1);
+    REQUIRE_ALWAYS(!(s3 >  s1), "", 1);
+
+    simplex<1> s4(3,1);
+
+    REQUIRE_ALWAYS(!equal_ordered  ::equal(s1,s4), "", 1);
+    REQUIRE_ALWAYS(!equal_unordered::equal(s1,s4), "", 1);
+    REQUIRE_ALWAYS( compare_ordered  ::less(s1,s4), "", 1);
+    REQUIRE_ALWAYS( compare_unordered::less(s1,s4), "", 1);
+
+    REQUIRE_ALWAYS(!(s1 == s4), "", 1);
+    REQUIRE_ALWAYS( (s1 != s4), "", 1);
+    REQUIRE_ALWAYS( (s1 <  s4), "", 1);
+    REQUIRE_ALWAYS( (s4 >  s1), "", 1);
+
+  }
 }
 
