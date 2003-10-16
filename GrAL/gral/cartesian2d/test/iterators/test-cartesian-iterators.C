69d31eee3dfa77318c0d63e8970c2d62300d8fff
berti <>
2003-10-16 11:05:30 +0000

    added iterator tests for all iterators

--- GrAL/gral/cartesian2d/test/iterators/test-cartesian-iterators.C
+++ GrAL/gral/cartesian2d/test/iterators/test-cartesian-iterators.C
@@ -11,6 +11,8 @@ int main() {
   using namespace std;
   using namespace cartesian2d;
 
+  checkgt<grid_types<cartesian2d::CartesianGrid2D> >(cout);
+
   REQUIRE_ALWAYS( RegGrid2D::BeginArchetype()->NumOfVertices() == 4, "", 1);
 
   ofstream* out = new ofstream("vertex-iterator.out");
@@ -24,9 +26,26 @@ int main() {
            << R.NumOfVertices() << " vertices\n";
       REQUIRE_ALWAYS( (R.NumOfVertices() + R.NumOfCells() == R.NumOfEdges() + 1),
                       "Euler formula does not hold!\n",1);
-      test_vertex_iterator(R,*out);
-      test_vertex_on_cell_iterator(R,*out);
       *out << endl;
+
+      test_vertex_iterator(R,*out);
+      test_edge_iterator  (R,*out);
+      test_cell_iterator  (R,*out);
+      test_face_iterator  (R,*out);
+      test_facet_iterator  (R,*out);
+
+      test_vertex_on_vertex_iterator(R,*out);
+      test_cell_on_vertex_iterator  (R,*out);
+
+      test_vertex_on_edge_iterator (R,*out);
+      test_vertex_on_facet_iterator(R,*out);
+      test_vertex_on_face_iterator (R,*out);
+      test_vertex_on_cell_iterator (R,*out);
+
+      test_edge_on_cell_iterator  (R,*out);
+      test_edge_on_face_iterator  (R,*out);
+      test_cell_on_cell_iterator  (R,*out);
+
     }
   }
   delete out;
