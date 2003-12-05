c7bef6fc2f50930209318449c164c57500abb0e0
berti <>
2003-12-05 10:18:35 +0000

    corrected empty constructor to give empty box added templated construction added scaling added docs

--- GrAL/geometry/include/Geometry/box.h
+++ GrAL/geometry/include/Geometry/box.h
@@ -6,17 +6,20 @@
 
 #include <algorithm>
 #include "Geometry/point-traits.h"
+#include "Geometry/algebraic-primitives.h"
+
 #include <boost/limits.hpp>
 
 /*! \brief An axisparallel box for arbitrary coordinate types.
 
     This class is especially useful for representing bounding boxes.
-    Union (operator |) and intersection (operator &) of boxes give
+    Union (<tt> operator | </tt>) and intersection (<tt> operator & </tt>) of boxes give
     again boxes (namely bounding boxes of the set-theoretic union
     and intersection, resp.)
 
-    A box can be used as a function, mapping [0,1]^d to the convex
-    hull of its min and max corner, in the obvious way.
+    A box can be used as a function, mapping \f$ [0,1]^d \f$ to the convex
+    hull of its min and max corner <tt> [the_min(), the_max()]</tt>, 
+    in the obvious way.
 
     \see Tested in test-box.C 
  */
@@ -26,21 +29,64 @@
 template<class coord>
 class box {
   typedef box<coord>          self;
-  typedef point_traits<coord> pt;
+public:
+  typedef point_traits<coord>         pt;
   typedef typename pt::component_type scalar_type;
 private:
   coord minc, maxc;
 public:
-  box() : minc( std::numeric_limits<scalar_type>::infinity()),
-	  maxc(-std::numeric_limits<scalar_type>::infinity())
-  {}    // => |= works correctly
-  box(const coord& cmin) : minc(cmin), maxc(cmin) {}
-  box(const coord& cmin, const coord& cmax) : minc(cmin), maxc(cmax) 
-  {}
+  /*! \brief Create empty box
+   */
+  box() : minc( std::numeric_limits<scalar_type>::max()),
+	  maxc(-std::numeric_limits<scalar_type>::max())
+  {
+    REQUIRE(empty(), "minc=" << minc << " maxc=" << maxc,1);
+  }  
+  /*! \brief Create bounding box of the single point \c p
+   */
+  box(const coord& p) : minc(p), maxc(p) {}
+  /*! \brief Create bounding box of the set <tt> {cmin, cmax} </tt>
+   */
+  box(const coord& cmin, const coord& cmax) : minc(cmin), maxc(cmin)
+  { *this |= cmax; }
+  
+  /*! \brief Create bounding box of the sequence <tt> [begin, end[ </tt>
+   */
+  template<class INPUTITERATOR >
+  void init(INPUTITERATOR begin, INPUTITERATOR end)
+  {
+    minc = coord( std::numeric_limits<scalar_type>::max());
+    maxc = coord(-std::numeric_limits<scalar_type>::max());
+    while(begin != end) {
+      for(int i = pt::LowerIndex(minc); i <= pt::UpperIndex(minc); ++i) {
+	minc[i] = std::min(minc[i], (*begin)[i]);
+	maxc[i] = std::max(maxc[i], (*begin)[i]);
+      }
+      ++begin;
+    }
+  }
 
   const coord& the_min() const { return minc;}
   const coord& the_max() const { return maxc;}
   coord        center()  const { return 0.5*(minc+maxc);}
+
+  /*! \brief Scale by a factor \f$ s \geq 0 \f$
+     
+     The scaling is symmetric with respect to the center.
+     A scaling factor of \f$ s = 1 \f$ will leave the box unchanged.
+  */
+  void scale(scalar_type s) { 
+    REQUIRE(s >= 0.0, " s=" << s, 1);
+    coord c(center());
+    minc = c + s*(minc - c);
+    maxc = c + s*(maxc - c);
+  }
+  /*! \brief Translate the box by \c t
+   */
+  void translate(coord t) {
+    minc += t;
+    maxc += t;
+  }
 
   // dangerous - cannot check consistency
   coord& the_min()  { return minc;}
@@ -57,9 +103,16 @@ public:
   // function interface
   typedef coord argument_type;
   typedef coord result_type;
+  /*! \brief Map local coordinates to global coordinates
+     
+     \c local \f$ = (0, \ldots, 0) \mapsto \f$ \c the_min(), <br>
+     \c local \f$ = (1, \ldots, 1) \mapsto \f$ \c the_max().
+   */
   coord operator()(const coord& local) const
     { return global_coords(local); }
 
+  /*! \brief True iff \f$  p \in \f$ <tt> [the_min(), the_max()] </tt>
+   */
   bool contains(coord const& p) const {
     bool res = true;
     for(int i = pt::LowerIndex(p); i <= pt::UpperIndex(p); ++i) {
@@ -69,6 +122,10 @@ public:
   }
   
   // bool element(const coord& p) const;
+  /*! \brief True iff the box is empty.
+
+     The box is empty iff <tt> the_min()[i] > the_max()[i] </tt> for some \c i.
+  */
   bool empty() const {
     bool res = false;
     for(int i = pt::LowerIndex(minc); i <= pt::UpperIndex(minc); ++i) 
@@ -104,6 +161,30 @@ public:
     }
   */
 };
+
+/*! \brief Approximate Hausdorff distance of two bounding boxes
+   
+    This is not the exact Hausdorff distance.
+*/
+template<class COORD>
+inline
+typename box<COORD>::scalar_type distance(box<COORD> const& b1, box<COORD> const& b2)
+{
+  typedef algebraic_primitives<COORD> ap;
+  return std::max(ap::distance(b1.the_min(), b2.the_min()),
+		  ap::distance(b1.the_max(), b2.the_max()));
+}
+
+/*! \brief Diameter of a box.
+
+ */
+template<class COORD>
+inline
+typename box<COORD>::scalar_type diameter(box<COORD> const& b)
+{
+  typedef algebraic_primitives<COORD> ap;
+  return ap::distance(b.the_min(), b.the_max());
+}
 
 #endif
 
