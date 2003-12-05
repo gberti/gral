4cd78375bf2877ff80e3d450d954ed5383a82d75
berti <>
2003-12-05 10:19:06 +0000

    corrected empty constructor to give empty box added templated construction added scaling

--- GrAL/geometry/test/box/test-box.C
+++ GrAL/geometry/test/box/test-box.C
@@ -7,7 +7,9 @@
 #include "Geometry/box.h"
 #include "Geometry/coords.h"
 #include "Utility/pre-post-conditions.h"
+
 #include <iostream>
+#include <iomanip>
 
 
 template<class COORD>
@@ -19,18 +21,26 @@ inline void  print_contains(box<COORD> const& b, COORD const& p)
 }
 
 int main() {
+  using namespace std;
   typedef coordN<2> coord_type;
   typedef box<coord_type> box_type;
   
+  cout << "+numeric_limits<float> ::max() =  " << setprecision(numeric_limits<float> ::digits10) <<  numeric_limits<float>::max() << endl
+       << "-numeric_limits<float> ::max() = " << setprecision(numeric_limits<float> ::digits10) << -numeric_limits<float>::max() << endl
+       << "+numeric_limits<double>::max() =  " << setprecision(numeric_limits<double>::digits10) <<  numeric_limits<double>::max() << endl
+       << "-numeric_limits<double>::max() = " << setprecision(numeric_limits<double>::digits10) << -numeric_limits<double>::max() << endl;
+
+
   coord_type p_1(-1,-1);
   coord_type p0 (0,0);
   coord_type p1 (1,1);
 
   box_type b_10(p_1,p0);     REQUIRE_ALWAYS(!b_10.empty(), "",1);
-  box_type b0_1(p0 ,p_1);    REQUIRE_ALWAYS( b0_1.empty(), "",1);
+  box_type b0_1(p0 ,p_1);    REQUIRE_ALWAYS(!b0_1.empty(), "",1);
   box_type b_11(p_1,p1);     REQUIRE_ALWAYS(!b_11.empty(), "",1);
-  box_type b1_1(p1,p_1);     REQUIRE_ALWAYS( b1_1.empty(), "",1);
+  box_type b1_1(p1,p_1);     REQUIRE_ALWAYS(!b1_1.empty(), "",1);
 
+  box_type be;               REQUIRE_ALWAYS(be.empty(), "", 1);
   
   print_contains(b_10, p0);
   print_contains(b_10, p_1);
@@ -50,14 +60,22 @@ int main() {
   
   REQUIRE_ALWAYS( b_10.contains(p0),  "",1);  
   REQUIRE_ALWAYS( b_10.contains(p_1), "",1);  
-  REQUIRE_ALWAYS(!b0_1.contains(p0),  "",1);  
-  REQUIRE_ALWAYS(!b0_1.contains(p_1), "",1);  
+  REQUIRE_ALWAYS( b0_1.contains(p0),  "",1);  
+  REQUIRE_ALWAYS( b0_1.contains(p_1), "",1);  
 
 
 
 
   REQUIRE_ALWAYS( b_11.contains(p0), "", 1);
-  REQUIRE_ALWAYS(!b1_1.contains(p0), "", 1);
+  REQUIRE_ALWAYS( b1_1.contains(p0), "", 1);
+
   
+  typedef coord_type ct;
+  ct points[] = { ct(1.0,1.0), ct(2.0,1.0), ct(2.0, 2.0), ct(1.0, 2.0)};
+  
+  box<coord_type> b;
+  b.init(points+0, points+sizeof(points)/sizeof(ct));
+  REQUIRE_ALWAYS(b.the_min() == ct(1.0,1.0), " b.the_min()=" << b.the_min(), 1);
+  REQUIRE_ALWAYS(b.the_max() == ct(2.0,2.0), " b.the_max()=" << b.the_max(), 1);
 
 }
