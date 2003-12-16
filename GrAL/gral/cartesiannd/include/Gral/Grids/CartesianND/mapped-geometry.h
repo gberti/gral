5759138fd637b0976a7fd208deb703586b82be3b
berti <>
2003-12-16 12:50:36 +0000

    added length(Edge), barycenter(Cell) changed default mapping to isotropic cells

--- GrAL/gral/cartesiannd/include/Gral/Grids/CartesianND/mapped-geometry.h
+++ GrAL/gral/cartesiannd/include/Gral/Grids/CartesianND/mapped-geometry.h
@@ -21,22 +21,38 @@ namespace cartesiannd {
   };
 
 
+  /*! \brief A geometry for a Cartesian grid defined by an analytic mapping
+    
+     The mapping is assumed to be defined on \f$ [0,1]^d \f$.
+     Each vertex gets first assigned a unit coordinate in \f$ [0,1]^d \f$, 
+     such that cells are isotropic hypercubes. This means that in the case of grids
+     with different number of vertices in each direction, only the vertices
+     with the largest integer indices will have a corresponding unit coordinate of 1.
+     For example, in a grid with 4x2 cells, the right upper vertex (index (4,2)) will be mapped
+     to \f$(1, 0.5) \f$.
+
+     \note In the case of a nonlinear mapping, some predicates do not work correctly.
+   */
   template<class CARTGRID, class MAP> //  = identity<cartesiannd::default_coord<CARTGRID>::type > >
   class mapped_geometry {
   public:
     typedef CARTGRID grid_type;
     typedef MAP      mapping_type;
-    typedef typename mapping_type::result_type coord_type;
-    typedef grid_types<grid_type>    gt;
-    typedef point_traits<coord_type> pt;
 
-    typedef typename gt::Vertex Vertex;
-    typedef typename gt::Cell   Cell;
+    typedef typename mapping_type::result_type coord_type;
+    typedef point_traits<coord_type>           pt;
+    typedef algebraic_primitives<coord_type>   ap;
+    typedef typename pt::component_type        scalar_type;
+
+    typedef grid_types<grid_type>   gt;
+    typedef typename gt::Vertex     Vertex;
+    typedef typename gt::Edge       Edge;
+    typedef typename gt::Cell       Cell;
     typedef typename gt::index_type index_type;
 
     typedef point_location_result<gt, coord_type> location_result_type;
   private:
-    ref_ptr<grid_type const> g;
+    ref_ptr<grid_type    const> g;
     ref_ptr<mapping_type const> f;     // defined on unit cube (at least).
     ref_ptr<mapping_type const> f_inverse;
 
@@ -47,17 +63,41 @@ namespace cartesiannd {
   public:
     mapped_geometry() {}
     mapped_geometry(grid_type const& gg, mapping_type ff = mapping_type()) 
-      : g(gg), f(new mapping_type(ff)), low_(gg.low_vertex_index()), high_(gg.high_vertex_index()) { init();}
+      : g(gg), 
+	low_(gg.low_vertex_index()), 
+	high_(gg.high_vertex_index()) 
+    { 
+      f.make_own(new mapping_type(ff));
+      init();
+      preserve_aspect_ratio();
+    }
+
     mapped_geometry(grid_type const& gg, index_type lw, index_type hgh, 
 		    mapping_type ff = mapping_type()) 
-      : g(gg), f(new mapping_type(ff)), low_(lw), high_(hgh) { init();}
+      : g(gg),
+	low_(lw), high_(hgh) 
+    { 
+      f.make_own(new mapping_type(ff));
+      init();
+    }
 
-    void set_inverse_mapping(mapping_type inv) { f_inverse = new mapping_type(inv);}
+    void set_inverse_mapping(mapping_type inv) 
+    { f_inverse.make_own(new mapping_type(inv));}
 
     void init() {
       delta = coord_type(1.0);
-      delta = quotient(delta, high_ - low_);
+      delta = quotient(delta, coord_type(high_ - low_));
     }
+    void preserve_aspect_ratio() 
+    {
+      scalar_type mx = * std::min_element(delta.begin(), delta.end());
+      delta = coord_type(mx);
+    }
+
+    void set_mapping(mapping_type const& ff) {
+      f.make_own(new mapping_type(ff));
+    }
+
     void rebind(grid_type const& gg) {
       g = ref_ptr<grid_type const>(gg);
       low_  = g->low_vertex_index();
@@ -78,20 +118,41 @@ namespace cartesiannd {
     }
     //! index i such that local_p is in cell with index i
     index_type unit2index(coord_type local_p) const {
+      //coord_type rational_index = quotient(local_p, delta);
+      //index_type integral_index = floor_tuple(rational_index);
       return floor_tuple(quotient(local_p, delta)) + coord_type(low_);
+
     }
     coord_type coord (Vertex const& v) const { cb(); return (*f)(unit_coord(v.index()));}
+    /*! \brief length of an edge.
+        
+       \note The result is only correct in the case of an affine mapping
+     */
+    scalar_type length(Edge   const& e) const { 
+      cb(); 
+      return ap::distance(coord(e.Vl(typename Edge::local_index_type(0))), 
+			  coord(e.Vl(typename Edge::local_index_type(1))));
+    }
     /*! \brief Result is the mapping of the center of c with respect to unit coordinates \f$ \in [0,1]^d \f$
         
         This is the center of inertia only if the mapping \c f is affine. 
     */
     coord_type center(Cell   const& c) const { cb(); return (*f)(unit_coord(c.index()) + 0.5*delta);} 
+    coord_type barycenter(Cell const& c) const { 
+      cb();
+      coord_type res(0.0);
+      for(typename gt::VertexOnCellIterator vc(c); !vc.IsDone(); ++vc)
+	res += coord(*vc);
+      res *= 1.0/c.NumOfVertices();
+      return res;
+    }
 
     bool bound() const { return g != 0;}
     void cb()    const { REQUIRE(bound(), "", 1);}
 
     /*! \brief locate point \c pp in grid
 
+       \note Points on the high boundary will be reported as projected, not as inside the grid.
      */
     template<class COORD>
     location_result_type locate(COORD const& pp) const;
@@ -116,7 +177,8 @@ namespace cartesiannd {
 	index_type projected_cell    = clamp_tuple(TheGrid()->low_cell_index(), TheGrid()->high_cell_index(), cell_of_p);
 	coord_type projected_local_p = clamp_tuple(unit_coord(TheGrid()->low_vertex_index()), 
 						   unit_coord(TheGrid()->high_vertex_index()), local_p);
-        // FIXME: Problems occur for points at 'upper' grid boundary (one coordinate = higher grid coordinate)
+        // FIXME: Problems occur for points at 'upper' grid boundary (at least one coordinate 
+	// = highest grid coordinate in this direction)
 	// These will be rounded to outside cell => should measure relative difference of local_p and projected_local_p 
 	// This is a real problem only for exact arithmetic, though.
 	return location_result_type(Cell(TheGrid(), projected_cell), (*f)(projected_local_p), 
