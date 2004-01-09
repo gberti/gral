b6e3de6e9d4bd6fc8b6b2a74aeda6aec38a68b1e
berti <>
2004-01-09 09:31:59 +0000

    added template<>

--- GrAL/gral/distributed/include/Gral/Distributed/mpi-types.h
+++ GrAL/gral/distributed/include/Gral/Distributed/mpi-types.h
@@ -18,22 +18,28 @@
 
 template<class T> struct mpi_types {};
 
+template<>
 struct mpi_types<int> { 
   static MPI_Datatype tag() { return MPI_INTEGER; }
 };
 
+template<>
 struct mpi_types<short> { 
   static MPI_Datatype tag() { return MPI_SHORT; }
 };
 
+
+template<>
 struct mpi_types<float> { 
   static MPI_Datatype tag() { return MPI_FLOAT; }
 };
 
+template<>
 struct mpi_types<double> { 
   static MPI_Datatype tag() { return MPI_DOUBLE; }
 };
 
+template<>
 struct mpi_types<long double> { 
   static MPI_Datatype tag() { return MPI_LONG_DOUBLE; }
 };
