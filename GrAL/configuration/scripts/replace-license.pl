9d72daa86fbad919e7b44b2f4d1bf5af21a1faf5
berti <>
2004-04-13 08:48:46 +0000

    replace LICENSE_NEC_XXXX

--- GrAL/configuration/scripts/replace-license.pl
+++ GrAL/configuration/scripts/replace-license.pl
@@ -6,10 +6,10 @@ exec perl -w -x $0 ${1+"$@"} # -*- mode: perl; perl-indent-level: 2; -*-
 $license1 = "
 /* ------------------------------------------------------------
 
-    Copyright (C) 1997-2003 Guntram Berti,
+    Copyright (C) 1997-2004 Guntram Berti,
     NEC C&C Research Laboratories, NEC Europe Ltd.,
     Rathausallee 10, D-53757 St. Augustin, Germany.
-    Contact: berti\@ccrl-nece.de
+    Contact: berti AT ccrl-nece DOT de
 
     This file is part of the Grid Algorithms Library (GrAL),
     which is available at
@@ -36,7 +36,10 @@ $license = $license1;
 
 #print "executing replace-license.pl\n";
 while(<>) {
+    s|//[ ]*\$LICENSE_NEC_2004|$license|;
     s|//[ ]*\$LICENSE_NEC_2003|$license|;
+    s|//[ ]*\$LICENSE_NEC_2002|$license|;
+    s|//[ ]*\$LICENSE_NEC_2001|$license|;
     s|//[ ]*\$LICENSE_NEC|$license|;
     s|//[ ]*\$LICENSE|$license|;
     print;
