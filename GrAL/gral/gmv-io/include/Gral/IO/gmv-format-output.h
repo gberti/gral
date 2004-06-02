941c1e976b440cb83bd2c881b3eaf7a6bbad9f79
berti <>
2004-06-02 14:08:55 +0000

    added support for material

--- GrAL/gral/gmv-io/include/Gral/IO/gmv-format-output.h
+++ GrAL/gral/gmv-io/include/Gral/IO/gmv-format-output.h
@@ -5,13 +5,25 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include "Gral/Base/common-grid-basics.h"
 
 #include "Container/heterogeneous-list.h"
+#include "Container/histogram-table.h"
+#include "Container/bijective-mapping.h"
+
 #include "Utility/as-string.h"
 
+#include <boost/type_traits.hpp>
+
 namespace GrAL {
+
+  struct true_type{};
+  struct false_type {};
+  template<bool B> struct decide        { typedef true_type  type;};
+  template<>       struct decide<false> { typedef false_type type;};
+
 
 /*! \brief Output adapter for the GMV format base class.
    
@@ -25,6 +37,7 @@ protected:
   std::ostream * out;
   bool           owned;
   int            num_vars;
+  bool           material_treat_special;
 
   void copy(self const& rhs);
 
@@ -41,6 +54,8 @@ public:
   void init(std::string const& nm);
 
   std::ostream& Out() { return *out;}
+
+  void output_materials() { material_treat_special = true;}
 
   template<int N>
   std::string element_tag(vertex_type_tag, grid_dim_tag<N>) const { return "1";}
@@ -75,7 +90,7 @@ private:
 
       *out << varname << " ";
       *out << element_tag(element_type_tag(), grid_dimension_tag());
-      *out << '\n';;
+      *out << '\n';
       for(typename et::ElementIterator e(gf.TheGrid()); ! e.IsDone(); ++e) {
 	// we assume  numbering in gf.TheGrid() 
 	// is the same as numbering in GMV file.
@@ -83,17 +98,80 @@ private:
       }
       *out << '\n';
     }
+  template<class GF>
+  void copy_material(GF const& gf)
+  { 
+    typedef typename GF::value_type vt;
+    copy_material(gf, decide<boost::is_integral<vt>::value>::type()); 
+  }
+
+  template<class GF>
+  void copy_material(GF const& gf, false_type) {}
+ 
+  template<class GF>
+  void copy_material(GF const& gf, true_type) 
+  {
+    typedef element_traits<typename GF::element_type> et;
+    typedef typename et::element_type_tag   element_type_tag;
+    typedef typename et::grid_dimension_tag grid_dimension_tag;
+
+    // map material IDs to consecutive range [1, nmats]
+    typedef histogram_table<typename GF::value_type> hist_table_type;
+    hist_table_type hist(gf.begin(), gf.end());
+    unsigned nmats = hist.size();
+    if(nmats > 128) {
+      std::cerr << "WARNING: GMV supports only 128 materials, but " << nmats << " were given!" << std::endl;
+      nmats = 128;
+    }
+
+    std::vector<typename GF::value_type> mat; // (hist.size());
+    for(typename hist_table_type::const_iterator it = hist.begin(); it != hist.end(); ++it)
+      mat.push_back((*it).first);
+    std::sort(mat.begin(), mat.end());
+    bijective_mapping<typename GF::value_type, int> new_mat;
+    for(unsigned m = 0; m < nmats; ++m)
+      new_mat[mat[m]] = m+1;
+    // map possible extraneous materials
+    for(unsigned m = nmats; m < mat.size(); ++m)
+      new_mat[mat[m]] = nmats;
+
+
+    *out << "material "
+	 << nmats << " "
+	 << element_tag(element_type_tag(), grid_dimension_tag())
+	 << "\n";
+    for(unsigned m = 0; m < nmats; ++m)
+      *out << "mat" << mat[m] << " ";
+    *out << "\n";
+    for(typename et::ElementIterator e(gf.TheGrid()); ! e.IsDone(); ++e) {
+      *out << new_mat(gf(*e)) << " ";
+    }
+    *out << "\n";
+  }
 
   typedef heterogeneous_list::END END;
   void copy_grid_functions_rec(heterogeneous_list::List<END,END>) {}
+ typedef heterogeneous_list::END END;
+  void copy_materials_rec(heterogeneous_list::List<END,END>) {}
 
 
   template<class GF, class TAIL>
   void copy_grid_functions_rec(heterogeneous_list::List<GF,TAIL> gfs)
     { 
-      num_vars++;
-      copy_gf(*(gfs.head().gf), gfs.head().name);
+      if( ! (material_treat_special && gfs.head().name == "material")) {
+	num_vars++;
+	copy_gf(*(gfs.head().gf), gfs.head().name);
+      }
       copy_grid_functions_rec(gfs.tail());
+    }
+
+  template<class GF, class TAIL>
+  void copy_materials_rec(heterogeneous_list::List<GF,TAIL> gfs)
+    { 
+      if(material_treat_special && gfs.head().name == "material") {
+        copy_material(*(gfs.head().gf));
+      }
+      copy_materials_rec(gfs.tail());
     }
 
  
@@ -106,6 +184,7 @@ public:
       begin_variable();
       copy_grid_functions_rec(gfs);
       end_variable(); 
+      copy_materials_rec(gfs);
     }
   
 };
--- GrAL/gral/gmv-io/src/IO/gmv-format-output.C
+++ GrAL/gral/gmv-io/src/IO/gmv-format-output.C
@@ -6,13 +6,13 @@
 namespace GrAL {
   
 OstreamGMVFmt_base::OstreamGMVFmt_base()
-  : out(0), owned(false) {}
+  : out(0), owned(false), num_vars(0), material_treat_special(false) {}
 
 OstreamGMVFmt_base::OstreamGMVFmt_base(std::ostream& ot)
-  : out(&ot), owned(false), num_vars(0) {}
+  : out(&ot), owned(false), num_vars(0), material_treat_special(false) {}
 
 OstreamGMVFmt_base::OstreamGMVFmt_base(std::string const& nm)
-  : out(new std::ofstream(nm.c_str())), owned(true), num_vars(0) {}
+  : out(new std::ofstream(nm.c_str())), owned(true), num_vars(0), material_treat_special(false) {}
 
 OstreamGMVFmt_base::~OstreamGMVFmt_base() 
 { if(owned) delete out;}
@@ -23,6 +23,7 @@ void OstreamGMVFmt_base::copy(OstreamGMVFmt_base const& rhs)
   out = rhs.out;
   num_vars = rhs.num_vars;
   owned = false;
+  material_treat_special = rhs.material_treat_special;
   // offset = rhs.offset;
   //  nv = rhs.nv;
   // nc = rhs.nc;
