1c326a6f92d4ff61579f8f5104fcadb66f190d0b
berti <>
2004-12-16 10:51:05 +0000

    added print() and empty()

--- GrAL/sequence/include/Container/histogram-table.h
+++ GrAL/sequence/include/Container/histogram-table.h
@@ -76,15 +76,27 @@ public:
 
   //! number of different entries
   size_type size() const { return the_map.size();}
+  //! true if <code>size() == 0</code>
+  bool empty() const { return the_map.empty();} 
 
   /*! \brief STL conforming iterators
    */
   //@{
-  iterator begin() { return the_map.begin();}
-  iterator end()   { return the_map.end();}
+  iterator       begin()       { return the_map.begin();}
+  iterator       end()         { return the_map.end();}
   const_iterator begin() const { return the_map.begin();}
   const_iterator end()   const { return the_map.end();}
   //@}
+
+  void print(std::ostream& out, 
+	     std::string const& key_label = " ",
+	     std::string const& num_label = "\n") const
+  {
+    for(const_iterator it = begin(); it != end(); ++it) {
+      // int w = int(ceil(log10((*this)(max_entry()))));
+      out << (*it).first << key_label << (*it).second << num_label;
+    }
+  }
 
   //! returns key with maximal number of entries 
   key_type max_entry() const {
@@ -99,6 +111,30 @@ public:
     return res;
   }
 };
+
+  template<class T>
+  class histogram_printer {
+  private:
+    histogram_table<T> const& hist;
+    std::string key_label;
+    std::string num_label;
+  public:
+    histogram_printer(histogram_table<T> const& h,
+		      std::string const& key_label_ = " ",
+		      std::string const& num_label_ = "\n") 
+			: hist(h), key_label(key_label_), num_label(num_label_) {}
+    void p(std::ostream& out) const { hist.print(out, key_label, num_label);}
+ };
+
+  template<class T>
+  std::ostream& operator<<(std::ostream& out, histogram_printer<T> const& h) { h.p(out); return out;}
+
+  template<class T>
+  histogram_printer<T> print_histogram(histogram_table<T> const& hist, 
+				       std::string const& key_label = " ",
+				       std::string const& num_label = "\n")
+  { return histogram_printer<T>(hist, key_label, num_label);}
+  
 
 } // namespace GrAL 
 
