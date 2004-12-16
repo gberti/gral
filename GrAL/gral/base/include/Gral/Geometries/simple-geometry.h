0a15a7d0acb9f48f82d72453dd76d3b1faea43af
berti <>
2004-12-16 14:47:16 +0000

    docs added, added area()/length()/volume()

--- GrAL/gral/base/include/Gral/Geometries/simple-geometry.h
+++ GrAL/gral/base/include/Gral/Geometries/simple-geometry.h
@@ -8,6 +8,7 @@
 #include "Utility/pre-post-conditions.h"
 #include "Geometry/point-traits.h"
 #include "Geometry/algebraic-primitives.h"
+#include "Gral/Geometries/geometric-types-2d.h"
 
 namespace GrAL {
 
@@ -15,10 +16,11 @@ namespace GrAL {
 
     \ingroup  gridgeometries
     \see \ref gridgeometries module
-    \todo Test
+    \see Tested in \ref test-simple-geometry.C
  */
 template<class GRID, class COORD> 
 class simple_geometry : public grid_types<GRID> {
+  typedef simple_geometry<GRID, COORD> self;
  public:
   typedef COORD               coord_type;
   typedef grid_types<GRID>    gt;
@@ -35,20 +37,60 @@ class simple_geometry : public grid_types<GRID> {
 
 public:
   simple_geometry() {}
+  /*!  \brief Initialize with grid \c g.
+     The geometrie (vertex coordinates) still need to be initialized.
+   */
   simple_geometry(GRID const& g) : coords(g) {}
 
+  /*! \brief Construct with coordinate sequence
+    
+      The sequence <tt> [begin_coord, end_coord[ </tt> corresponds to the 
+      vertex sequence <tt> [g.FirstVertex(), g.EndVertex()[ </tt>
+   */
   template<class It>
   simple_geometry(GRID const& g, It begin_coord, It end_coord) : coords(g)
   { 
     copy(begin_coord, end_coord);
   }
+
+  /*! \brief Initialize grid and coordinates
+    
+      The sequence <tt> [begin_coord, end_coord[ </tt> corresponds to the 
+      vertex sequence <tt> [g.FirstVertex(), g.EndVertex()[ </tt>
+
+      The code sequence 
+      \code
+      simple_geometry<grid_type, coord_type> geom;
+      geom.init(grid, coords.begin(), coords.end());
+      \endcode
+      is equivalent to
+      \code
+      simple_geometry<grid_type, coord_type> geom(grid, coords.begin(), coords.end())
+      \endcode
+   */
   template<class It>
   void init(GRID const& g, It begin_coord, It end_coord) 
   {
     coords.set_grid(g);
     copy(begin_coord, end_coord);
   }
+  /*! \brief Initialize (or overwrite) coordinates.
 
+      \pre The object is bound to a grid.
+
+      The sequence <tt> [begin_coord, end_coord[ </tt> corresponds to the 
+      vertex sequence <tt> [TheGrid().FirstVertex(), TheGrid().EndVertex()[ </tt>
+
+      The code sequence 
+      \code
+      simple_geometry<grid_type, coord_type> geom(grid);
+      geom.copy(grid, coords.begin(), coords.end());
+      \endcode
+      is equivalent to
+      \code
+      simple_geometry<grid_type, coord_type> geom(grid, coords.begin(), coords.end())
+      \endcode
+   */
   template<class It>
   void copy(It begin_coord, It end_coord) {
     for(typename gt::VertexIterator v(TheGrid()); ! v.IsDone(); ++v, ++begin_coord)
@@ -60,13 +102,34 @@ public:
   void rebind  (grid_type const& g) { coords.rebind(g);}
 
   bool empty() const { cb(); return TheGrid().NumOfVertices() == 0;}
-  // does not work for empty grid
+
+  /*! \brief Dimension of the embedding space
+      \todo This does not work for an empty grid - use compile-time dimension if possible,
+      or pass an extra parameter for the dimension if \c coord_type has a run-time dimension only.
+  */
   int space_dimension() const { cb(); return pt::Dim(coords(* TheGrid().FirstVertex()));}
 
+  //! Coordinate of vertex \c v
   coord_type const& coord(Vertex const& v) const { cb(); return coords(v);}
+  //! Coordinate of vertex \c v (read/write access)
   coord_type      & coord(Vertex const& v)       { cb(); return coords[v];}
 
-  scalar_type length(Edge const& e) const { return ap::distance(coord(e.V1()), coord(e.V2()));}
+  //! \brief Euclidean lenght of edge \c e
+  scalar_type length(typename gt::Edge const& e) const { cb(); return ap::distance(coord(e.V1()), coord(e.V2()));}
+  //! \brief Euclidean length of edge \c e
+  scalar_type volume(typename gt::Edge const& e) const {       return length(e);}
+
+  //! Area of the face \c f
+  scalar_type area(typename gt::Face const& f) const { 
+    cb();
+    Polygon2d<typename gt::Face,self> pf(f, *this);
+    return pf.convex_area();
+  }
+  //! \brief Area of the face \c f
+  scalar_type volume(typename gt::Face const& f) const { return area(f);}
+ 
+
+  //! \brief Barycenter of the vertices of cell \c c 
   coord_type barycenter(Cell const& c) const { 
     cb();
     coord_type res(0.0);
