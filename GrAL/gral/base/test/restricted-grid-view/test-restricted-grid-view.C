b7cf11159dc1c45688ed3049ad4ede1f273e1bbe
berti <>
2005-04-04 16:26:52 +0000

    test grid_function_view

--- GrAL/gral/base/test/restricted-grid-view/test-restricted-grid-view.C
+++ GrAL/gral/base/test/restricted-grid-view/test-restricted-grid-view.C
@@ -6,17 +6,22 @@
 
 
 #include "Gral/Views/restricted-grid-view.h"
+#include "Gral/Base/restricted-grid-function-adapter.h"
+
 #include "Gral/Grids/Cartesian2D/all.h"
 #include "Gral/Test/all.h"
 
+#include "Gral/Base/element-numbering.h"
 
 #include "Utility/pre-post-conditions.h"
 
+#include <algorithm>
 
 typedef GrAL::cartesian2d::CartesianGrid2D cart_grid_type;
 
-struct true_pred  { bool operator()(cart_grid_type::Cell) const { return true; } };
-struct false_pred { bool operator()(cart_grid_type::Cell) const { return false;} };
+struct true_pred  { bool operator()(cart_grid_type::Cell)  const { return true; } };
+struct false_pred { bool operator()(cart_grid_type::Cell)   const { return false;} };
+struct even_pred  { bool operator()(cart_grid_type::Cell c) const { return ( (c.handle() % 2) == 0 ? true : false); } };
 
 // using namespace GrAL;
 namespace GrAL {
@@ -34,11 +39,12 @@ int main() {
 
   typedef cartesian2d::CartesianGrid2D grid_type;
   typedef grid_types<grid_type>        gt;
-  grid_type R(2,2);
+  grid_type R(3,3);
   
   namespace rgv = restricted_grid_view;
-  rgv::grid_view<grid_type, true_pred >  all_view(R, true_pred());
-  rgv::grid_view<grid_type, false_pred>  nil_view(R, false_pred());
+  rgv::grid_view<grid_type, true_pred >  all_view (R, true_pred());
+  rgv::grid_view<grid_type, false_pred>  nil_view (R, false_pred());
+  rgv::grid_view<grid_type, even_pred >  even_view(R, even_pred());
   
 
   REQUIRE_ALWAYS( all_view.NumOfCells() == R.NumOfCells(), "", 1);
@@ -46,8 +52,9 @@ int main() {
   REQUIRE_ALWAYS( all_view.NumOfVertices() == R.NumOfVertices(), "", 1);
   REQUIRE_ALWAYS( nil_view.NumOfVertices() == 0,              "", 1);
 
-  typedef grid_types<rgv::grid_view<grid_type, true_pred >  > truegt;
+  typedef grid_types<rgv::grid_view<grid_type, true_pred  > > truegt;
   typedef grid_types<rgv::grid_view<grid_type, false_pred > > falsegt;
+  typedef grid_types<rgv::grid_view<grid_type, even_pred  > > evengt;
 
 
   /* cannot construct cells/vertices from handles
@@ -61,5 +68,22 @@ int main() {
   truegt::CellIterator tc(all_view);
   for( ; !c.IsDone(); ++c, ++tc)
     REQUIRE_ALWAYS( *c == *tc, "", 1);
+
+  grid_function<gt::Cell, int> gf(R,0);
+  grid_function_view<evengt::Cell, int> even_gf(even_view, gf);
+
+  grid_function_view<evengt::Cell, int, const grid_function<gt::Cell, int> > even_const_gf(even_view, gf);
+
+  REQUIRE_ALWAYS(even_gf.size() == even_view.NumOfCells(), "", 1);
+
+  for(evengt::CellIterator ec(even_view); !ec.IsDone(); ++ec) {
+    even_gf[*ec] = 1;
+    int i = even_const_gf(*ec);
+  }
+
+  bool eq = std::equal(even_gf.begin(), even_gf.end(), even_const_gf.begin());
+
+
+  element_numbering<gt::Vertex>  VNum(R,1);
 
 }
