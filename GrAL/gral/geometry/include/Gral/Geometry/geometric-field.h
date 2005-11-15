f53edfbfd625e31cb08397831704fc877b16fb06
berti <>
2005-11-15 10:24:49 +0000

    made handling of points outside underlying grid configurable

--- GrAL/gral/geometry/include/Gral/Geometry/geometric-field.h
+++ GrAL/gral/geometry/include/Gral/Geometry/geometric-field.h
@@ -23,18 +23,24 @@ public:
   typedef typename GT::Vertex            Vertex;
   typedef typename function_type::value_type result_type;
   typedef point_locator<grid_type, geom_type> point_locator_type;
+
+  typedef typename point_locator_type::location_result_type loc_result_type;
+
 private:
+
   ref_ptr<geom_type const>     the_geom;
   ref_ptr<function_type const> f;
   // could be shared by many instances ...
   ref_ptr<point_locator_type>  loc;
+  result_type outside_value; 
+  bool        outside_value_set;
 public:
-  geometric_field() 
+  geometric_field() : outside_value_set(false)
   {
     loc.make_shared(new point_locator_type());
   }
   geometric_field(geom_type const& geo,
-		  function_type const& ff) : the_geom(geo), f(ff)
+		  function_type const& ff) : the_geom(geo), f(ff), outside_value_set(false)
   {
     loc.make_shared(new point_locator_type(TheGrid(), TheGeometry()));
     loc->init();
@@ -53,8 +59,9 @@ public:
 
   result_type operator()(coord_type X) const 
   { 
-    typedef typename point_locator_type::location_result_type loc_result_type;
     loc_result_type res = loc->locate(X);
+    if(outside_value_set && res.tag() != loc_result_type::inside_tag)
+      return outside_value;
     if(res.tag() == loc_result_type::outside_tag)
       std::cout << " field(" << X << "): No cell found!" << std::endl; 
     Cell c = loc->locate(X).TheCell();
@@ -72,6 +79,12 @@ public:
     // this is a Problem on hybrid grids
     return temporary<interpolator_type>(interpolator_type(*the_geom, *f, c));
   }
+
+
+  void set_outside_value(result_type v) { outside_value = v; outside_value_set = true;}
+  void unset_outside_valu() { outside_value_set = false;}
+
+  bool inside(coord_type X) const { return loc->locate(X).tag() == loc_result_type::inside_tag;}
 
   ref_ptr<geom_type const> TheGeometry() const { return the_geom;}
   ref_ptr<grid_type const> TheGrid()     const { return ref_ptr<grid_type const>(the_geom->TheGrid());}
