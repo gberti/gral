6292d762b6278565cdadb4c51e7b03cd246f5d30
berti <>
2006-02-03 16:44:25 +0000

    new implementation, using metafunctions instead of deep inheritance (did cause exorbitant compile times for Intel icc) Now every type is formally defined (as grid_types_detail::undefined), and has_XXX has been changed accordingly

--- GrAL/gral/base/include/Gral/Base/internal/type-aliases.h
+++ GrAL/gral/base/include/Gral/Base/internal/type-aliases.h
@@ -3,141 +3,188 @@
 
 // $LICENSE_NEC_2006
 
+#include "Gral/Base/type-tags.h"
 #include "Gral/Base/internal/compile-time-reflection.h"
+#include "Gral/Base/internal/iterator-type-maps.h"
 
-namespace GrAL { namespace grid_types_detail {
+namespace GrAL { 
 
-  // Automatically define dimension dependent aliases for types
+  namespace grid_types_detail { namespace type_aliases {
 
-#define DEFINE_A_AS_B(A,B) \
-  template<class GT,int HAS_A, int HAS_B> \
-  struct map_ ## A ## _as_ ## B ## _aux          : public GT {}; \
-  template<class GT> \
-  struct map_ ## A ## _as_ ## B ## _aux <GT,1,0> : public GT { typedef typename GT::A B;}; \
-  \
-  template<class GT> struct map_ ## A ## _as_ ## B \
-   : public map_ ## A ## _as_ ## B ## _aux<GT, has_##A<GT>::result, has_##B<GT>::result> {}; 
+    template<class GT, int D> struct  dim_dep_defs_dim : public GT {};
 
-#define DEFINE_A_IS_B(A,B) \
-  DEFINE_A_AS_B(A,B); \
-  DEFINE_A_AS_B(B,A); \
-  template<class GT> class  A ## _is_ ## B \
-   : public map_ ## A ## _as_ ## B < map_ ## B ## _as_ ## A <GT> > {};
 
-#define DEFINE_A_IS_B_ALL_INC(A,B) \
-  DEFINE_A_IS_B(A ## OnVertexIterator, B ## OnVertexIterator); \
-  DEFINE_A_IS_B(A ## OnEdgeIterator,   B ## OnEdgeIterator); \
-  DEFINE_A_IS_B(A ## OnFaceIterator,   B ## OnFaceIterator); \
-  DEFINE_A_IS_B(A ## OnFacetIterator,  B ## OnFacetIterator); \
-  DEFINE_A_IS_B(A ## OnCellIterator,   B ## OnCellIterator); \
-  DEFINE_A_IS_B(VertexOn ## A ## Iterator, VertexOn ## B ## Iterator); \
-  DEFINE_A_IS_B(EdgeOn   ## A ## Iterator, EdgeOn   ## B ## Iterator); \
-  DEFINE_A_IS_B(FaceOn   ## A ## Iterator, FaceOn   ## B ## Iterator); \
-  DEFINE_A_IS_B(FacetOn  ## A ## Iterator, FacetOn  ## B ## Iterator); \
-  DEFINE_A_IS_B(CellOn   ## A ## Iterator, CellOn   ## B ## Iterator); \
+    // specializations for D = 0 - 3
+
+    template<class GT> 
+    struct dim_dep_defs_dim<GT,0> : public GT {
+
+      typedef typename element_handle_d    <GT,0>  ::type vertex_handle;
+      typedef typename element_handle_d    <GT,0>  ::type cell_handle;
+
+      typedef typename element_d           <GT,0>  ::type Vertex;
+      typedef typename element_d           <GT,0>  ::type Cell;
+
+      typedef typename sequence_iterator_d <GT,0>  ::type VertexIterator;
+      typedef typename sequence_iterator_d <GT,0>  ::type CellIterator;
+
+      typedef typename incidence_iterator_d<GT,0,0>::type VertexOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type VertexOnCellIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type CellOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type CellOnCellIterator;
+    };
 
 
 
-  // 0D
-  DEFINE_A_IS_B(Vertex,Cell);
-  DEFINE_A_IS_B(vertex_handle, cell_handle);
-  DEFINE_A_IS_B(VertexIterator,CellIterator);
-  DEFINE_A_IS_B_ALL_INC(Vertex,Cell);
+
+    template<class GT> 
+    struct dim_dep_defs_dim<GT,1> : public GT {
+
+      typedef typename element_handle_d    <GT,0>  ::type vertex_handle;
+      typedef typename element_handle_d    <GT,0>  ::type facet_handle;
+      typedef typename element_handle_d    <GT,1>  ::type edge_handle;
+      typedef typename element_handle_d    <GT,1>  ::type cell_handle;
+
+      typedef typename element_d           <GT,0>  ::type Vertex;
+      typedef typename element_d           <GT,0>  ::type Facet;
+      typedef typename element_d           <GT,1>  ::type Edge;
+      typedef typename element_d           <GT,1>  ::type Cell;
+
+      typedef typename sequence_iterator_d <GT,0>  ::type VertexIterator;
+      typedef typename sequence_iterator_d <GT,0>  ::type FacetIterator;
+      typedef typename sequence_iterator_d <GT,1>  ::type EdgeIterator;
+      typedef typename sequence_iterator_d <GT,1>  ::type CellIterator;
+
+      typedef typename incidence_iterator_d<GT,0,0>::type VertexOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type VertexOnFacetIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type FacetOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,0,0>::type FacetOnFacetIterator;
+
+      typedef typename incidence_iterator_d<GT,0,1>::type VertexOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,0,1>::type VertexOnCellIterator;
+      typedef typename incidence_iterator_d<GT,0,1>::type FacetOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,0,1>::type FacetOnCellIterator;
+
+      typedef typename incidence_iterator_d<GT,1,0>::type EdgeOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,1,0>::type EdgeOnFacetIterator;
+      typedef typename incidence_iterator_d<GT,1,0>::type CellOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,1,0>::type CellOnFacetIterator;
+
+      typedef typename incidence_iterator_d<GT,1,1>::type EdgeOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,1,1>::type EdgeOnCellIterator;
+      typedef typename incidence_iterator_d<GT,1,1>::type CellOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,1,1>::type CellOnCellIterator;
+    };
 
 
 
-  // 1D
-  DEFINE_A_IS_B(Edge,Cell);
-  DEFINE_A_IS_B(edge_handle,cell_handle);
-  DEFINE_A_IS_B(EdgeIterator,CellIterator);
-  DEFINE_A_IS_B_ALL_INC(Edge,Cell);
+    template<class GT> 
+    struct dim_dep_defs_dim<GT,2> : public GT {
+
+      typedef typename element_handle_d    <GT,0>  ::type vertex_handle;
+      typedef typename element_handle_d    <GT,1>  ::type edge_handle;
+      typedef typename element_handle_d    <GT,2>  ::type face_handle;
+      typedef typename element_handle_d    <GT,1>  ::type facet_handle;
+      typedef typename element_handle_d    <GT,2>  ::type cell_handle;
+
+      typedef typename element_d           <GT,0>  ::type Vertex;
+      typedef typename element_d           <GT,1>  ::type Edge;
+      typedef typename element_d           <GT,2>  ::type Face;
+      typedef typename element_d           <GT,1>  ::type Facet;
+      typedef typename element_d           <GT,2>  ::type Cell;
+
+      typedef typename sequence_iterator_d <GT,0>  ::type VertexIterator;
+      typedef typename sequence_iterator_d <GT,1>  ::type EdgeIterator;
+      typedef typename sequence_iterator_d <GT,2>  ::type FaceIterator;
+      typedef typename sequence_iterator_d <GT,1>  ::type FacetIterator;
+      typedef typename sequence_iterator_d <GT,2>  ::type CellIterator;
 
 
-  DEFINE_A_IS_B(Vertex,Facet);
-  DEFINE_A_IS_B(vertex_handle, facet_handle);
-  DEFINE_A_IS_B(VertexIterator,FacetIterator);
-  DEFINE_A_IS_B_ALL_INC(Vertex,Facet);
+      typedef typename incidence_iterator_d<GT,0,1>::type VertexOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,0,1>::type VertexOnFacetIterator;
 
-  // 2D
-  DEFINE_A_IS_B(Cell,Face);
-  DEFINE_A_IS_B(cell_handle,  face_handle);
-  DEFINE_A_IS_B(CellIterator, FaceIterator);
-  //DEFINE_A_IS_B(CellOnCellIterator, CellOnFaceIterator);
-  DEFINE_A_IS_B_ALL_INC(Cell,Face);
+      typedef typename incidence_iterator_d<GT,0,2>::type VertexOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,0,2>::type VertexOnCellIterator;
 
 
-  DEFINE_A_IS_B(Edge,Facet);
-  DEFINE_A_IS_B(edge_handle,facet_handle);
-  DEFINE_A_IS_B(EdgeIterator,FacetIterator);
-  DEFINE_A_IS_B_ALL_INC(Edge,Facet);
+      typedef typename incidence_iterator_d<GT,1,0>::type EdgeOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,1,0>::type FacetOnVertexIterator;
 
-  // 3D
-  DEFINE_A_IS_B(Facet,Face);
-  DEFINE_A_IS_B(facet_handle,  face_handle);
-  DEFINE_A_IS_B(FacetIterator, FaceIterator);
-  DEFINE_A_IS_B_ALL_INC(Facet,Face);
+      typedef typename incidence_iterator_d<GT,1,2>::type EdgeOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,1,2>::type EdgeOnCellIterator;
+      typedef typename incidence_iterator_d<GT,1,2>::type FacetOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,1,2>::type FacetOnCellIterator;
 
 
-#undef DEFINE_A_IS_B
-#undef DEFINE_A_IS_B_ALL_INC
-  
-#define DEFINE_A_IS_B_INC_ITERS(A,B) \
-    A ## OnVertexIterator ## _is_ ## B  ## OnVertexIterator \
-    <A ## OnEdgeIterator   ## _is_ ## B  ## OnEdgeIterator \
-     <A ## OnFaceIterator ## _is_ ## B  ## OnFaceIterator \
-      <A ## OnFacetIterator ## _is_ ## B  ## OnFacetIterator \
-       <A ## OnCellIterator ## _is_ ## B  ## OnCellIterator \
-        <VertexOn ## A ## Iterator_is_VertexOn ## B ## Iterator \
-	 <EdgeOn   ## A ## Iterator_is_EdgeOn   ## B ## Iterator	\
-	  <FaceOn   ## A ## Iterator_is_FaceOn   ## B ## Iterator	\
-	   <FacetOn  ## A ## Iterator_is_FacetOn  ## B ## Iterator	\
-	    <CellOn   ## A ## Iterator_is_CellOn   ## B ## Iterator	\
-	     <GT>							\
-            > \
-           > \
-          > \
-         > \
-        > \
-       > \
-      > \
-     > \
-    > \
+      typedef typename incidence_iterator_d<GT,2,0>::type CellOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,2,0>::type FaceOnVertexIterator;
+
+      typedef typename incidence_iterator_d<GT,2,1>::type FaceOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,2,1>::type FaceOnFacetIterator;
+      typedef typename incidence_iterator_d<GT,2,1>::type CellOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,2,1>::type CellOnFacetIterator;
+
+      typedef typename incidence_iterator_d<GT,2,2>::type FaceOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,2,2>::type FaceOnCellIterator;
+      typedef typename incidence_iterator_d<GT,2,2>::type CellOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,2,2>::type CellOnCellIterator;
+    };
 
 
 
-#define DEFINE_A_IS_B_ALL(A,a,B,b) \
-  template<class GT> struct a ## _is_ ## b : public \
-  A ## _is_ ## B \
-  < a ## _handle_is_ ## b ## _handle				\
-   < A ## Iterator_is_ ## B ## Iterator			\
-     < DEFINE_A_IS_B_INC_ITERS(A,B) >			\
-   > \
-  > \
-  {};
 
-  
+    template<class GT> 
+    struct dim_dep_defs_dim<GT,3> : public GT {
 
-  // 0D
-  DEFINE_A_IS_B_ALL(Vertex,vertex,Cell,cell);
-  // 1D 
-  DEFINE_A_IS_B_ALL(Vertex,vertex,Facet,facet);
-  DEFINE_A_IS_B_ALL(Edge,  edge,  Cell, cell);
-  // 2D
-  DEFINE_A_IS_B_ALL(Cell,cell,Face,face);
-  DEFINE_A_IS_B_ALL(Edge,edge,Facet,facet);
-  // 3D
-  DEFINE_A_IS_B_ALL(Facet, facet, Face, face);
+      typedef typename element_handle_d    <GT,0>  ::type vertex_handle;
+      typedef typename element_handle_d    <GT,1>  ::type edge_handle;
+      typedef typename element_handle_d    <GT,2>  ::type face_handle;
+      typedef typename element_handle_d    <GT,2>  ::type facet_handle;
+      typedef typename element_handle_d    <GT,3>  ::type cell_handle;
 
+      typedef typename element_d           <GT,0>  ::type Vertex;
+      typedef typename element_d           <GT,1>  ::type Edge;
+      typedef typename element_d           <GT,2>  ::type Face;
+      typedef typename element_d           <GT,2>  ::type Facet;
+      typedef typename element_d           <GT,3>  ::type Cell;
 
-  template<class GT, int D> struct dim_dep_defs_aux : public GT {};
-  template<class GT> struct dim_dep_defs_aux<GT,0> : public vertex_is_cell              <GT>   {};
-  template<class GT> struct dim_dep_defs_aux<GT,1> : public edge_is_cell<vertex_is_facet<GT> > {};
-  template<class GT> struct dim_dep_defs_aux<GT,2> : public cell_is_face<edge_is_facet  <GT> > {};
-  template<class GT> struct dim_dep_defs_aux<GT,3> : public facet_is_face               <GT>   {};
+      typedef typename sequence_iterator_d <GT,0>  ::type VertexIterator;
+      typedef typename sequence_iterator_d <GT,1>  ::type EdgeIterator;
+      typedef typename sequence_iterator_d <GT,2>  ::type FaceIterator;
+      typedef typename sequence_iterator_d <GT,2>  ::type FacetIterator;
+      typedef typename sequence_iterator_d <GT,3>  ::type CellIterator;
 
 
-  template<class GT> struct dim_dep_defs : public dim_dep_defs_aux<GT, GT::dimension_tag::dim> {};
 
-}} // namespace GrAL { namespace grid_types_detail
+      typedef typename incidence_iterator_d<GT,0,2>::type VertexOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,0,2>::type VertexOnFacetIterator;
+
+
+      typedef typename incidence_iterator_d<GT,1,2>::type EdgeOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,1,2>::type EdgeOnFacetIterator;
+
+
+      typedef typename incidence_iterator_d<GT,2,0>::type FaceOnVertexIterator;
+      typedef typename incidence_iterator_d<GT,2,0>::type FacetOnVertexIterator;
+
+      typedef typename incidence_iterator_d<GT,2,1>::type FaceOnEdgeIterator;
+      typedef typename incidence_iterator_d<GT,2,1>::type FacetOnEdgeIterator;
+
+      typedef typename incidence_iterator_d<GT,2,3>::type FaceOnCellIterator;
+      typedef typename incidence_iterator_d<GT,2,3>::type FacetOnCellIterator;
+
+
+      typedef typename incidence_iterator_d<GT,3,2>::type CellOnFaceIterator;
+      typedef typename incidence_iterator_d<GT,3,2>::type CellOnFacetIterator;
+    };
+
+
+  }} //   namespace grid_types_detail { namespace type_aliases {
+
+  template<class GT>
+  struct dim_dep_defs : grid_types_detail::type_aliases::dim_dep_defs_dim<GT, dimgt<GT>::value> {};
+
+
+} // namespace GrAL
 
 #endif
