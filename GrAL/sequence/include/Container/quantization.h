a87a0880b3dcac650236ca33ec90f6900104546e
berti <>
2006-02-08 10:13:11 +0000

    *** empty log message ***

--- GrAL/sequence/include/Container/quantization.h
+++ GrAL/sequence/include/Container/quantization.h
@@ -6,14 +6,16 @@
 
 #include "Utility/pre-post-conditions.h"
 #include "Container/sequence-algorithms.h"
+#include "Container/integer-range.h"
 
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 namespace GrAL {
 
 
-  /*! \brief Map a continous range of values to a discrete one.
+  /*! \brief Map a continuous range of values to a discrete one.
 
   */
   template<class A, class R>
@@ -32,7 +34,7 @@ namespace GrAL {
 
   public:
 
-    quantization() { this->labels.push_back(0); }
+    quantization() { this->label.push_back(0); }
     quantization(argument_type x) {
       threshold.push_back(x);
       label.push_back(0);
@@ -40,28 +42,14 @@ namespace GrAL {
       ENSURE_ALWAYS( this->operator()(x) == label[1], "", 1);
     }
     quantization(argument_type t0, argument_type t1) {
-      REQUIRE_ALWAYS(t0 < t1, "", 1);
-      threshold.push_back(t0);
-      threshold.push_back(t1);
-      label.push_back(0);
-      label.push_back(1);
-      label.push_back(2);
-      ENSURE_ALWAYS( this->operator()(t0) == label[1], "", 1);
-      ENSURE_ALWAYS( this->operator()(t1) == label[2], "", 1);
+      argument_type a[2] = { t0, t1 };
+      init(a,a+2);
     }
     quantization(argument_type t0, argument_type t1, argument_type t2) {
-      REQUIRE_ALWAYS(t0 <= t1 && t1 <= t2, "", 1);
-      threshold.push_back(t0);
-      threshold.push_back(t1);
-      threshold.push_back(t2);
-      label.push_back(0);
-      label.push_back(1);
-      label.push_back(2);
-      label.push_back(3);
-      ENSURE_ALWAYS( this->operator()(t0) == label[1], "", 1);
-      ENSURE_ALWAYS( this->operator()(t1) == label[2], "", 1);
-      ENSURE_ALWAYS( this->operator()(t2) == label[3], "", 1);
+      argument_type a[3] = { t0, t1, t2 };
+      init(a, a+3);
     }
+    void clear() { label.clear(); threshold.clear(); }
 
     template<class It>
     void init(It first, It beyond) {
@@ -74,22 +62,80 @@ namespace GrAL {
       label.push_back(i);
     }
 
+    template<class TIt, class LIt>
+    void init(TIt first_threshold, TIt beyond_threshold,
+	      LIt first_label,     LIt beyond_label);
 
-    label_type operator()(argument_type v) const { 
+    int label_index(argument_type v) const {
       int i = 0;
-      //while(v >= threshold[i] && i < (int)threshold.size() -1)
       while( i < (int)threshold.size() && v >= threshold[i])
 	++i;
-      //if(v >= threshold[i])
-	// in this case, we stopped because i == threshold.size() - 1 
-	//++i;
-      return label[i];
+      // now i == threshold.size() || v < threshold[i]
+      return i;
     }
+    label_type  operator()(argument_type v) const { return label[label_index(v)];}
+    label_type& operator()(argument_type v)       { return label[label_index(v)];}
 
     label_type low () const { return label.front();}
     label_type high() const { return label.back ();}
 
-  };
+    void read (std::istream& in);
+    void write(std::ostream& out) const;
+
+  }; // class quantization
+
+
+
+  template<class A, class R>
+  std::istream& operator>>(std::istream& in,  quantization<A,R>      & q) { q.read(in); return in;}
+
+  template<class A, class R>
+  std::ostream& operator<<(std::ostream& out, quantization<A,R> const& q) { q.write(out); return out;}
+
+
+
+  template<class A, class R>
+  template<class TIt, class LIt>
+  void quantization<A,R>::init(TIt first_threshold, TIt beyond_threshold,
+			       LIt first_label,     LIt beyond_label) 
+  {
+    REQUIRE_ALWAYS(sequence::is_increasing(first_threshold, beyond_threshold), "", 1);
+    REQUIRE_ALWAYS(1 + std::distance(first_threshold, beyond_threshold) 
+		   =   std::distance(first_label,     beyond_label), "", 1);
+    while(first_threshold != beyond_threshold) {
+      threshold.push_back(*first_threshold++);
+      label    .push_back(*first_label    ++);
+    }
+    label.push_back(*first_label);
+  }
+
+
+
+  template<class A, class R>
+  void quantization<A,R>::read(std::istream & in) 
+  { 
+    clear();
+    
+    int n_thresholds;
+    in >> n_thresholds;
+    threshold.resize(n_thresholds);
+    for(int t = 0; t < n_thresholds; ++t) 
+      in >> threshold[t];
+    label.resize(n_thresholds + 1);
+    for(int i = 0; i < (int)label.size(); ++i)
+      in >> label[i];
+  }
+
+  template<class A, class R>
+  void quantization<A,R>::write(std::ostream& out) const 
+  {
+    out << threshold.size() << " ";
+    for(int t = 0; t < (int)threshold.size(); ++t)
+      out << threshold[t] << " ";
+    for(int i = 0; i < (int)label.size(); ++i)
+      out << label[i] << " ";
+  }
+
 
 } // namespace GrAL
 
---
+++ GrAL/sequence/test/quantization/test-quantization.C
@@ -0,0 +1,43 @@
+/*! \file
+
+*/
+
+#include "Container/quantization.h"
+
+#include "Utility/pre-post-conditions.h"
+
+#include <string>
+#include <sstream>
+
+namespace GrAL {
+  template class quantization<double,int>;
+}
+
+
+int main() {
+  
+  using namespace GrAL;
+  using namespace std;
+
+  quantization<double,int> q(-1.0, 0.0, 1.0);
+
+  REQUIRE_ALWAYS(q(-2.0) == 0, "q(-2.0)=" << q(-2.0), 1);
+  REQUIRE_ALWAYS(q(-1.0) == 1, "q(-1.0)=" << q(-1.0), 1);
+  REQUIRE_ALWAYS(q(-0.5) == 1, "q(-0.5)=" << q(-0.5), 1);
+  REQUIRE_ALWAYS(q( 1.0) == 3, "q( 1.0)=" << q( 1.0), 1);
+  REQUIRE_ALWAYS(q( 2.0) == 3, "q( 2.0)=" << q( 2.0), 1);
+
+  string s = "3 -1.0  0.0  1.0  0 1 2 3 ";
+  istringstream in(s);
+
+  quantization<double,int> q2;
+  in >> q2;
+  REQUIRE_ALWAYS(q(-2.0) == 0, "q(-2.0)=" << q(-2.0), 1);
+  REQUIRE_ALWAYS(q(-1.0) == 1, "q(-1.0)=" << q(-1.0), 1);
+  REQUIRE_ALWAYS(q(-0.5) == 1, "q(-0.5)=" << q(-0.5), 1);
+  REQUIRE_ALWAYS(q( 1.0) == 3, "q( 1.0)=" << q( 1.0), 1);
+  REQUIRE_ALWAYS(q( 2.0) == 3, "q( 2.0)=" << q( 2.0), 1);
+
+
+
+}
