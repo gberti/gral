0346e60e4391151554c9fb0ba12a88644e5295cb
berti <>
2007-02-21 12:39:13 +0000

    *** empty log message ***

---
+++ GrAL/sequence/include/Container/counting-iterator.h
@@ -0,0 +1,88 @@
+#ifndef GRAL_GB_SEQUENCE_COUNTING_ITERATOR_H
+#define GRAL_GB_SEQUENCE_COUNTING_ITERATOR_H
+
+// $LICENSE_NEC_2006
+
+#include <iterator>
+
+namespace GrAL {
+
+  /*! \defgroup counting_iterator_grp Counting output iterator
+      \ingroup helperfunctors
+  */
+
+  /*! \brief A simple output iterator counting the number of increments
+ 
+      \ingroup counting_iterator_grp
+    
+      This class models STL "OutputIterator"
+      and can be used in STL algorithms when only the size of a result
+      sequence is of interest.
+      Because of the missing assignment operator, 
+      <a href="http://www.boost.org/libs/iterator/doc/counting_iterator.html" target="parent"> boost::counting_iterator </a>
+      cannot be used in these cases.
+
+      \see Tested in \ref test-counting-iterator.C
+
+      \b Example:
+      \code
+      int a[] = { 1, 2, 3 };
+      int b[] = { 3, 4 };
+      // 3 different ways of accessing the output size:
+      assert( 2 == * std::set_difference(a+0,a+3,  b+0, b+2, GrAL::counter(0)));
+      assert( 2 ==   std::set_difference(a+0,a+3,  b+0, b+2, GrAL::counter(0)).size() );
+      assert( 2 ==   std::set_difference(a+0,a+3,  b+0, b+2, GrAL::counter(0)).count());
+      \endcode
+   */ 
+  template<class I = int>
+  class counting_output_iterator  {
+    typedef counting_output_iterator<I> self;
+    I cnt;
+
+    struct proxy_type {
+      I value;
+      proxy_type(I v) : value(v) {}
+
+      //! \brief formal assigment (without effect)
+      template<class T>
+      proxy_type& operator=(T const&) { return *this;}
+      
+      operator I() const { return value;}
+    };
+  public:
+    typedef I                        value_type;
+    typedef proxy_type               reference;
+    typedef I*                       pointer;
+    typedef ptrdiff_t                difference_type;
+    typedef size_t                   size_type;
+    typedef std::output_iterator_tag iterator_category;
+  public:
+    //! \brief Construction 
+    counting_output_iterator(I start = 0) : cnt(start) {}
+    /*! \brief Convertible to value_type, giving the number of increments.
+     
+    Assignment to the result is a no-op, but must be syntactically allowed,
+    as this class is used as output iterator.
+    */
+    proxy_type operator*() const { return proxy_type(cnt);}
+    //! \brief preincrement
+    self& operator++() { ++cnt; return *this;} 
+    //! \brief postincrement
+    self& operator++(int) { self res = *this; ++cnt; return res;}
+ 
+    //! \brief number of increments
+    I size()  const { return cnt;}
+    //! \brief number of increments
+    I count() const { return cnt;}
+ };
+
+  /*! \brief Creator function for counting_output_iterator
+
+      \ingroup counting_iterator_grp
+   */
+  template<class I> 
+  inline counting_output_iterator<I> counter(I start) { return counting_output_iterator<I>(start);}
+
+};
+
+#endif
---
+++ GrAL/sequence/test/counting-iterator/test-counting-iterator.C
@@ -0,0 +1,45 @@
+/*! \file
+
+*/
+
+// $LICENSE_NEC_2006
+
+#include "Container/counting-iterator.h"
+
+#include <algorithm>
+#include <iostream>
+
+namespace GrAL {
+  template class counting_output_iterator<int>;
+}
+
+
+
+int main() 
+{
+  int a[] = { 1, 2, 3};
+  int n;
+
+  n = * std::copy(a+0, a+0, GrAL::counter(0));
+  std::cout << n << " elements copied " << std::endl;
+ 
+  n = * std::copy(a+0, a+3, GrAL::counter(0));
+  std::cout << n << " elements copied " << std::endl;
+
+  int b[] = { 3, 4 };
+  n = * std::copy(a+0, a+3, std::copy(b+0, b+2, GrAL::counter(0)));
+  std::cout << n << " elements copied " << std::endl;
+  
+
+  n = * std::set_difference(a+0, a+3,
+			    b+0, b+2,
+			    GrAL::counter(0));
+  std::cout << n << " elements in a \\ b " << std::endl;
+  n = * std::set_difference(a+0, a+3,
+			    a+0, a+3,
+			    GrAL::counter(0));
+  std::cout << n << " elements in a \\ a " << std::endl;
+
+
+ 
+}
