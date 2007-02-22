3fc13fc15093ea137bff237a70ed8574ebe08a31
berti <>
2007-02-22 18:50:13 +0000

    test first coord line for different space dimension

--- GrAL/gral/base/src/IO/complex2d-format-input.C
+++ GrAL/gral/base/src/IO/complex2d-format-input.C
@@ -1,4 +1,9 @@
 #include "Gral/IO/complex2d-format-input.h"
+#include "Utility/string-utils.h"
+
+#include <vector>
+#include <sstream>
+#include <iterator>
 
 namespace GrAL {
 
@@ -7,8 +12,27 @@ IstreamComplex2DFmt::read_coords() const
 {
   base::init();
   typedef point_traits<coord_type> pt;
-  coords =  ::std::vector<coord_type>(NumOfVertices());
-  for(unsigned v = 0; v < NumOfVertices(); ++v) {
+  coords = std::vector<coord_type>(NumOfVertices());
+
+  // read first line, check number of floating point numbers
+  std::string coord_line = get_non_empty_line(In());
+  std::istringstream is(coord_line);
+  std::vector<double> coords_1;
+  coords_1.insert(coords_1.begin(),
+		  std::istream_iterator<double>(is),
+		  std::istream_iterator<double>());
+  if(coords_1.size() != spacedim) {
+    std::cerr << "Changing spacedim from " << spacedim << " to " << coords_1.size() << std::endl;
+    spacedim = coords_1.size();
+  }
+  unsigned v = 0;
+  pt::ConstructWithDim(spacedim,coords[v]);
+  for(int i = pt::LowerIndex(coords[v]); i <= pt::UpperIndex(coords[v]); ++i) {
+    coords[v][i] = coords_1[i-pt::LowerIndex(coords[v])];
+  }
+  
+  for(v = 1; v < NumOfVertices(); ++v) {
+
     pt::ConstructWithDim(spacedim,coords[v]);
     for(int i = pt::LowerIndex(coords[v]); i <= pt::UpperIndex(coords[v]); ++i) {
       In() >> coords[v][i];
