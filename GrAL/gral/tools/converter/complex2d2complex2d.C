5d70380e5c10206872ee4b0d7dcf7080724633da
berti <>
2007-03-02 17:30:30 +0000

    added geometric transformation of grids

--- GrAL/gral/tools/converter/complex2d2complex2d.C
+++ GrAL/gral/tools/converter/complex2d2complex2d.C
@@ -1,6 +1,14 @@
-#include "Gral/IO/complex2d-format.h"
+#include <Gral/IO/complex2d-format.h>
+#include <Gral/Grids/Complex2D/all.h>
 
-#include "IO/control-device.h"
+#include <Gral/Geometries/simple-geometry.h>
+#include <Gral/Geometries/transformed-geometry.h>
+
+#include <IO/control-device.h>
+
+#include <Container/tuple-point-traits.h>
+#include <Geometry/affine-mapping.h>
+#include <Geometry/matrix.h>
 
 #include <string>
 
@@ -8,37 +16,78 @@ int main(int argc, char* argv[]) {
   using namespace GrAL;
   using namespace std;
 
+  typedef tuple<double,3>                               coord_type;
+  typedef point_traits<coord_type>                      pt;
+  typedef algebraic_primitives<coord_type>              ap;
+  typedef GrAL::matrix<3,3,0>                           matrix_type;
+  typedef GrAL::affine_mapping<matrix_type, coord_type> mapping_type;
+
+
   ControlDevice Ctrl = 
-    GetCommandlineAndFileControlDevice(argc,argv,"complex2d2complex2d.in","main");
+    GetCommandlineAndFileControlDevice(argc,argv,"","main");
 
   string h; 
-  h += "complex2d2complex2d -g <grid>   [-gd <spacedim=2> -goff <offset=0>]\n";
-  h += "                    -o <output> [-od <spacedim=2> -ooff <offset=0>]\n";
+  h += "complex2d2complex2d: Transform a mesh in complex2d format.\n";
+
   string grid_in;
   RegisterAt(Ctrl, "-g", grid_in);
+  RegisterAt(Ctrl, "-i", grid_in);
+  RegisterAt(Ctrl, "-in", grid_in);
+  h += "   -g|-i|-in <grid>  (input grid, complex2d format)\n";
+  // Nice to have:
+  // RegisterAt(Ctrl, opt("-g", "-i", "-in"), grid_in);
 
   string grid_out;
-  RegisterAt(Ctrl, "-o", grid_out);
+  RegisterAt(Ctrl, "-o",   grid_out);
+  RegisterAt(Ctrl, "-out", grid_out);
+  h += "   -o|-out <grid>  (output grid, complex2d format)\n";
+
   unsigned sdim_in  = 2;
   RegisterAt(Ctrl, "-gd", sdim_in);
+  h += "   -gd <spacedim=2> (spatial dim of input grid)\n";
+
   unsigned sdim_out = 2;
   RegisterAt(Ctrl, "-od", sdim_out);
+  h += "   -od <spacedim=2> (spatial dim of output grid)\n";
+
   unsigned offset_in = 0;
   RegisterAt(Ctrl, "-goff", offset_in);
+  h += "   -goff <offset=0> (vertex numbering offset of input grid)\n";
+
   unsigned offset_out = 0;
   RegisterAt(Ctrl, "-ooff", offset_out);
+  h += "   -ooff <offset=0> (vertex numbering offset of output grid)\n";
 
-  Mutator* helpmsg = new MessageOnReadMutator(cerr,h);
+  // transformation info
+  mapping_type M;
+  RegisterAt(Ctrl, "-map", M);
+  h += "    -map  <a11 a12 a13 t1 a21 a22 a23 t2 a31 a32 a33 t3> \n";
+  h += "          (mapping coefficients, the aij forming the linear part and the ti the translation part)\n";
+
+
+
+  Mutator helpmsg = GetMessageOnReadMutator(cerr,h);
   Ctrl.add("-h",     helpmsg);
   Ctrl.add("--help", helpmsg);
   Ctrl.add("-?",     helpmsg);
 
   Ctrl.update();
 
+
   IstreamComplex2DFmt In(grid_in, offset_in);
   In.set_spacedim(sdim_in);
-  OstreamComplex2DFmt Out(grid_out, offset_out);
-  //  Out.set_spacedim(sdim_out);
 
-  ConstructGrid(Out, In, In);
+  typedef Complex2D                                     grid_type;
+  typedef simple_geometry<grid_type, coord_type>        geom_type;
+
+  grid_type  G;
+  geom_type  GeomG;
+  ConstructGrid(G,GeomG, In,In);
+
+  transformed_geom<geom_type, mapping_type> TGeomG(GeomG, M);
+
+
+  OstreamComplex2DFmt Out(grid_out, offset_out);
+
+  ConstructGrid(Out, G, TGeomG);
 }
