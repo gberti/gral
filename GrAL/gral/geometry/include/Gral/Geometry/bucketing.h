ade648ac6a1c3a0098432c2942c7507f44feb2aa
berti <>
2007-03-06 10:17:58 +0000

    *** empty log message ***

---
+++ GrAL/gral/geometry/include/Gral/Geometry/bucketing.h
@@ -0,0 +1,135 @@
+#ifndef GRAL_GB_GRAL_GEOMETRY_BUCKETING_H
+#define GRAL_GB_GRAL_GEOMETRY_BUCKETING_H
+
+// $LICENSE_NEC_2007
+
+#include "Geometry/affine-mapping.h"
+#include "Geometry/matrix.h"
+#include "Geometry/point-traits.h"
+#include "Geometry/algebraic-primitives.h"
+
+#include "Utility/ref-ptr.h"
+#include "Container/sequence-algorithms.h"
+
+#include "Gral/Grids/CartesianND/all.h"
+
+#include <vector>
+
+namespace GrAL {
+
+
+  template<class T, int N>
+  class bucket_table {
+  public:
+    typedef std::vector<T>                                             value_type;
+
+    typedef cartesiannd::grid<N>                                       grid_type;
+    typedef grid_types<grid_type>                                      gt;
+    typedef matrix<N, N, 0>                                            matrix_type;
+    typedef typename cartesiannd::default_coord<grid_type>::type       coord_type;
+    typedef affine_mapping<matrix_type, coord_type>                    mapping_type;
+    typedef cartesiannd::mapped_geometry<grid_type, mapping_type>      geom_type;
+                                                                       
+    typedef typename grid_type::index_type                             index_type;
+    typedef typename gt::Cell                                          Cell;
+    typedef typename gt::cartesian_subrange_type                       cart_subrange_type;
+
+    typedef partial_grid_function<Cell, value_type>                    table_type;
+  private:
+    ref_ptr<grid_type>                                   g;
+    ref_ptr<geom_type>                                   geom;
+    ref_ptr<table_type>                                  b;
+  
+  public:
+    bucket_table() {}
+    bucket_table(index_type n, coord_type min_x, coord_type max_x) { init(n, min_x, max_x);}
+
+    void init(index_type n, coord_type min_x, coord_type max_x);
+
+    template<class COORD>
+    Cell locate(COORD x) const { 
+      REQUIRE(inside(x), "could not find point " << x, 1);
+      return geom->locate(x).TheCell(); 
+    }
+
+    template<class COORD>
+    bool inside(COORD x) const { return geom->locate(x).found();}
+
+    /*! \brief Access values at bucket of \c x 
+     */
+    template<class COORD>
+    value_type & operator[](COORD x) {
+      return (*b)[locate(x)];
+    }
+    
+    /*! \brief Access values at bucket of \c x (read only)
+     */
+    template<class COORD>
+    value_type const& operator()(COORD x) {
+      return get(locate(x));
+    }
+    
+    value_type const& get(Cell c) const {
+      return (*b)(c);
+    }
+     
+    template<class COORD>    
+    value_type get_neighborhood(COORD x, int width = 1) const {
+      return get_neighborhood(locate(x), width);
+    }
+    
+    value_type get_neighborhood(Cell c, int width = 1) const {
+      index_type w(width);
+      cart_subrange_type nbs(g, 
+			     Cell(g, clamp_tuple(g->low_cell_index(), g->high_cell_index(), c.index() - w)),
+			     Cell(g, clamp_tuple(g->low_cell_index(), g->high_cell_index(), c.index() + w)));
+			     
+      
+      value_type res;
+      typedef grid_types<cart_subrange_type> rgt;
+      for(typename rgt::CellIterator nb = GrAL::begin_x(nbs); !nb.IsDone(); ++nb) {
+	value_type const& nbs_i = get(*nb);
+	res.insert(res.end(), nbs_i.begin(), nbs_i.end());
+      }
+      GrAL::sequence::sort_and_make_unique(res);
+      return res;
+    }
+
+    grid_type const& TheGrid() const { return *g;}
+    geom_type const& TheGeom() const { return *geom;}
+
+    coord_type min() const { return geom->coord(typename gt::Vertex(g, g->low_vertex_index()));}
+    coord_type max() const { return geom->coord(typename gt::Vertex(g, g->high_vertex_index()));}
+    
+  }; // class bucket_table
+
+
+  template<class T, int N>
+  void bucket_table<T,N>::init(typename bucket_table<T,N>::index_type n,
+			       typename bucket_table<T,N>::coord_type min_x,
+			       typename bucket_table<T,N>::coord_type max_x)
+ 
+    { 
+      // make the window a bit larger, to avoid boundary situations
+      // and dist values of zero.
+      n = max_tuple(n, index_type(1));
+      coord_type adjust = quotient(coord_type(1.0), coord_type(n));
+      min_x = min_x - 0.5*adjust;
+      max_x = max_x + 0.5*adjust;
+      coord_type dist  = max_x - min_x;
+
+      g    = new_ref_ptr(new grid_type(n+index_type(1)));
+      typedef mapping_type mt;
+      mapping_type A     = mt::translation(min_x) (mt::scaling(dist));
+      mapping_type A_inv = mt::scaling(inverse(dist))(mt::translation(-min_x));
+      geom = new_ref_ptr(new geom_type(*g, g->low_vertex_index(), g->high_vertex_index(), A));
+      geom->set_inverse_mapping(A_inv);
+      b = new_ref_ptr(new table_type(g));
+    }
+  
+
+
+}
+
+
+#endif
---
+++ GrAL/gral/geometry/test/bucketing/test-bucketing.C
@@ -0,0 +1,57 @@
+/*! \file */
+
+// $LICENSE_NEC_2007
+
+#include "Gral/Geometry/bucketing.h"
+
+#include "Utility/pre-post-conditions.h"
+#include "Container/range.h"
+
+#include <iostream>
+
+namespace GrAL {
+  template class bucket_table<int,2>;
+}
+
+
+int main() {
+  using namespace std;
+
+  typedef GrAL::bucket_table<int, 2>::coord_type c_type;
+  typedef GrAL::bucket_table<c_type, 2>  bucket_type;
+  typedef bucket_type::grid_type      grid_type;
+  typedef GrAL::grid_types<grid_type> gt;
+  typedef bucket_type::index_type index_type;
+  typedef bucket_type::coord_type coord_type;
+
+  coord_type min_x(1.0,2.0);
+  coord_type max_x(5.0,6.0);
+  index_type n(2,3);
+  bucket_type b(n, min_x, max_x);
+  cout << "Bucket: cells [" << b.TheGrid().low_cell_index()  << "  " << b.TheGrid().high_cell_index() <<"]\n";
+  cout << "Bucket: range [" << b.min() << ", " << b.max() << "]\n";
+
+  
+  index_type N(10,10);
+  coord_type delta = quotient(max_x-min_x, coord_type(N));
+  for(int x = 0; x < N[0]; ++x)
+    for(int y = 0; y < N[1]; ++y) {
+      coord_type xy = min_x + product(delta, coord_type(x,y));
+      b[xy].push_back(xy);
+      REQUIRE_ALWAYS(b(xy).back() == xy, "", 1);
+    }
+
+
+  //  for(gt::CellIterator c = GrAL::begin_x(b.TheGrid()); c != GrAL::end_x(b.TheGrid()); ++c) {
+  for(gt::CellIterator c(b.TheGrid()); !c.IsDone(); ++c) {
+    cout << "bucket " << c.index() << ": " << GrAL::range_c(b.get(*c), ", ", "(", ")") << "\n";
+  }
+  
+
+  for(gt::CellIterator c(b.TheGrid()); !c.IsDone(); ++c) {
+    cout  << "bucket " << c.index() << ": " << b.get(*c).size() << " entries, "
+	  << "neighborhood 1: " << b.get_neighborhood(*c, 1).size() << " entries.\n";
+  }
+
+
+}
